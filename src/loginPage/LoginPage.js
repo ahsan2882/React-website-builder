172988c698d6ff913954f75a0776cfb10758e2c1
Ahsan Shahid <ahsanshahid2882@gmail.com>
2021-10-04 22:36:09 +0500

    files updated 4/10/21 10:35PM

--- src/loginPage/LoginPage.js
+++ src/loginPage/LoginPage.js
@@ -1,78 +1,62 @@
-import React, { useState } from 'react'
+import React, { Component, useState } from 'react'
 import LoginPageCss from './LoginPage.module.css';
 import loginBackground from '../assets/loginPageAssets/backgrouned.svg'
-import { Link } from 'react-router-dom';
+import { Redirect, withRouter } from 'react-router-dom';
 
 
 export const LoginPage = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const [allEntry, setAllEntry] = useState([]);
+  const userEmail = 'demo@gmail.com';
+  const userPassword = 'demo123';
   const submitForm = (e) => {
-    e.preventDefault();
-    const newEntry = { email: email, password: password };
-    setAllEntry([...allEntry, newEntry]);
-    console.log(allEntry);
+    e.preventDefault()
+    if(email===userEmail && password===userPassword){
+      this.props.history.push('/login');
+    }
   }
-
-
 
   return (
     <div className={LoginPageCss.bodyWrapper}>
-      <div className={LoginPageCss.body} style={{ "background": `url(${loginBackground}) no-repeat center/cover`}}>
-        <form onSubmit={submitForm}>
+      <div className={LoginPageCss.body} style={{ "background": `url(${loginBackground}) no-repeat center/cover` }}>
+        <form onSubmit={submitForm.bind(this)}>
           <h1 className="text-white text-3xl"> Login </h1>
           <div className={LoginPageCss.form}>
             <input type="Email" name="Email" autoComplete="off" required value={email}
               onChange={(e) => setEmail(e.target.value)} />
-
-
             <label htmlFor="name" className={LoginPageCss.labelName}>
               <span className={LoginPageCss.contentName}>  Email </span>
-
-
             </label>
-
-
-
-
-
           </div>
-
           <div className={LoginPageCss.form}>
             <input type="password" name="Email" autoComplete="off" required value={password}
               onChange={(e) => setPassword(e.target.value)} />
             <label htmlFor="name" className={LoginPageCss.labelName}>
               <span className={LoginPageCss.contentName}>  Password </span>
-
-
             </label>
-
           </div>
-
-
-
-
-
-          <Link to='/select-template' className={LoginPageCss.button}> Login   </Link>
-          <div>
-            {
-              allEntry.map((curEle) => {
-                return (
-                  <div>
-                    <p className="text-white">  Email :{curEle.email} </p>
-                    <p className="text-white"> Password : {curEle.password} </p>
-
-                  </div>
-                )
-              })
-            }
-
-
-
-          </div>
+          {/* <Link to='/select-template' className={LoginPageCss.button}> Login   </Link> */}
+          <button type="submit" className={LoginPageCss.button}>Login</button>
         </form>
       </div>
     </div>
   )
 }
+export default withRouter(LoginPage);
+
+// export default class LoginPage extends Component {
+//   onSubmit = () => {
+//     if (email === userEmail && password === userPassword) {
+//       return <Redirect to="/posts/" />
+//     }
+//   }
+//   render() {
+//     const [email, setEmail] = useState("");
+//     const [password, setPassword] = useState("");
+//     return (
+//       <div>
+        
+//       </div>
+//     )
+//   }
+// }
