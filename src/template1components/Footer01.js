664e037365b8759ec2d9af819080575c905fd2ab
ZaidebinUsama <77499086+usama-khalied@users.noreply.github.com>
2021-10-11 19:08:54 +0500

    11-10-2021 07:08 PM

---
+++ src/template1components/Footer01.js
@@ -0,0 +1,87 @@
+import React from 'react'
+import Footer01Css from './Footer01.module.css'
+
+
+
+
+
+export const Footer01 = () => {
+  return (
+    <>
+      <footer>
+        <div className={`${Footer01Css.footer}`}>
+
+
+        <div className={`${Footer01Css['row-foot']}`}>
+
+
+
+        <div className={`${Footer01Css['column-foot']}`}>
+        <div className={`${Footer01Css.span}`}></div>
+        <h2 id={`${Footer01Css.hh1}`}><b>Citrix</b></h2>
+        <h1 id={`${Footer01Css.hh2}`} >Consultancy</h1>
+        <p className={`${Footer01Css['main-para']}`}>Visualize quality intellectual capital without superior collaboration and idea sharing installed base portals.</p>
+        <p><i className={`${Footer01Css['fas fa-map-marker-alt i1']}`}></i> Address: 4010 Feeney Way</p>
+
+        <p> <i style={{color:`white`}} className={`${Footer01Css['fas fa-phone-alt i1']}`}></i>020 7946 0020  </p>
+
+        <p><i className={`${Footer01Css['fas fa-envelope i1']}`}></i>critrixconsultancy @gmail.com</p>
+
+        </div>
+        <div className={`${Footer01Css['column-foot']}`} >
+        <div className={`${Footer01Css.span}`}></div>
+        <h2 id={`${Footer01Css.hh1}`}><b>Our Locations</b></h2>
+        <h1 id={`${Footer01Css.hh2}`} >Where to find us?</h1>
+        <img style={{height:`200px`}} src="https://i.ibb.co/L0HJLvC/img-footer-map.png" alt="img-footer-map" border="0" />
+
+        </div>
+
+        <div   className={`${Footer01Css['column-foot']}`}>
+        <div className={`${Footer01Css.col}`}>
+        <p><i style={{color:`#00D4FF`}} className='fas fa-map-marker-alt i1 p-5 text-3xl'></i> San Diego: 619 270 8578  </p>
+
+        <p><i style={{color:`#00D4FF`}} className='fas fa-map-marker-alt i1 p-5 text-3xl'></i> Ontario: 613 285 5534</p>
+        <p><i style={{color:`#00D4FF`}} className='fas fa-map-marker-alt i1 p-5 text-3xl'></i>London: 020 7946 0020 </p>
+
+
+
+
+        </div>
+
+
+
+        </div>
+
+        <div>
+        <div className={`${Footer01Css['column-foot']}`}>
+        <div className={`${Footer01Css.span}`}></div>
+        <h2 id={`${Footer01Css.hh1}`}><b>Get in touch</b></h2>
+        <h1 id={`${Footer01Css.hh2}`} >Citrix Social links</h1>
+        <p className={`${Footer01Css['main-para']}`}>Taking seamless key performance indicators offline to maximise the long tail</p>
+        <p>
+        <i  className="fab fa-facebook text-white p-1 text-4xl text-center"></i>
+        <i className="fab fa-twitter text-white p-1 text-4xl text-center"></i>
+        <i className="fab fa-pinterest text-white p-1 text-4xl text-center"></i>
+        <i className="fab fa-linkedin text-white p-1 text-4xl text-center"></i>
+        </p>
+        </div>
+        </div>
+
+
+
+        </div>
+
+
+        <div className={`${Footer01Css['bottom-foot']}`}>
+        <div className={`${Footer01Css.copyright}`}>
+        <p>Copyright © 2021 CITRIX CONSULTANCY.All Rights Reserved.</p>
+        </div>
+
+
+        </div>
+        </div>
+      </footer>
+
+    </>
+  );
+}
--- src/template1components/Template1Page.js
+++ src/template1components/Template1Page.js
@@ -1,9 +1,28 @@
 import React from 'react'
-
+import {Header01} from './Header01'
+import {Service01} from './Service01'
+import {Testimonials} from './Testimonials';
+import {Prizing01}  from './Prizing01'
+import {Guard01} from './Guard01'
+import {News01} from './News01'
+import {Program01} from './Program01'
+import {About01} from './About01'
+import {Footer01} from './Footer01'
 export default function Template1Page() {
     return (
-        <div>
-            <h2>Template 1</h2>
-        </div>
-    )
+     
+     <>
+     <Header01/>
+     <Service01/>
+     <Testimonials/>
+     <Prizing01/>
+     <Guard01/>
+     <News01/>
+     <Program01/>
+     <About01/>
+     <Footer01/>
+
+
+     </>
+    );
 }
