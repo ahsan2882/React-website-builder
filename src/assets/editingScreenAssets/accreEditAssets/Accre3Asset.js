c2fb8f9cf1cd312dd3498e78aa35c5e8ed9c243f
Ahsan Shahid <ahsanshahid2882@gmail.com>
2021-10-11 15:21:14 +0500

    files updated

---
+++ src/assets/editingScreenAssets/accreEditAssets/Accre3Asset.js
@@ -0,0 +1,43 @@
+import React from 'react'
+
+export default function Accre3Asset() {
+    return (
+        <section className="accre-sec">
+            <div className="container-main">
+                <div className="heading-main">
+                    <h2 className="subhead-main">we are verified</h2>
+                    <h1 className="main-head">Our Accrediation</h1>
+                </div>
+                <div className="cards-wrapper">
+                    <div className="accre-card">
+                        <div className="card-contain">
+                            <img src="images/client1.png" alt="Accrediation 1" />
+                            <h3>Lorem ipsum.</h3>
+                        </div>
+                    </div>
+
+                    <div className="accre-card">
+                        <div className="card-contain">
+                            <img src="images/client2.png" alt="Accrediation 2" />
+                            <h3>Lorem ipsum.</h3>
+                        </div>
+                    </div>
+
+                    <div className="accre-card">
+                        <div className="card-contain">
+                            <img src="images/client3.png" alt="Accrediation 3" />
+                            <h3>Lorem ipsum.</h3>
+                        </div>
+                    </div>
+
+                    <div className="accre-card">
+                        <div className="card-contain">
+                            <img src="images/client4.png" alt="Accrediation 4" />
+                            <h3>Lorem ipsum.</h3>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    )
+}
