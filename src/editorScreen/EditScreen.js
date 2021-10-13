ccf3a085577aa9620de5812d53b340b0070b56f5
Ahsan Shahid <ahsanshahid2882@gmail.com>
2021-10-13 16:09:34 +0500

    Update EditScreen.js

--- src/editorScreen/EditScreen.js
+++ src/editorScreen/EditScreen.js
@@ -2,22 +2,29 @@ import React, { useState, createContext } from 'react'
 import HeaderEdit from './HeaderEdit'
 import ContainerEdit from './ContainerEdit'
 import FooterEdit from './FooterEdit'
+import { DndProvider } from 'react-dnd';
+import { HTML5Backend } from 'react-dnd-html5-backend'
 
-export const NewAssetContext = createContext({ newAssetState: null, setNewAssetState: (() => void 0), newAssetCount: 0, setNewAssetCount: (() => void 0)});
+export const NewAssetContext = createContext({ newAssetState: null, setNewAssetState: (() => void 0), newAssetCount: 0, setNewAssetCount: (() => void 0) });
 
 export default function EditScreen({ temp }) {
     const [defWidth, setWidth] = useState(window.innerWidth);
     const [assetSelected, setAssetSelected] = useState(false);
-    const [newAssetState, setNewAssetState] = useState(null);
-    const [newAssetCount, setNewAssetCount] = useState(0);
-    
+    // const [newAssetState, setNewAssetState] = useState(null);
+    // const [newAssetCount, setNewAssetCount] = useState(0);
+
     return (
         <>
-            <NewAssetContext.Provider value={{newAssetState, newAssetCount, setNewAssetState, setNewAssetCount}}>
+            {/* <NewAssetContext.Provider value={{newAssetState, newAssetCount, setNewAssetState, setNewAssetCount}}>
                 <HeaderEdit setWidth={setWidth} />
                 <ContainerEdit defWidth={defWidth} templateNum={temp} overlayPresent={assetSelected} />
                 <FooterEdit setOverlay={setAssetSelected} />
-            </NewAssetContext.Provider>
+            </NewAssetContext.Provider> */}
+            <DndProvider backend={HTML5Backend}>
+                <HeaderEdit setWidth={setWidth} />
+                <ContainerEdit defWidth={defWidth} templateNum={temp} overlayPresent={assetSelected} />
+                <FooterEdit setOverlay={setAssetSelected} />
+            </DndProvider>
         </>
     )
 }
