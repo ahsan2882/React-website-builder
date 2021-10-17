f90422565ebd883f4f0482ef8fda213df889e5de
Ahsan Shahid <ahsanshahid2882@gmail.com>
2021-10-17 21:52:22 +0500

    Update assetCode.js

--- src/editorScreen/assetCode.js
+++ src/editorScreen/assetCode.js
@@ -1,5 +1,13 @@
+import Accre1Asset from "../assets/editingScreenAssets/accreEditAssets/Accre1Asset";
 import Accre2Asset from "../assets/editingScreenAssets/accreEditAssets/Accre2Asset";
+import Accre3Asset from "../assets/editingScreenAssets/accreEditAssets/Accre3Asset";
+import Accre4Asset from "../assets/editingScreenAssets/accreEditAssets/Accre4Asset";
+import ContactAss1 from "../assets/editingScreenAssets/contactusEditAssets/Contact1Asset";
 
 export const assetObject = {
-    accre2 : <Accre2Asset/>
+    'accre1': <Accre1Asset/>,
+    'accre2': <Accre2Asset />,
+    'accre3': <Accre3Asset />,
+    'accre4': <Accre4Asset />,
+    'contactus1': <ContactAss1/>
 }
