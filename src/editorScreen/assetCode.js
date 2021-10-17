aab6bba4c2c299c59618ebc52be5f4e12d26b136
Ahsan Shahid <ahsanshahid2882@gmail.com>
2021-10-17 23:21:38 +0500

    Update assetCode.js

--- src/editorScreen/assetCode.js
+++ src/editorScreen/assetCode.js
@@ -1,29 +1,7 @@
-import Nav1Asset from '../assets/editingScreenAssets/navEditAssets/Nav1Asset';
-import Nav2Asset from '../assets/editingScreenAssets/navEditAssets/Nav2Asset';
-import Nav3Asset from '../assets/editingScreenAssets/navEditAssets/Nav3Asset';
-import Nav4Asset from '../assets/editingScreenAssets/navEditAssets/Nav4Asset';
-import Nav5Asset from '../assets/editingScreenAssets/navEditAssets/Nav5Asset';
-
-import Hero1Asset from '../assets/editingScreenAssets/headerEditAssets/Hero1Asset';
-import Hero2Asset from '../assets/editingScreenAssets/headerEditAssets/Hero2Asset';
-import Hero3Asset from '../assets/editingScreenAssets/headerEditAssets/Hero3Asset';
-import Hero4Asset from '../assets/editingScreenAssets/headerEditAssets/Hero4Asset';
-import Hero5Asset from '../assets/editingScreenAssets/headerEditAssets/Hero5Asset';
-
-import Footer1Asset from '../assets/editingScreenAssets/footerEditAssets/Footer1Asset';
-import Footer2Asset from '../assets/editingScreenAssets/footerEditAssets/Footer2Asset';
-import Footer3Asset from '../assets/editingScreenAssets/footerEditAssets/Footer3Asset';
-import Footer4Asset from '../assets/editingScreenAssets/footerEditAssets/Footer4Asset';
-
-import Testimonial1Asset from '../assets/editingScreenAssets/testimonialEditAssets/Testimonial1Asset';
-import Testimonial2Asset from '../assets/editingScreenAssets/testimonialEditAssets/Testimonial2Asset';
-import Testimonial3Asset from '../assets/editingScreenAssets/testimonialEditAssets/Testimonial3Asset';
-import Testimonial4Asset from '../assets/editingScreenAssets/testimonialEditAssets/Testimonial4Asset';
-
-import Faq1Asset from '../assets/editingScreenAssets/faqsEditAssets/Faq1Asset';
-import Faq2Asset from '../assets/editingScreenAssets/faqsEditAssets/Faq2Asset';
-import Faq3Asset from '../assets/editingScreenAssets/faqsEditAssets/Faq3Asset';
-import Faq4Asset from '../assets/editingScreenAssets/faqsEditAssets/Faq4Asset';
+import Accre1Asset from "../assets/editingScreenAssets/accreEditAssets/Accre1Asset";
+import Accre2Asset from "../assets/editingScreenAssets/accreEditAssets/Accre2Asset";
+import Accre3Asset from "../assets/editingScreenAssets/accreEditAssets/Accre3Asset";
+import Accre4Asset from "../assets/editingScreenAssets/accreEditAssets/Accre4Asset";
 
 import Contact1Asset from "../assets/editingScreenAssets/contactusEditAssets/Contact1Asset";
 import Contact2Asset from "../assets/editingScreenAssets/contactusEditAssets/Contact2Asset";
@@ -31,18 +9,54 @@ import Contact3Asset from "../assets/editingScreenAssets/contactusEditAssets/Con
 import Contact4Asset from "../assets/editingScreenAssets/contactusEditAssets/Contact4Asset";
 import Contact5Asset from "../assets/editingScreenAssets/contactusEditAssets/Contact5Asset";
 
+import Faq1Asset from '../assets/editingScreenAssets/faqsEditAssets/Faq1Asset';
+import Faq2Asset from '../assets/editingScreenAssets/faqsEditAssets/Faq2Asset';
+import Faq3Asset from '../assets/editingScreenAssets/faqsEditAssets/Faq3Asset';
+import Faq4Asset from '../assets/editingScreenAssets/faqsEditAssets/Faq4Asset';
+
+import Footer1Asset from '../assets/editingScreenAssets/footerEditAssets/Footer1Asset';
+import Footer2Asset from '../assets/editingScreenAssets/footerEditAssets/Footer2Asset';
+import Footer3Asset from '../assets/editingScreenAssets/footerEditAssets/Footer3Asset';
+import Footer4Asset from '../assets/editingScreenAssets/footerEditAssets/Footer4Asset';
+
+import Hero1Asset from '../assets/editingScreenAssets/headerEditAssets/Hero1Asset';
+import Hero2Asset from '../assets/editingScreenAssets/headerEditAssets/Hero2Asset';
+import Hero3Asset from '../assets/editingScreenAssets/headerEditAssets/Hero3Asset';
+import Hero4Asset from '../assets/editingScreenAssets/headerEditAssets/Hero4Asset';
+import Hero5Asset from '../assets/editingScreenAssets/headerEditAssets/Hero5Asset';
+
+import Nav1Asset from '../assets/editingScreenAssets/navEditAssets/Nav1Asset';
+import Nav2Asset from '../assets/editingScreenAssets/navEditAssets/Nav2Asset';
+import Nav3Asset from '../assets/editingScreenAssets/navEditAssets/Nav3Asset';
+import Nav4Asset from '../assets/editingScreenAssets/navEditAssets/Nav4Asset';
+import Nav5Asset from '../assets/editingScreenAssets/navEditAssets/Nav5Asset';
+
+import Pricing1Asset from '../assets/editingScreenAssets/pricingEditAssets/Pricing1Asset';
+import Pricing2Asset from '../assets/editingScreenAssets/pricingEditAssets/Pricing2Asset';
+import Pricing3Asset from '../assets/editingScreenAssets/pricingEditAssets/Pricing3Asset';
+import Pricing4Asset from '../assets/editingScreenAssets/pricingEditAssets/Pricing4Asset';
+import Pricing5Asset from '../assets/editingScreenAssets/pricingEditAssets/Pricing5Asset';
+
+import Slider1Asset from '../assets/editingScreenAssets/sliderEditAssets/Slider1Asset';
+import Slider2Asset from '../assets/editingScreenAssets/sliderEditAssets/Slider2Asset';
+import Slider3Asset from '../assets/editingScreenAssets/sliderEditAssets/Slider3Asset';
+import Slider4Asset from '../assets/editingScreenAssets/sliderEditAssets/Slider4Asset';
+import Slider5Asset from '../assets/editingScreenAssets/sliderEditAssets/Slider5Asset';
+
 import Social1Asset from '../assets/editingScreenAssets/socialEditAssets/Social1Asset';
 import Social2Asset from '../assets/editingScreenAssets/socialEditAssets/Social2Asset';
 import Social3Asset from '../assets/editingScreenAssets/socialEditAssets/Social3Asset';
 
-import Slider1Asset from '../assets/editingScreenAssets/sliderEditAssets/Slider1Asset';
+import Testimonial1Asset from '../assets/editingScreenAssets/testimonialEditAssets/Testimonial1Asset';
+import Testimonial2Asset from '../assets/editingScreenAssets/testimonialEditAssets/Testimonial2Asset';
+import Testimonial3Asset from '../assets/editingScreenAssets/testimonialEditAssets/Testimonial3Asset';
+import Testimonial4Asset from '../assets/editingScreenAssets/testimonialEditAssets/Testimonial4Asset';
 
-import Accre1Asset from "../assets/editingScreenAssets/accreEditAssets/Accre1Asset";
-import Accre2Asset from "../assets/editingScreenAssets/accreEditAssets/Accre2Asset";
-import Accre3Asset from "../assets/editingScreenAssets/accreEditAssets/Accre3Asset";
-import Accre4Asset from "../assets/editingScreenAssets/accreEditAssets/Accre4Asset";
-
-
+import Service1Asset from '../assets/editingScreenAssets/serviceEditAssets/Service1Asset';
+import Service2Asset from '../assets/editingScreenAssets/serviceEditAssets/Service2Asset';
+import Service3Asset from '../assets/editingScreenAssets/serviceEditAssets/Service3Asset';
+import Service4Asset from '../assets/editingScreenAssets/serviceEditAssets/Service4Asset';
+import Service5Asset from '../assets/editingScreenAssets/serviceEditAssets/Service5Asset';
 
 export const assetObject = {
     'nav1': <Nav1Asset />,
@@ -50,43 +64,48 @@ export const assetObject = {
     'nav3': <Nav3Asset />,
     'nav4': <Nav4Asset />,
     'nav5': <Nav5Asset />,
-
     'header1': <Hero1Asset />,
     'header2': <Hero2Asset />,
     'header3': <Hero3Asset />,
     'header4': <Hero4Asset />,
     'header5': <Hero5Asset />,
-
     'footer1': <Footer1Asset />,
     'footer2': <Footer2Asset />,
     'footer3': <Footer3Asset />,
     'footer4': <Footer4Asset />,
-
     'testimonial1': <Testimonial1Asset />,
     'testimonial2': <Testimonial2Asset />,
     'testimonial3': <Testimonial3Asset />,
     'testimonial4': <Testimonial4Asset />,
-
     'faq1': <Faq1Asset />,
     'faq2': <Faq2Asset />,
     'faq3': <Faq3Asset />,
     'faq4': <Faq4Asset />,
-
     'contact1': <Contact1Asset />,
     'contact2': <Contact2Asset />,
     'contact3': <Contact3Asset />,
     'contact4': <Contact4Asset />,
     'contact5': <Contact5Asset />,
-
     'social1': <Social1Asset />,
     'social2': <Social2Asset />,
     'social3': <Social3Asset />,
-    
-    'slider1': <Slider1Asset/>,
-
+    'slider1': <Slider1Asset />,
+    'slider2': <Slider2Asset />,
+    'slider3': <Slider3Asset />,
+    'slider4': <Slider4Asset />,
+    'slider5': <Slider5Asset />,
+    'pricing1': <Pricing1Asset />,
+    'pricing2': <Pricing2Asset />,
+    'pricing3': <Pricing3Asset />,
+    'pricing4': <Pricing4Asset />,
+    'pricing5': <Pricing5Asset />,
+    'service1': <Service1Asset />,
+    'service2': <Service2Asset />,
+    'service3': <Service3Asset />,
+    'service4': <Service4Asset />,
+    'service5': <Service5Asset />,
     'accre1': <Accre1Asset />,
     'accre2': <Accre2Asset />,
     'accre3': <Accre3Asset />,
-    'accre4': <Accre4Asset />,
-
+    'accre4': <Accre4Asset />
 }
