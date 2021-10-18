955609d911ff89970cf376143a51a0fc9e8cd0fc
Ahsan Shahid <ahsanshahid2882@gmail.com>
2021-10-18 14:37:05 +0500

    Update Contact1Asset.js

--- src/assets/editingScreenAssets/contactusEditAssets/Contact1Asset.js
+++ src/assets/editingScreenAssets/contactusEditAssets/Contact1Asset.js
@@ -1,55 +1,154 @@
-import React from 'react'
+import { Editor } from '@tinymce/tinymce-react'
+import React, { useRef } from 'react'
 
 export default function Contact1Asset() {
+    const editorRef = useRef(null);
     return (
         <>
-            <div className="rounded-lg shadow sm:max-w-md bg-opacity-8 0 bg-blue-900 sm:w-full sm:mx-auto sm:overflow-hidden mt-4 mb-4">
-                <div className="px-4 py-8 sm:px-10">
-                    <div className="relative mt-6">
-                        <div className="absolute inset-0 flex items-center">
-                            <div className="w-full border-t border-gray-300">
-                            </div>
-                        </div>
-                        <div className="relative flex justify-center text-sm leading-5">
-                            <span className="px-2 text-gray-500 bg-white">
-                                Contact Us
-                            </span>
-                        </div>
+            <section className="text-gray-600 body-font relative">
+                <div className="container px-5 py-24 mx-auto bg-blue-900 mt-9">
+                    <div className="flex flex-col text-center w-full mb-12">
+                        {/* <h1 className="sm:text-3xl text-2xl font-medium title-font mb-4 text-gray-100">Contact Us</h1> */}
+                        <h1 className="sm:text-3xl text-2xl font-medium title-font mb-4 text-gray-100">
+                            <Editor
+                                onInit={(evt, editor) => (editorRef.current = editor)}
+                                inline={true}
+                                key="contact1asset1"
+                                tinymceScriptSrc={
+                                    process.env.PUBLIC_URL + "/tinymce/tinymce.min.js"
+                                }
+                                initialValue="Contact us"
+                                init={{
+                                    menubar: false,
+                                    toolbar:
+                                        "bold italic underline | fontsizeselect | backcolor forecolor | alignleft aligncenter " +
+                                        "alignright alignjustify | fontselect",
+                                    fontsize_formats:
+                                        "8px 9px 10px 11px 12px 14px 16px 18px 20px 24px 30px 36px 48px 60px 72px 96px",
+                                    font_formats:
+                                        "Andale Mono=andale mono,times; Arial=arial,helvetica,sans-serif; Arial Black=arial black,avant garde; Book Antiqua=book antiqua,palatino; Comic Sans MS=comic sans ms,sans-serif; Courier New=courier new,courier; Georgia=georgia,palatino; Helvetica=helvetica; Impact=impact,chicago; Symbol=symbol; Tahoma=tahoma,arial,helvetica,sans-serif; Terminal=terminal,monaco; Times New Roman=times new roman,times; Trebuchet MS=trebuchet ms,geneva; Verdana=verdana,geneva; Webdings=webdings; Wingdings=wingdings,zapf dingbats",
+                                }}
+                            />
+                        </h1>
+                        {/* <p className="lg:w-2/3 mx-auto leading-relaxed text-base text-gray-100">Security is not a product, it is a process..</p> */}
+                        <p className="lg:w-2/3 mx-auto leading-relaxed text-base text-gray-100">
+                            <Editor
+                                onInit={(evt, editor) => (editorRef.current = editor)}
+                                inline={true}
+                                key="contact1asset2"
+                                tinymceScriptSrc={
+                                    process.env.PUBLIC_URL + "/tinymce/tinymce.min.js"
+                                }
+                                initialValue="Security is not a product, it is a process.."
+                                init={{
+                                    menubar: false,
+                                    toolbar:
+                                        "bold italic underline | fontsizeselect | backcolor forecolor | alignleft aligncenter " +
+                                        "alignright alignjustify | fontselect",
+                                    fontsize_formats:
+                                        "8px 9px 10px 11px 12px 14px 16px 18px 20px 24px 30px 36px 48px 60px 72px 96px",
+                                    font_formats:
+                                        "Andale Mono=andale mono,times; Arial=arial,helvetica,sans-serif; Arial Black=arial black,avant garde; Book Antiqua=book antiqua,palatino; Comic Sans MS=comic sans ms,sans-serif; Courier New=courier new,courier; Georgia=georgia,palatino; Helvetica=helvetica; Impact=impact,chicago; Symbol=symbol; Tahoma=tahoma,arial,helvetica,sans-serif; Terminal=terminal,monaco; Times New Roman=times new roman,times; Trebuchet MS=trebuchet ms,geneva; Verdana=verdana,geneva; Webdings=webdings; Wingdings=wingdings,zapf dingbats",
+                                }}
+                            />
+                        </p>
                     </div>
-                    <div className="mt-6">
-                        <div className="w-full space-y-6">
-                            <div className="w-full">
-                                <div className=" relative ">
-                                    <input type="text" id="search-form-price" className=" rounded-lg border-transparent flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent" placeholder="Name" />
+                    <div className="lg:w-1/2 md:w-2/3 mx-auto">
+                        <div className="flex flex-wrap -m-2">
+                            <div className="p-2 w-1/2">
+                                <div className="relative">
+                                    <label for="name" className="leading-7 text-sm text-gray-100">Name</label>
+                                    <input type="text" id="name" name="name" className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-blue-500 focus:bg-white focus:ring-2 focus:ring-blue-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
                                 </div>
                             </div>
-                            <div className="w-full">
-                                <div className=" relative ">
-                                    <input type="text" id="search-form-location" className=" rounded-lg border-transparent flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent" placeholder="Email" />
+                            <div className="p-2 w-1/2">
+                                <div className="relative">
+                                    <label for="email" className="leading-7 text-sm text-gray-100">Email</label>
+                                    <input type="email" id="email" name="email" className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-blue-500 focus:bg-white focus:ring-2 focus:ring-blue-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
                                 </div>
                             </div>
-                            <div className="w-full">
-                                <div className=" relative ">
-                                    <input type="text" id="search-form-name" className=" rounded-lg border-transparent flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent" placeholder="Phone" />
+                            <div className="p-2 w-full">
+                                <div className="relative">
+                                    <label for="message" className="leading-7 text-sm text-gray-100">Message</label>
+                                    <textarea id="message" name="message" className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-blue-500 focus:bg-white focus:ring-2 focus:ring-blue-200 h-32 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"></textarea>
                                 </div>
                             </div>
-                            <div>
-                                <span className="block w-full rounded-md shadow-sm">
-                                    <button type="button" className="py-2 px-4  bg-indigo-600 hover:bg-indigo-700 focus:ring-indigo-500 focus:ring-offset-indigo-200 text-white w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2  rounded-lg ">
-                                        Submit
+                            <div className="p-2 w-full">
+                                <button className="flex mx-auto text-white bg-blue-500 border-0 py-2 px-8 focus:outline-none hover:bg-blue-600 rounded text-lg">SUBMIT</button>
+                            </div>
+                            <div className="p-2 w-full pt-8 mt-8 border-t border-gray-100 text-center">
+                                {/* <button className="text-blue-100">critrixconsultancy@gmail.com</button> */}
+                                <button className="text-blue-100">
+                                    <Editor
+                                        onInit={(evt, editor) => (editorRef.current = editor)}
+                                        inline={true}
+                                        key="contact1asset3"
+                                        tinymceScriptSrc={
+                                            process.env.PUBLIC_URL + "/tinymce/tinymce.min.js"
+                                        }
+                                        initialValue="critrixconsultancy@gmail.com"
+                                        init={{
+                                            menubar: false,
+                                            toolbar:
+                                                "bold italic underline | fontsizeselect | backcolor forecolor | alignleft aligncenter " +
+                                                "alignright alignjustify | fontselect",
+                                            fontsize_formats:
+                                                "8px 9px 10px 11px 12px 14px 16px 18px 20px 24px 30px 36px 48px 60px 72px 96px",
+                                            font_formats:
+                                                "Andale Mono=andale mono,times; Arial=arial,helvetica,sans-serif; Arial Black=arial black,avant garde; Book Antiqua=book antiqua,palatino; Comic Sans MS=comic sans ms,sans-serif; Courier New=courier new,courier; Georgia=georgia,palatino; Helvetica=helvetica; Impact=impact,chicago; Symbol=symbol; Tahoma=tahoma,arial,helvetica,sans-serif; Terminal=terminal,monaco; Times New Roman=times new roman,times; Trebuchet MS=trebuchet ms,geneva; Verdana=verdana,geneva; Webdings=webdings; Wingdings=wingdings,zapf dingbats",
+                                        }}
+                                    />
+                                </button>
+                                {/* <p className="leading-normal my-5 text-white">4010 Feeney Way Saint Cloud, MN 56301</p> */}
+                                <p className="leading-normal my-5 text-white">
+                                    <Editor
+                                    onInit={(evt, editor) => (editorRef.current = editor)}
+                                    inline={true}
+                                    key="contact1asset4"
+                                    tinymceScriptSrc={
+                                        process.env.PUBLIC_URL + "/tinymce/tinymce.min.js"
+                                    }
+                                        initialValue="4010 Feeney Way Saint Cloud, MN 56301"
+                                    init={{
+                                        menubar: false,
+                                        toolbar:
+                                            "bold italic underline | fontsizeselect | backcolor forecolor | alignleft aligncenter " +
+                                            "alignright alignjustify | fontselect",
+                                        fontsize_formats:
+                                            "8px 9px 10px 11px 12px 14px 16px 18px 20px 24px 30px 36px 48px 60px 72px 96px",
+                                        font_formats:
+                                            "Andale Mono=andale mono,times; Arial=arial,helvetica,sans-serif; Arial Black=arial black,avant garde; Book Antiqua=book antiqua,palatino; Comic Sans MS=comic sans ms,sans-serif; Courier New=courier new,courier; Georgia=georgia,palatino; Helvetica=helvetica; Impact=impact,chicago; Symbol=symbol; Tahoma=tahoma,arial,helvetica,sans-serif; Terminal=terminal,monaco; Times New Roman=times new roman,times; Trebuchet MS=trebuchet ms,geneva; Verdana=verdana,geneva; Webdings=webdings; Wingdings=wingdings,zapf dingbats",
+                                    }}
+                                    />
+                                </p>
+                                <span className="inline-flex">
+                                    <button className="text-gray-100">
+                                        <svg fill="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="w-5 h-5" viewBox="0 0 24 24">
+                                            <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z"></path>
+                                        </svg>
+                                    </button>
+                                    <button className="ml-4 text-gray-100">
+                                        <svg fill="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="w-5 h-5" viewBox="0 0 24 24">
+                                            <path d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z"></path>
+                                        </svg>
+                                    </button>
+                                    <button className="ml-4 text-gray-100">
+                                        <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="w-5 h-5" viewBox="0 0 24 24">
+                                            <rect width="20" height="20" x="2" y="2" rx="5" ry="5"></rect>
+                                            <path d="M16 11.37A4 4 0 1112.63 8 4 4 0 0116 11.37zm1.5-4.87h.01"></path>
+                                        </svg>
+                                    </button>
+                                    <button className="ml-4 text-gray-100">
+                                        <svg fill="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="w-5 h-5" viewBox="0 0 24 24">
+                                            <path d="M21 11.5a8.38 8.38 0 01-.9 3.8 8.5 8.5 0 01-7.6 4.7 8.38 8.38 0 01-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 01-.9-3.8 8.5 8.5 0 014.7-7.6 8.38 8.38 0 013.8-.9h.5a8.48 8.48 0 018 8v.5z"></path>
+                                        </svg>
                                     </button>
                                 </span>
                             </div>
                         </div>
                     </div>
                 </div>
-                <div className="px-4 py-6 border-t-2 border-gray-400 bg-black sm:px-10">
-                    <p className="text-xs leading-5 text-gray-100">
-                        Security services for your personal safety
-
-                    </p>
-                </div>
-            </div>
+            </section>
         </>
     )
 }
