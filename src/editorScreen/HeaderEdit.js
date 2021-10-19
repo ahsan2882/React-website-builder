6d18c6333834337bda63bfa5e2070f25ebb8145d
Ahsan Shahid <ahsanshahid2882@gmail.com>
2021-10-19 10:09:13 +0500

    Update HeaderEdit.js

--- src/editorScreen/HeaderEdit.js
+++ src/editorScreen/HeaderEdit.js
@@ -29,41 +29,29 @@ export default function Header({ setWidth, templateNum }) {
 
         
         <div className={`flex justify-between items-center border-b-2 border-gray-100 py-6 ${HeaderEditCss['max-w-7xl']} ${HeaderEditCss.maxHeadH} mx-auto px-4 bg-white fixed z-50 w-full top-0`}>
-        
+            <div className="w-80"></div>
             <nav>
                 <div className="relative w-32 flex justify-between">
                     <button  className="hover:bg-BL-400 rounded-full w-20 h-16" >
                         <i  className={`fas fa-desktop text-2xl`}>
-                            
                         </i>
-          
-                   
                     </button>
-                  
                         <button onClick={IncNum}  className="hover:bg-BL-400 rounded-full w-20 h-16">
                         <i className="fas fa-mobile-alt text-2xl"></i>
-                     
                     </button>
-                       
-                
-                
                 </div>
             </nav>
-         
             <div className="flex items-center justify-between w-80">
                 <button className="flex items-center px-2 py-3 hover:bg-BL-400 w-20 justify-between rounded-2xl">
-             
                     <i className="far fa-save text-2xl"></i>
                     <h2> SAVE</h2>
                 </button>
                 <Link className="flex items-center px-2 py-3 hover:bg-BL-400 w-28 justify-between rounded-2xl" target='_blank' rel='noopener noreferrer' to={path}>
                     {/* <img src={previewIm} alt="Preview" className={`w-9 h-auto`} /> */}
-
                     <i className="far fa-eye text-2xl"></i>
                     <h2>PREVIEW</h2>
                 </Link>
                 <button className="flex items-center px-2 py-3 hover:bg-BL-400 w-28 justify-between rounded-2xl">
-                
                     <i className="fas fa-file-export text-2xl"></i>
                     <h2>EXPORT</h2>
                 </button>
