50d33ba55d0e34c8a4b8fa8e4a1eba12bacf444b
Ahsan Shahid <ahsanshahid2882@gmail.com>
2021-10-20 08:29:17 +0500

    Update Contact4Asset.js

--- src/assets/editingScreenAssets/contactusEditAssets/Contact4Asset.js
+++ src/assets/editingScreenAssets/contactusEditAssets/Contact4Asset.js
@@ -1,9 +1,13 @@
 import React from 'react'
+import Contact4AssetCss from './Contact4Asset.module.css'
 
 export default function Contact4Asset() {
     return (
         <>
-            
+            <h1 className={Contact4AssetCss.firstHead}>Contact Asset 4</h1>
+            <h1 className={Contact4AssetCss.secondHead}>Please contact us</h1>
+            <h1 className={Contact4AssetCss.thirdHead}>We love you!</h1>
+            <h1 className={Contact4AssetCss.fourthHead}>Send pleasing remarks</h1>
         </>
     )
 }
