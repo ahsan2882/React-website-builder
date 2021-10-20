f5f598b42b0a2519472d655b4f12b3eb8f7df1c9
Ahsan Shahid <ahsanshahid2882@gmail.com>
2021-10-20 12:33:25 +0500

    Update Nav3Asset.js

--- src/assets/editingScreenAssets/navEditAssets/Nav3Asset.js
+++ src/assets/editingScreenAssets/navEditAssets/Nav3Asset.js
@@ -1,14 +1,14 @@
 import React from 'react'
 
 export default function Nav3Asset() {
-    return (
-        <>
+  return (
+    <>
       <header class="text-gray-600 body-font bg-red-50 mt-10 mb-10">
         <div class="container mx-auto flex flex-wrap p-2 flex-col md:flex-row items-center justify-center">
 
           <a href={2} class="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
-         
-           
+
+
           </a>
           <nav class="md:ml-auto md:mr-auto flex flex-wrap items-center text-base justify-evenly">
             <a href={2} class="mr-5 hover:text-white  p-3 bg-red-800 rounded-md text-white font-semibold tracking-wide">Home</a>
@@ -17,11 +17,11 @@ export default function Nav3Asset() {
             <a href={2} class="mr-5 hover:text-white hover:bg-red-800 p-3 rounded-md transition font-semibold text-red-800">visit</a>
           </nav>
           <a href={2}>
-        <input class=" bg-red-100 p-2 rounded-md"  type="search"/> <i class="fas fa-search text-red-800"></i>
-        Search
-      </a> 
+            <input class=" bg-red-100 p-2 rounded-md" type="search" /> <i class="fas fa-search text-red-800"></i>
+            Search
+          </a>
         </div>
       </header>
-        </>
-    )
+    </>
+  )
 }
