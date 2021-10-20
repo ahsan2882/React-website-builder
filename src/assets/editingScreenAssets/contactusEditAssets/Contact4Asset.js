e27714633d3d69b423cece96ae38222f27e58e11
Ahsan Shahid <ahsanshahid2882@gmail.com>
2021-10-20 12:29:34 +0500

    Update Contact4Asset.js

--- src/assets/editingScreenAssets/contactusEditAssets/Contact4Asset.js
+++ src/assets/editingScreenAssets/contactusEditAssets/Contact4Asset.js
@@ -4,7 +4,7 @@ import Contact4AssetCss from './Contact4Asset.module.css'
 export default function Contact4Asset() {
     return (
         <>
-            <h1 className={Contact4AssetCss.firstHead}>Contact Asset 4</h1>
+            <h1 className={Contact4AssetCss.firstHead} style={{ color: "red",fontSize: '5rem' }}>Contact Asset 4</h1>
             <h1 className={Contact4AssetCss.secondHead}>Please contact us</h1>
             <h1 className={Contact4AssetCss.thirdHead}>We love you!</h1>
             <h1 className={Contact4AssetCss.fourthHead}>Send pleasing remarks</h1>
