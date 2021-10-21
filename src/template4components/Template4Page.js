461fc9b55fc16cdd447e0828481c120020cc17a1
Ahsan Shahid <ahsanshahid2882@gmail.com>
2021-10-21 17:22:33 +0500

    Update Template4Page.js

--- src/template4components/Template4Page.js
+++ src/template4components/Template4Page.js
@@ -8,12 +8,14 @@ import OurGuards4 from './OurGuards4';
 import Blog4 from './Blog4';
 import Accreditation4 from './Accreditation4';
 import Footer4 from './Footer4';
+import Header4 from './Header4';
+import Navigation4 from './Navigation4';
 
 export default function Template4Page() {
-    var template4Components = [<HeroSection4 />, <AboutUs4 />, <Pricing4 />, <Services4 />, <OurGuards4 />, <Blog4 />, <Accreditation4 />, <Footer4 />]
+    var template4Components = [<Header4/>, <Navigation4/>, <HeroSection4 />, <AboutUs4 />, <Pricing4 />, <Services4 />, <OurGuards4 />, <Blog4 />, <Accreditation4 />, <Footer4 />]
     return (
         <>
-            {template4Components.map((item) => <>{ item }</>)}
+            {template4Components}
         </>
     );
 }
