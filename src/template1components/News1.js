10fd23731f8d0730b02b7e96771c53ffc9d6fda4
Ahsan Shahid <ahsanshahid2882@gmail.com>
2021-10-22 21:59:58 +0500

    Update News1.js

--- src/template1components/News1.js
+++ src/template1components/News1.js
@@ -1,9 +1,9 @@
 import React from 'react'
 import News1Css from './News1.module.css'
-import news1 from '../assets/template1images/news1.jpg'
-import news2 from '../assets/template1images/news2.jpg'
+import news1 from '../assets/template1images/news1.png'
+import news2 from '../assets/template1images/news2.png'
 
-import news3 from '../assets/template1images/news3.jpg'
+import news3 from '../assets/template1images/news3.png'
 
 export const News1 = () => {
     return (
