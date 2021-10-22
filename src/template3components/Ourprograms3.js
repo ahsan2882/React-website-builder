d8b358fd239bc123a05a415d85ff5b9e979d09f3
Ahsan Shahid <ahsanshahid2882@gmail.com>
2021-10-22 22:00:48 +0500

    Update Ourprograms3.js

--- src/template3components/Ourprograms3.js
+++ src/template3components/Ourprograms3.js
@@ -1,8 +1,8 @@
 import React from 'react'
 import Ourprograms3Css from './Ourprograms.module.css'
-import news1 from '../assets/template3images/news1.jpg'
-import news2 from '../assets/template3images/news2.jpg'
-import news3 from '../assets/template3images/news3.jpg'
+import news1 from '../assets/template3images/news1.png'
+import news2 from '../assets/template3images/news2.png'
+import news3 from '../assets/template3images/news3.png'
 export default function Ourprograms3() {
     return (
         <>
