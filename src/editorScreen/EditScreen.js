82b7279fd6359844aec9f6fb09787a2a2bf95d3a
Ahsan Shahid <ahsanshahid2882@gmail.com>
2021-10-23 20:32:08 +0500

    Update EditScreen.js

--- src/editorScreen/EditScreen.js
+++ src/editorScreen/EditScreen.js
@@ -1,11 +1,9 @@
-import React, { useState, createContext } from 'react'
+import React, { useState } from 'react'
 import HeaderEdit from './HeaderEdit'
 import ContainerEdit from './ContainerEdit'
 import FooterEdit from './FooterEdit'
 import { DndProvider } from 'react-dnd';
 import { HTML5Backend } from 'react-dnd-html5-backend'
-
-export const NewAssetContext = createContext({ newAssetState: null, setNewAssetState: (() => void 0), newAssetCount: 0, setNewAssetCount: (() => void 0) });
 
 export default function EditScreen({ temp }) {
     const [assetSelected, setAssetSelected] = useState(false);
@@ -14,16 +12,9 @@ export default function EditScreen({ temp }) {
         templateData: null
     });
     const [saveClicked, setSaveClicked] = useState(false);
-    // const [newAssetState, setNewAssetState] = useState(null);
-    // const [newAssetCount, setNewAssetCount] = useState(0);
 
     return (
         <>
-            {/* <NewAssetContext.Provider value={{newAssetState, newAssetCount, setNewAssetState, setNewAssetCount}}>
-                <HeaderEdit setWidth={setWidth} />
-                <ContainerEdit defWidth={defWidth} templateNum={temp} overlayPresent={assetSelected} />
-                <FooterEdit setOverlay={setAssetSelected} />
-            </NewAssetContext.Provider> */}
             <DndProvider backend={HTML5Backend}>
                 <HeaderEdit
                     templateNum={temp}
