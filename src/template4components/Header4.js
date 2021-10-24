b3f3ed26d109cdd9124da9c2d08deb1f7c5b81a3
Ahsan Shahid <ahsanshahid2882@gmail.com>
2021-10-24 17:56:54 +0500

    Update Header4.js

--- src/template4components/Header4.js
+++ src/template4components/Header4.js
@@ -17,7 +17,7 @@ export default function Header4() {
                             <i className="fas fa-phone-alt"></i>
                         </div>
                         {/* <DivText className="mr-1" text="+10800-33-000"></DivText> */}
-                        <div>
+                        <p>
                             <Editor
                                 onInit={(evt, editor) => editorRef.current = editor}
                                 inline={true}
@@ -34,7 +34,7 @@ export default function Header4() {
 
                                 }}
                             />
-                        </div>
+                        </p>
                     </li>
                     <li className={`${Header4Css.HeaderLinks} flex justify-around items-center`}>
                         <div className="mr-1">
@@ -42,7 +42,7 @@ export default function Header4() {
                         </div>
                         {/* <div className="mr-1">security</div> */}
                         {/* <DivText className="mr-1" text="security"></DivText> */}
-                        <div>
+                        <p>
                             <Editor
                                 onInit={(evt, editor) => editorRef.current = editor}
                                 inline={true}
@@ -59,7 +59,7 @@ export default function Header4() {
 
                                 }}
                             />
-                        </div>
+                        </p>
                     </li>
                     <li className={`${Header4Css.HeaderLinks} flex justify-around items-center`}>
                         <div className="mr-1">
@@ -67,24 +67,24 @@ export default function Header4() {
                         </div>
                         {/* <div className="mr-1">21 Newstreet, New York, 269-24</div> */}
                         {/* <DivText className="mr-1" text="21 Newstreet, New York, 269-24"></DivText> */}
-                        <div>
-                        <Editor
-                            onInit={(evt, editor) => editorRef.current = editor}
-                            inline={true}
-                            key='headed3'
-                            tinymceScriptSrc={process.env.PUBLIC_URL + '/tinymce/tinymce.min.js'}
-                            initialValue="21 Newstreet, New York, 269-24"
-                            init={{
-                                forced_root_block: "",
-                                menubar: false,
-                                toolbar: 'bold italic underline | fontsizeselect | backcolor forecolor | alignleft aligncenter ' +
-                                    'alignright alignjustify | fontselect',
-                                fontsize_formats: "8px 9px 10px 11px 12px 14px 16px 18px 20px 24px 30px 36px 48px 60px 72px 96px",
-                                font_formats: "Andale Mono=andale mono,times; Arial=arial,helvetica,sans-serif; Arial Black=arial black,avant garde; Book Antiqua=book antiqua,palatino; Comic Sans MS=comic sans ms,sans-serif; Courier New=courier new,courier; Georgia=georgia,palatino; Helvetica=helvetica; Impact=impact,chicago; Symbol=symbol; Tahoma=tahoma,arial,helvetica,sans-serif; Terminal=terminal,monaco; Times New Roman=times new roman,times; Trebuchet MS=trebuchet ms,geneva; Verdana=verdana,geneva; Webdings=webdings; Wingdings=wingdings,zapf dingbats"
+                        <p>
+                            <Editor
+                                onInit={(evt, editor) => editorRef.current = editor}
+                                inline={true}
+                                key='headed3'
+                                tinymceScriptSrc={process.env.PUBLIC_URL + '/tinymce/tinymce.min.js'}
+                                initialValue="21 Newstreet, New York, 269-24"
+                                init={{
+                                    forced_root_block: "",
+                                    menubar: false,
+                                    toolbar: 'bold italic underline | fontsizeselect | backcolor forecolor | alignleft aligncenter ' +
+                                        'alignright alignjustify | fontselect',
+                                    fontsize_formats: "8px 9px 10px 11px 12px 14px 16px 18px 20px 24px 30px 36px 48px 60px 72px 96px",
+                                    font_formats: "Andale Mono=andale mono,times; Arial=arial,helvetica,sans-serif; Arial Black=arial black,avant garde; Book Antiqua=book antiqua,palatino; Comic Sans MS=comic sans ms,sans-serif; Courier New=courier new,courier; Georgia=georgia,palatino; Helvetica=helvetica; Impact=impact,chicago; Symbol=symbol; Tahoma=tahoma,arial,helvetica,sans-serif; Terminal=terminal,monaco; Times New Roman=times new roman,times; Trebuchet MS=trebuchet ms,geneva; Verdana=verdana,geneva; Webdings=webdings; Wingdings=wingdings,zapf dingbats"
 
-                            }}
-                        />
-                        </div>
+                                }}
+                            />
+                        </p>
                     </li>
                 </ul>
                 <ul className="list-none flex p-0 justify-between items-center w-72">
