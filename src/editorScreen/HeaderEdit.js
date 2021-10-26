16bbad680f6e9f64777e94b45241b1f161ff4cc5
Usama Khalied <usamakhalied@gmail.com>
2021-10-26 21:11:38 +0500

    Make a slider

--- src/editorScreen/HeaderEdit.js
+++ src/editorScreen/HeaderEdit.js
@@ -1,8 +1,13 @@
-import React from 'react'
+import React, { useState } from 'react'
 import HeaderEditCss from './HeaderEdit.module.css'
 import { Link } from 'react-router-dom';
 
 export default function Header({ templateNum, setSaveClicked, toSave, saveClicked }) {
+const [isClicked , setIsClicked] = useState(false);
+const onClicked = () => setIsClicked ((condition) => !condition);
+
+
+
     let path = '';
     if (templateNum === 1) {
         path = '/preview/template-1'
@@ -37,13 +42,41 @@ export default function Header({ templateNum, setSaveClicked, toSave, saveClicke
             .then(response => response.json())
             .catch(error => console.log(error))
     }
+   
     return (
+        <>
+
         <div className={`flex justify-between items-center border-b-2 border-gray-100 py-6 ${HeaderEditCss['max-w-7xl']} ${HeaderEditCss.maxHeadH} mx-auto px-4 bg-white fixed z-50 w-full top-0`}>
-            <div className="w-80">
-           <button>
-      Pages
-            </button>
+
+            <div className="w-80 ml-4">
+                {/* ---usama */}
+            <div className="list">
+                <h1 style={{background:`rgb(60,172,254)`,width:`120px`,borderRadius:`25px`,color:`white`,cursor:`pointer`}} onClick={onClicked}> {isClicked ? <h2 style={{background:`red`,padding:`12px 30px 12px`,borderRadius:`25px`}}> Close </h2> : <h2 style={{padding:`12px 30px 12px`}}> Pages </h2>} </h1>
+<div className="w-48" style={ isClicked ? { left: `0`,position:`absolute`, top:`100%`} : {position:`absolute`, top:`100%`,left:`-10%`, height:`100vh`,background:`white`,boxShadow:`1px 2px 2px 2px rgba(0,0,0,0.3)`}}>
+   <div className="flex flex-col mt-12">
+       <h5 className="m-10"> Home </h5>
+       <h5 className="m-10"> Contact </h5>
+       <h5 className="m-10"> About  </h5>
+       <h5 className="m-10"> Service </h5>
+     
+
+
+   </div>
+</div>
+            
+
+
+
+
+            </div>
+       
+         
                  </div>
+
+
+  
+
+{/* Usama */}
             <nav>
                 <div className="relative w-32 flex justify-between">
                     <button className="hover:bg-BL-400 rounded-full w-20 h-16" >
@@ -77,5 +110,6 @@ export default function Header({ templateNum, setSaveClicked, toSave, saveClicke
             </div>
         </div>
 
+</>
     );
 }
