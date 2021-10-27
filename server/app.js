7a54b84a6fbe8be457e156a0fd13f44c7330f43c
Ahsan Shahid <ahsanshahid2882@gmail.com>
2021-10-27 14:51:44 +0500

    Update app.js

--- server/app.js
+++ server/app.js
@@ -8,6 +8,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var generateFileRouter = require('./routes/generateFile')
 var imageUploader = require('./routes/uploadImages')
+var zipGenerator = require('./routes/generateZip')
 var app = express();
 
 // view engine setup
@@ -25,6 +26,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/generate-file', generateFileRouter)
 app.use('/upload-images', imageUploader)
+app.use('/download-zip', zipGenerator)
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
