9ab3eb4ad592d6e09c0166a0228b2592afc338a7
Ahsan Shahid <ahsanshahid2882@gmail.com>
2021-10-27 08:29:20 +0500

    Update app.js

--- server/app.js
+++ server/app.js
@@ -7,6 +7,7 @@ var cors = require('cors')
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var generateFileRouter = require('./routes/generateFile')
+var imageUploader = require('./routes/uploadImages')
 var app = express();
 
 // view engine setup
@@ -23,6 +24,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/generate-file', generateFileRouter)
+app.use('/upload-images', imageUploader)
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
