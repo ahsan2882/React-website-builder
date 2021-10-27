9ab97461955302d0dfd5f0822f855ab318efb86e
Ahsan Shahid <ahsanshahid2882@gmail.com>
2021-10-27 08:29:31 +0500

    Create uploadImages.js

---
+++ server/routes/uploadImages.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const multer = require('multer');
+const path = require('path');
+var router = express.Router();
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, 'uploads/');
+    },
+
+    // By default, multer removes file extensions so let's add them back
+    filename: function (req, file, cb) {
+        cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname));
+    }
+});
+
+router.post("/", (req, res, next) => {
+    let upload = multer({ storage: storage, fileFilter: imageFilter }).single('profile_pic');
+    upload(req, res, function (err) {
+
+        if (req.fileValidationError) {
+            return res.send(req.fileValidationError);
+        }
+        else if (!req.file) {
+            return res.send('Please select an image to upload');
+        }
+        else if (err instanceof multer.MulterError) {
+            return res.send(err);
+        }
+        else if (err) {
+            return res.send(err);
+        }
+
+        // Display uploaded image for user validation
+        res.send(`You have uploaded this image: <hr/><img src="${req.file.path}" width="500"><hr /><a href="./">Upload another image</a>`);
+    });
+})
+
+const imageFilter = function (req, file, cb) {
+    // Accept images only
+    if (!file.originalname.match(/\.(jpg|JPG|jpeg|JPEG|png|PNG|gif|GIF)$/)) {
+        req.fileValidationError = 'Only image files are allowed!';
+        return cb(new Error('Only image files are allowed!'), false);
+    }
+    cb(null, true);
+};
+module.exports = router;
