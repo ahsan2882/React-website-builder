5fadd06e69aa0da50db158e27920249677d07c80
Ahsan Shahid <ahsanshahid2882@gmail.com>
2021-10-27 08:29:28 +0500

    Update generateFile.js

--- server/routes/generateFile.js
+++ server/routes/generateFile.js
@@ -2,7 +2,7 @@ var express = require('express'),
     fs = require('fs');
 var router = express.Router();
 
-var htmlTemplate = "<!DOCTYPE html><html lang=\"en\"><head><meta charset=\"UTF-8\"><meta http-equiv=\"X-UA-Compatible\" content=\"IE=edge\"><meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\"><link href=\"https://unpkg.com/tailwindcss@^1.0/dist/tailwind.min.css\" rel=\"stylesheet\"><title>Document</title></head><body><main>"
+var htmlTemplate = "<!DOCTYPE html><html lang=\"en\"><head><meta charset=\"UTF-8\"><meta http-equiv=\"X-UA-Compatible\" content=\"IE=edge\"><meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\"><link rel=\"stylesheet\" href=\"https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.7.2/animate.min.css\"><link href=\"https://unpkg.com/tailwindcss@^1.0/dist/tailwind.min.css\" rel=\"stylesheet\"><title>Document</title></head><body><script src=\"https://kit.fontawesome.com/6fcb08b902.js\" crossorigin=\"anonymous\"></script><script src=\"https://cdn.lordicon.com/libs/mssddfmo/lord-icon-2.1.0.js\"></script><main>"
 var htmlClosingTemplate = "</main></body></html>"
 
 router.post("/",  (req, res, next) => {
