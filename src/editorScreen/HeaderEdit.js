5d50f6fd82af5549b15c90fb4587255e457bd8ef
Ahsan Shahid <ahsanshahid2882@gmail.com>
2021-10-27 14:51:57 +0500

    Update HeaderEdit.js

--- src/editorScreen/HeaderEdit.js
+++ src/editorScreen/HeaderEdit.js
@@ -1,13 +1,13 @@
 import React, { useState } from 'react'
 import HeaderEditCss from './HeaderEdit.module.css'
 import { Link } from 'react-router-dom';
+// import b64toBlob from 'b64-to-blob';
+// import FileSaver from 'file-saver';
 
 
 export default function Header({ templateNum, setSaveClicked, toSave, saveClicked, fileData }) {
-const [isClicked , setIsClicked] = useState(false);
-const onClicked = () => setIsClicked ((condition) => !condition);
-
-
+    const [isClicked, setIsClicked] = useState(false);
+    const onClicked = () => setIsClicked((condition) => !condition);
 
     let path = '';
     if (templateNum === 1) {
@@ -41,87 +41,88 @@ const onClicked = () => setIsClicked ((condition) => !condition);
         };
         fetch('/generate-file', requestOptions)
             .then(response => console.log(response.blob()))
-            .then((blob) => {
-                const url = window.URL.createObjectURL(
-                    new Blob([blob]),
-                );
-                const link = document.createElement('a');
-                link.href = url;
-
-                document.body.appendChild(link);
-                link.click();
-                link.parentNode.removeChild(link);
-            })
+            // .then(fetch('/download-zip')
+            //     .then((response) => {
+            //         return response.text();
+            //     })
+            //     .then((zipAsBase64) => {
+            //         // const blob = b64toBlob(zipAsBase64, "application/zip");
+            //         // FileSaver.saveAs(blob, `example.zip`);
+            //         let contentType = 'application/zip'
+            //         let data = zipAsBase64
+            //         var blob = b64toBlob(data, contentType);
+            //         var blobUrl = URL.createObjectURL(blob);
+            //         window.location = blobUrl;
+            //     }))
             .catch(error => console.log(error))
     }
-   
+
     return (
         <>
 
-        <div className={`flex justify-between items-center border-b-2 border-gray-100 py-6 ${HeaderEditCss['max-w-7xl']} ${HeaderEditCss.maxHeadH} mx-auto px-4 bg-white fixed z-50 w-full top-0`}>
+            <div className={`flex justify-between items-center border-b-2 border-gray-100 py-6 ${HeaderEditCss['max-w-7xl']} ${HeaderEditCss.maxHeadH} mx-auto px-4 bg-white fixed z-50 w-full top-0`}>
 
-            <div className="w-80 ml-4">
-                {/* ---usama */}
-            <div className="list">
-                <h1 style={{background:`rgb(60,172,254)`,width:`120px`,borderRadius:`25px`,color:`white`,cursor:`pointer`}} onClick={onClicked}> {isClicked ? <h2 style={{background:`red`,padding:`12px 30px 12px`,borderRadius:`25px`}}> Close </h2> : <h2 style={{padding:`12px 30px 12px`}}> Pages </h2>} </h1>
-<div className="w-48" style={ isClicked ? { left: `0`,position:`absolute`, top:`100%`} : {position:`absolute`, top:`100%`,left:`-10%`, height:`100vh`,background:`white`,boxShadow:`1px 2px 2px 2px rgba(0,0,0,0.3)`}}>
-   <div className="flex flex-col mt-12">
-       <h5 className="m-10"> Home </h5>
-       <h5 className="m-10"> Contact </h5>
-       <h5 className="m-10"> About  </h5>
-       <h5 className="m-10"> Service </h5>
-     
+                <div className="w-80 ml-4">
+                    <div className="list">
+                        <h1 style={{ background: `rgb(60,172,254)`, width: `120px`, borderRadius: `25px`, color: `white`, cursor: `pointer` }} onClick={onClicked}> {isClicked ? <h2 style={{ background: `red`, padding: `12px 30px 12px`, borderRadius: `25px` }}> Close </h2> : <h2 style={{ padding: `12px 30px 12px` }}> Pages </h2>} </h1>
+                        <div className="w-48" style={isClicked ? { left: `0`, position: `absolute`, top: `100%` } : { position: `absolute`, top: `100%`, left: `-10%`, height: `100vh`, background: `white`, boxShadow: `1px 2px 2px 2px rgba(0,0,0,0.3)` }}>
+                            <div className="flex flex-col mt-12">
+                                <h5 className="m-10"> Home </h5>
+                                <h5 className="m-10"> Contact </h5>
+                                <h5 className="m-10"> About  </h5>
+                                <h5 className="m-10"> Service </h5>
 
 
-   </div>
-</div>
-            
+
+                            </div>
+                        </div>
 
 
 
 
-            </div>
-       
-         
-                 </div>
+
+                    </div>
 
 
-  
+                </div>
 
-{/* Usama */}
-            <nav>
-                <div className="relative w-32 flex justify-between">
-                    <button className="hover:bg-BL-400 rounded-full w-20 h-16" >
-                        <i className={`fas fa-desktop text-2xl`}>
-                        </i>
+
+
+
+                {/* Usama */}
+                <nav>
+                    <div className="relative w-32 flex justify-between">
+                        <button className="hover:bg-BL-400 rounded-full w-20 h-16" >
+                            <i className={`fas fa-desktop text-2xl`}>
+                            </i>
+                        </button>
+                        <button className="hover:bg-BL-400 rounded-full w-20 h-16">
+                            <i className="fas fa-mobile-alt text-2xl"></i>
+                        </button>
+                    </div>
+                </nav>
+                <div className="flex items-center justify-between w-80">
+                    <button className="flex items-center px-2 py-3 hover:bg-BL-400 w-20 justify-between rounded-2xl"
+                        onClick={() => {
+                            saveTemplate();
+                        }}
+                    >
+                        <i className="far fa-save text-2xl"></i>
+                        <h2> SAVE</h2>
+                        <i className={saveClicked ? "fas fa-check" : null}></i>
                     </button>
-                    <button className="hover:bg-BL-400 rounded-full w-20 h-16">
-                        <i className="fas fa-mobile-alt text-2xl"></i>
+                    <Link to={path} target='_blank' rel='noopener noreferrer' className="flex items-center px-2 py-3 hover:bg-BL-400 w-28 justify-between rounded-2xl">
+                        {/* <img src={previewIm} alt="Preview" className={`w-9 h-auto`} /> */}
+                        <i className="far fa-eye text-2xl"></i>
+                        <h2>PREVIEW</h2>
+                    </Link>
+                    <button className="flex items-center px-2 py-3 hover:bg-BL-400 w-28 justify-between rounded-2xl" onClick={() => exportCode()}>
+                        <i className="fas fa-file-export text-2xl"></i>
+                        <h2>EXPORT</h2>
                     </button>
                 </div>
-            </nav>
-            <div className="flex items-center justify-between w-80">
-                <button className="flex items-center px-2 py-3 hover:bg-BL-400 w-20 justify-between rounded-2xl"
-                    onClick={() => {
-                        saveTemplate();
-                    }}
-                >
-                    <i className="far fa-save text-2xl"></i>
-                    <h2> SAVE</h2>
-                    <i className={saveClicked ? "fas fa-check" : null}></i>
-                </button>
-                <Link to={path} target='_blank' rel='noopener noreferrer' className="flex items-center px-2 py-3 hover:bg-BL-400 w-28 justify-between rounded-2xl">
-                    {/* <img src={previewIm} alt="Preview" className={`w-9 h-auto`} /> */}
-                    <i className="far fa-eye text-2xl"></i>
-                    <h2>PREVIEW</h2>
-                </Link>
-                <button className="flex items-center px-2 py-3 hover:bg-BL-400 w-28 justify-between rounded-2xl" onClick={() => exportCode()}>
-                    <i className="fas fa-file-export text-2xl"></i>
-                    <h2>EXPORT</h2>
-                </button>
             </div>
-        </div>
 
-</>
+        </>
     );
 }
