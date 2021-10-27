e5c86a25933668b2cb937f2441e6a18f8f2cc39d
Ahsan Shahid <ahsanshahid2882@gmail.com>
2021-10-27 09:51:27 +0500

    Update uploadImages.js

--- server/routes/uploadImages.js
+++ server/routes/uploadImages.js
@@ -7,8 +7,6 @@ const storage = multer.diskStorage({
     destination: function (req, file, cb) {
         cb(null, 'uploads/');
     },
-
-    // By default, multer removes file extensions so let's add them back
     filename: function (req, file, cb) {
         cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname));
     }
@@ -30,14 +28,10 @@ router.post("/", (req, res, next) => {
         else if (err) {
             return res.send(err);
         }
-
-        // Display uploaded image for user validation
-        res.send(`You have uploaded this image: <hr/><img src="${req.file.path}" width="500"><hr /><a href="./">Upload another image</a>`);
     });
 })
 
 const imageFilter = function (req, file, cb) {
-    // Accept images only
     if (!file.originalname.match(/\.(jpg|JPG|jpeg|JPEG|png|PNG|gif|GIF)$/)) {
         req.fileValidationError = 'Only image files are allowed!';
         return cb(new Error('Only image files are allowed!'), false);
