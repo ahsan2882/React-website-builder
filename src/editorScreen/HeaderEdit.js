5a0569c9d0386717499f40e7e6e143b1ce7a615e
Ahsan Shahid <ahsanshahid2882@gmail.com>
2021-10-28 09:29:09 +0500

    Update HeaderEdit.js

--- src/editorScreen/HeaderEdit.js
+++ src/editorScreen/HeaderEdit.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 import subPages from './TemplateSubPages';
 import FileSaver from 'file-saver';
 
-export default function Header({ templateNum, setSaveClicked, toSave, saveClicked, fileData, setTemplateSubPage }) {
+export default function Header({ templateNum, setSaveClicked, toSave, saveClicked, fileData, setTemplateSubPage, setDisplayDevice }) {
     const [isClicked, setIsClicked] = useState(false);
     const onClicked = () => setIsClicked((condition) => !condition);
     const [subPageList, setSubPageList] = useState([]);
@@ -61,7 +61,7 @@ export default function Header({ templateNum, setSaveClicked, toSave, saveClicke
         <>
 
             <div className={`flex justify-between items-center border-b-2 border-gray-100 py-6 ${HeaderEditCss['max-w-7xl']} ${HeaderEditCss.maxHeadH} mx-auto px-16 bg-black fixed z-50 w-full top-0`}>
-                <div className="w-80 ml-4">
+                <div className="w-96 ml-4">
                     <div className="list">
                         <div style={{ width: `110px`, cursor: `pointer` }} onClick={onClicked}> {isClicked ? <h2 style={{ background: `red`, padding: `12px 30px`, borderRadius: `5px` }}> Close </h2> : <h2 style={{ padding: `12px 30px`, borderRadius: `5px`, color: `white`, background: `rgb(60,172,254)`}}> Pages </h2>} </div>
                         <div className="w-48" style={isClicked ? { left: `0`, position: `absolute`, top: `100%`, transition: `0.25s all ease`, background: `white`, height: `100vh` } : { position: `absolute`, top: `100%`, left: `-10%`, height: `100vh`, background: `white`, boxShadow: `1px 4px 2px 2px rgba(0,0,0,0.3)`, transition: `0.25s all ease` }}>
@@ -73,32 +73,29 @@ export default function Header({ templateNum, setSaveClicked, toSave, saveClicke
                 </div>
                 <nav>
                     <div className="relative w-32 flex justify-between">
-                        <button className="hover:bg-BL-400 rounded-full w-20 h-16 text-white" >
+                        <button className="hover:bg-BL-400 rounded-full w-20 h-16 text-white" onClick={() => setDisplayDevice(true)}>
                             <i className={`fas fa-desktop text-2xl`}>
                             </i>
                         </button>
-                        <button className="hover:bg-BL-400 rounded-full w-20 h-16 text-white">
+                        <button className="hover:bg-BL-400 rounded-full w-20 h-16 text-white" onClick={() => setDisplayDevice(false)}>
                             <i className="fas fa-mobile-alt text-2xl"></i>
                         </button>
                     </div>
                 </nav>
-                <div className="flex items-center justify-between w-80">
-                    <button className="flex items-center px-2 py-3 hover:bg-BL-400 w-20 justify-between rounded-2xl text-white"
+                <div className="flex items-center justify-between w-96">
+                    <button className={`flex items-center px-2 py-3 w-28 justify-center pl-6 rounded-2xl text-white ${HeaderEditCss.sideButton}`}
                         onClick={() => {
                             saveTemplate();
                         }}
                     >
-                        <i className="far fa-save text-2xl"></i>
                         <h2> SAVE</h2>
-                        <i className={saveClicked ? "fas fa-check" : null}></i>
+                        <i className={saveClicked ? "fas fa-check" : "fas fa-check text-black"}></i>
                     </button>
-                    <Link to={newPath} target='_blank' rel='noopener noreferrer' className="flex items-center px-2 py-3 text-white hover:bg-BL-400 w-28 justify-between rounded-2xl">
+                    <Link to={newPath} target='_blank' rel='noopener noreferrer' className={`${HeaderEditCss.sideButton} flex items-center px-2 py-3 text-white w-28 justify-center rounded-2xl`}>
                         {/* <img src={previewIm} alt="Preview" className={`w-9 h-auto`} /> */}
-                        <i className="far fa-eye text-2xl"></i>
                         <h2>PREVIEW</h2>
                     </Link>
-                    <button className="flex items-center px-2 py-3 hover:bg-BL-400 text-white w-28 justify-between rounded-2xl" onClick={() => exportCode()}>
-                        <i className="fas fa-file-export text-2xl"></i>
+                    <button className={`${HeaderEditCss.sideButton} flex items-center px-2 py-3 text-white w-28 justify-center rounded-2xl`} onClick={() => exportCode()}>
                         <h2>EXPORT</h2>
                     </button>
                 </div>
