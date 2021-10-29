c4e921e27061b6cad1bb5d1f4cc9d60c8c44a4f4
Ahsan Shahid <ahsanshahid2882@gmail.com>
2021-10-29 18:56:03 +0500

    Update Pricing2.js

--- src/template2components/Pricing2.js
+++ src/template2components/Pricing2.js
@@ -7,8 +7,22 @@ export const Pricing2 = () => {
   return (
     <>
       <section className={Pricing2Css['head-main']}>
-        <div className={Pricing2Css['MainHeading']}><span className={Pricing2Css['SpanColor']}> OUR </span> pricing table</div>
-        <div className={Pricing2Css['spanDesc']}>
+      <h1 class={`${Pricing2Css.MainHeading}`}> 
+<Editor
+                            onInit={(evt, editor) => editorRef.current = editor}
+                            inline={true}
+                            key='About(2)48'
+                            tinymceScriptSrc={process.env.PUBLIC_URL + '/tinymce/tinymce.min.js'}
+                            initialValue={`<span class="${Pricing2Css.SpanColor}">Our </span> Services`}
+                            init={{
+                                forced_root_block: "",
+                                menubar: false,
+                                toolbar: 'bold italic underline | fontsizeselect | backcolor forecolor | alignleft aligncenter ' +
+                                    'alignright alignjustify | fontselect',
+                                fontsize_formats: "8px 9px 10px 11px 12px 14px 16px 18px 20px 24px 30px 36px 48px 60px 72px 96px"
+                            }}
+                        />   
+                        </h1>     <div className={Pricing2Css['spanDesc']}>
           <Editor
             onInit={(evt, editor) => editorRef.current = editor}
             inline={true}
