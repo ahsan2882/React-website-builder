0b0a45eb128ce7902b46b0b592aaa6f8e22a8768
Ahsan Shahid <ahsanshahid2882@gmail.com>
2021-10-30 09:58:01 +0500

    Update Hero4Asset.js

--- src/assets/editingScreenAssets/headerEditAssets/Hero4Asset.js
+++ src/assets/editingScreenAssets/headerEditAssets/Hero4Asset.js
@@ -4,7 +4,7 @@ export default function Hero4Asset() {
     return (
         <>
             <section className="relative" style={{width: "96rem", height: "50rem"}}>
-                
+                <img src="https://i.ibb.co/kqLdps8/news2.png" alt="news2" border="0"></img>
             </section>
         </>
     )
