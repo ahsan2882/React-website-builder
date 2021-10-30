3f182d7887bf14139db09faffee1d121dd4ec069
Ahsan Shahid <ahsanshahid2882@gmail.com>
2021-10-30 17:26:37 +0500

    Update About2policy.js

--- src/template2components/template2aboutcomponent/About2policy.js
+++ src/template2components/template2aboutcomponent/About2policy.js
@@ -1,6 +1,5 @@
 import React, { useRef } from 'react'
 import AboutpolicyCss from './About2policy.module.css'
-import Img1 from '../template2aboutcomponent/news3.jpg'
 import { Editor } from '@tinymce/tinymce-react';
 
 export default function About2policy() {
@@ -9,7 +8,24 @@ export default function About2policy() {
     <>
 
       <section className={`${AboutpolicyCss['policies']} ${AboutpolicyCss['head-main']}`}>
-        <div className={AboutpolicyCss['MainHeading']}><span className={AboutpolicyCss['SpanColor']}> OUR </span> Policy</div>
+        <div className={AboutpolicyCss['MainHeading']}>
+        <Editor
+
+onInit={(evt, editor) => editorRef.current = editor}
+inline={true}
+key='Service1'
+initialValue={` <span className=${AboutpolicyCss.SpanColor}> OUR </span> Policy`}
+init={{
+    forced_root_block: "",
+    menubar: false,
+    toolbar: 'bold italic underline | fontsizeselect | backcolor forecolor | alignleft aligncenter ' +
+        'alignright alignjustify | fontselect',
+    fontsize_formats: "8px 9px 10px 11px 12px 14px 16px 18px 20px 24px 30px 36px 48px 60px 72px 96px",
+    font_formats: "Andale Mono=andale mono,times; Arial=arial,helvetica,sans-serif; Arial Black=arial black,avant garde; Book Antiqua=book antiqua,palatino; Comic Sans MS=comic sans ms,sans-serif; Courier New=courier new,courier; Georgia=georgia,palatino; Helvetica=helvetica; Impact=impact,chicago; Symbol=symbol; Tahoma=tahoma,arial,helvetica,sans-serif; Terminal=terminal,monaco; Times New Roman=times new roman,times; Trebuchet MS=trebuchet ms,geneva; Verdana=verdana,geneva; Webdings=webdings; Wingdings=wingdings,zapf dingbats"
+}}
+/>
+          
+          </div>
         <div className={AboutpolicyCss['spanDesc']}>
           <Editor
             onInit={(evt, editor) => editorRef.current = editor}
@@ -167,7 +183,7 @@ export default function About2policy() {
             onInit={(evt, editor) => editorRef.current = editor}
             inline={true}
             key='Feeback7'
-            initialValue={` <img src=${Img1} alt="news3" />`}
+                initialValue={` <img src="https://i.ibb.co/6XHDXXN/news3.jpg" alt="news3" />`}
             init={
               {
               images_upload_handler: function (blobInfo, success, failure) {
