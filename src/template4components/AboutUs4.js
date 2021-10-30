c8dd212f747b81764c29c1c1068886e66cde6be3
Ahsan Shahid <ahsanshahid2882@gmail.com>
2021-10-30 07:16:16 +0500

    Update AboutUs4.js

--- src/template4components/AboutUs4.js
+++ src/template4components/AboutUs4.js
@@ -1,4 +1,4 @@
-import React, { useRef ,useState } from 'react'
+import React, { useRef, useState } from 'react'
 import About4Css from './AboutUs4.module.css'
 import { GithubPicker } from 'react-color';
 import { Editor } from '@tinymce/tinymce-react';
@@ -9,64 +9,70 @@ export default function AboutUs4({ showPopup, overSection, displayDevice }) {
     return (
         <>
             <div className={displayDevice ? `${About4Css.container02}` : `${About4Css.container02M}`} style={{ background: currentBackground }}>
-                
-                    {/* <img className={`${About4Css.aboutImg} w-11/12`} src={aboutimg} alt="about" /> */}
-                    <Editor
-                        onInit={(evt, editor) => editorRef.current = editor}
-                        inline={true}
-                        key='abouted1'
-                        initialValue={`<img  src="https://i.ibb.co/c8STtRy/news3.png" alt="about" class="${About4Css.aboutImg} w-96 h-72 object-cover"/>`}
-                        init={{
-                            forced_root_block:"",
-                            menubar: false,
-                            images_upload_handler: function (blobInfo, success, failure) {
-                                success("data:" + blobInfo.blob().type + ";base64," + blobInfo.base64());
-                            },
-                            images_upload_url: '/upload-images',
-                            plugins: [
-                                "image"
-                            ],
-                            toolbar: 'image'
-                        }}
-                    />
+
+                {/* <img className={`${About4Css.aboutImg} w-11/12`} src={aboutimg} alt="about" /> */}
+                <Editor
+                    onInit={(evt, editor) => editorRef.current = editor}
+                    inline={true}
+                    key='abouted1'
+                    initialValue={`<img  src="https://i.ibb.co/c8STtRy/news3.png" alt="about" class="${About4Css.aboutImg} w-96 h-72 object-cover"/>`}
+                    init={{
+                        forced_root_block: "",
+                        menubar: false,
+                        images_upload_handler: function (blobInfo, success, failure) {
+                            success("data:" + blobInfo.blob().type + ";base64," + blobInfo.base64());
+                        },
+                        images_upload_url: '/upload-images',
+                        plugins: [
+                            "image"
+                        ],
+                        toolbar: 'image'
+                    }}
+                />
                 <div className={displayDevice ? `${About4Css.box1}` : `${About4Css.box1M}`}>
                     <section>
                         <article>
                             <div className={About4Css.editableAbout}>
                                 <h6 className="About4Css">
-                                <Editor
-                                    onInit={(evt, editor) => editorRef.current = editor}
-                                    inline={true}
-                                    key='abouted1'
-                                    initialValue="About Us"
-                                    init={{
-                                        forced_root_block:"",
-                                        menubar: false,
-                                        toolbar: 'bold italic underline | fontsizeselect | backcolor forecolor | alignleft aligncenter ' +
-                                            'alignright alignjustify | fontselect',
-                                        fontsize_formats: "8px 9px 10px 11px 12px 14px 16px 18px 20px 24px 30px 36px 48px 60px 72px 96px",
-                                        font_formats: "Andale Mono=andale mono,times; Arial=arial,helvetica,sans-serif; Arial Black=arial black,avant garde; Book Antiqua=book antiqua,palatino; Comic Sans MS=comic sans ms,sans-serif; Courier New=courier new,courier; Georgia=georgia,palatino; Helvetica=helvetica; Impact=impact,chicago; Symbol=symbol; Tahoma=tahoma,arial,helvetica,sans-serif; Terminal=terminal,monaco; Times New Roman=times new roman,times; Trebuchet MS=trebuchet ms,geneva; Verdana=verdana,geneva; Webdings=webdings; Wingdings=wingdings,zapf dingbats"
-                                    }}
-                                />
+                                    <Editor
+                                        onInit={(evt, editor) => editorRef.current = editor}
+                                        inline={true}
+                                        key='abouted1'
+                                        initialValue="About Us"
+                                        init={{
+                                            plugins: [
+                                                "link"
+                                            ],
+                                            forced_root_block: "",
+                                            menubar: false,
+                                            toolbar: 'bold italic underline | link | fontsizeselect | backcolor forecolor | alignleft aligncenter ' +
+                                                'alignright alignjustify | fontselect',
+                                            fontsize_formats: "8px 9px 10px 11px 12px 14px 16px 18px 20px 24px 30px 36px 48px 60px 72px 96px",
+                                            font_formats: "Andale Mono=andale mono,times; Arial=arial,helvetica,sans-serif; Arial Black=arial black,avant garde; Book Antiqua=book antiqua,palatino; Comic Sans MS=comic sans ms,sans-serif; Courier New=courier new,courier; Georgia=georgia,palatino; Helvetica=helvetica; Impact=impact,chicago; Symbol=symbol; Tahoma=tahoma,arial,helvetica,sans-serif; Terminal=terminal,monaco; Times New Roman=times new roman,times; Trebuchet MS=trebuchet ms,geneva; Verdana=verdana,geneva; Webdings=webdings; Wingdings=wingdings,zapf dingbats"
+                                        }}
+                                    />
                                 </h6>
                                 {/* <h3 className="text-white text-3xl w-full mb-6">CCTV For your Safety</h3> */}
                                 <h3 className="About4Css">
-                                <Editor
-                                    onInit={(evt, editor) => editorRef.current = editor}
-                                    inline={true}
-                                    key='abouted2'
-                                    tinymceScriptSrc="../assets/tinymce/tinymce.min.js"
-                                    initialValue="CCTV For your Safety"
-                                    init={{
-                                        forced_root_block:"",
-                                        menubar: false,
-                                        toolbar: 'bold italic underline | fontsizeselect | backcolor forecolor | alignleft aligncenter ' +
-                                            'alignright alignjustify | fontselect',
-                                        fontsize_formats: "8px 9px 10px 11px 12px 14px 16px 18px 20px 24px 30px 36px 48px 60px 72px 96px",
-                                        font_formats: "Andale Mono=andale mono,times; Arial=arial,helvetica,sans-serif; Arial Black=arial black,avant garde; Book Antiqua=book antiqua,palatino; Comic Sans MS=comic sans ms,sans-serif; Courier New=courier new,courier; Georgia=georgia,palatino; Helvetica=helvetica; Impact=impact,chicago; Symbol=symbol; Tahoma=tahoma,arial,helvetica,sans-serif; Terminal=terminal,monaco; Times New Roman=times new roman,times; Trebuchet MS=trebuchet ms,geneva; Verdana=verdana,geneva; Webdings=webdings; Wingdings=wingdings,zapf dingbats"
+                                    <Editor
+                                        onInit={(evt, editor) => editorRef.current = editor}
+                                        inline={true}
+                                        key='abouted2'
+                                        tinymceScriptSrc="../assets/tinymce/tinymce.min.js"
+                                        initialValue="CCTV For your Safety"
+                                        init={{
+                                            plugins: [
+                                                "link"
+                                            ],
+                                            forced_root_block: "",
+                                            menubar: false,
+                                            toolbar: 'bold italic underline | link | fontsizeselect | backcolor forecolor | alignleft aligncenter ' +
+                                                'alignright alignjustify | fontselect',
+                                            fontsize_formats: "8px 9px 10px 11px 12px 14px 16px 18px 20px 24px 30px 36px 48px 60px 72px 96px",
+                                            font_formats: "Andale Mono=andale mono,times; Arial=arial,helvetica,sans-serif; Arial Black=arial black,avant garde; Book Antiqua=book antiqua,palatino; Comic Sans MS=comic sans ms,sans-serif; Courier New=courier new,courier; Georgia=georgia,palatino; Helvetica=helvetica; Impact=impact,chicago; Symbol=symbol; Tahoma=tahoma,arial,helvetica,sans-serif; Terminal=terminal,monaco; Times New Roman=times new roman,times; Trebuchet MS=trebuchet ms,geneva; Verdana=verdana,geneva; Webdings=webdings; Wingdings=wingdings,zapf dingbats"
 
-                                    }}
-                                />
+                                        }}
+                                    />
                                 </h3>
                                 {/* <p className="text-white text-xl py-3 px-0">Nunc pretium nibh non aliquam scelerisque. Integer porttitor
                                 dolor sit amet leo malesuada scelerisque. Sed sed tortor
@@ -80,9 +86,12 @@ export default function AboutUs4({ showPopup, overSection, displayDevice }) {
                                     tinymceScriptSrc="../assets/tinymce/tinymce.min.js"
                                     initialValue="<p>Nunc pretium nibh non aliquam scelerisque. Integer porttitor dolor sit amet leo malesuada scelerisque. Sed sed tortor lobortis, dictum lacus sed, mollis enim. Aenean ullamcorper accumsan sem sit amet aliquam. Cras euismod mauris felis, eget bibendum nibh facilisis ac.</p>"
                                     init={{
-                                        forced_root_block:"",
+                                        plugins: [
+                                            "link"
+                                        ],
+                                        forced_root_block: "",
                                         menubar: false,
-                                        toolbar: 'bold italic underline | fontsizeselect | backcolor forecolor | alignleft aligncenter ' +
+                                        toolbar: 'bold italic underline | link | fontsizeselect | backcolor forecolor | alignleft aligncenter ' +
                                             'alignright alignjustify | fontselect',
                                         fontsize_formats: "8px 9px 10px 11px 12px 14px 16px 18px 20px 24px 30px 36px 48px 60px 72px 96px",
                                         font_formats: "Andale Mono=andale mono,times; Arial=arial,helvetica,sans-serif; Arial Black=arial black,avant garde; Book Antiqua=book antiqua,palatino; Comic Sans MS=comic sans ms,sans-serif; Courier New=courier new,courier; Georgia=georgia,palatino; Helvetica=helvetica; Impact=impact,chicago; Symbol=symbol; Tahoma=tahoma,arial,helvetica,sans-serif; Terminal=terminal,monaco; Times New Roman=times new roman,times; Trebuchet MS=trebuchet ms,geneva; Verdana=verdana,geneva; Webdings=webdings; Wingdings=wingdings,zapf dingbats"
@@ -92,7 +101,7 @@ export default function AboutUs4({ showPopup, overSection, displayDevice }) {
                             </div>
                             <div className="py-5">
                                 <button className={`${About4Css.btnReadMore} text-white font-normal border flex justify-center w-40 items-center border-solid py-2 px-8 hover:text-black hover:bg-white`}>
-                          
+
                                     <Editor
                                         onInit={(evt, editor) => editorRef.current = editor}
                                         inline={true}
@@ -100,16 +109,19 @@ export default function AboutUs4({ showPopup, overSection, displayDevice }) {
                                         tinymceScriptSrc="../assets/tinymce/tinymce.min.js"
                                         initialValue=" Read More"
                                         init={{
-                                            forced_root_block:"",
+                                            plugins: [
+                                                "link"
+                                            ],
+                                            forced_root_block: "",
                                             menubar: false,
-                                            toolbar: 'bold italic underline | fontsizeselect | backcolor forecolor | alignleft aligncenter ' +
+                                            toolbar: 'bold italic underline | link | fontsizeselect | backcolor forecolor | alignleft aligncenter ' +
                                                 'alignright alignjustify | fontselect',
                                             fontsize_formats: "8px 9px 10px 11px 12px 14px 16px 18px 20px 24px 30px 36px 48px 60px 72px 96px",
                                             font_formats: "Andale Mono=andale mono,times; Arial=arial,helvetica,sans-serif; Arial Black=arial black,avant garde; Book Antiqua=book antiqua,palatino; Comic Sans MS=comic sans ms,sans-serif; Courier New=courier new,courier; Georgia=georgia,palatino; Helvetica=helvetica; Impact=impact,chicago; Symbol=symbol; Tahoma=tahoma,arial,helvetica,sans-serif; Terminal=terminal,monaco; Times New Roman=times new roman,times; Trebuchet MS=trebuchet ms,geneva; Verdana=verdana,geneva; Webdings=webdings; Wingdings=wingdings,zapf dingbats"
 
                                         }}
                                     />
-                                          <i style={{marginLeft:`4px`}} className="fas fa-arrow-right" aria-hidden="true"></i>
+                                    <i style={{ marginLeft: `4px` }} className="fas fa-arrow-right" aria-hidden="true"></i>
                                 </button>
                             </div>
                         </article>
