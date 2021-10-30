f02f8f76bb98fbfa1b34820a9a34d614deba0715
Ahsan Shahid <ahsanshahid2882@gmail.com>
2021-10-30 09:39:31 +0500

    Update Hero4Asset.js

--- src/assets/editingScreenAssets/headerEditAssets/Hero4Asset.js
+++ src/assets/editingScreenAssets/headerEditAssets/Hero4Asset.js
@@ -3,7 +3,9 @@ import React from 'react'
 export default function Hero4Asset() {
     return (
         <>
-
+            <section className="relative" style={{width: "96rem", height: "50rem"}}>
+                
+            </section>
         </>
     )
 }
