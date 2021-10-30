253bec17d8f03c7852114fbd73a6624f9fec15b1
Ahsan Shahid <ahsanshahid2882@gmail.com>
2021-10-30 14:10:03 +0500

    Update Template2Page.js

--- src/template2components/Template2Page.js
+++ src/template2components/Template2Page.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Navbar2 } from './Navbar2.js'
+import {Hero2} from './Hero2'
 import { Pricing2 } from './Pricing2.js'
 import { Services2 } from './Services2.js'
 import {Blog2} from './Blog2.js'
@@ -12,6 +13,7 @@ export default function Template2Page() {
   return (
     <>
       <Navbar2 />
+      <Hero2/>
       <Services2/>
       <Pricing2/>
       <Blog2/>
