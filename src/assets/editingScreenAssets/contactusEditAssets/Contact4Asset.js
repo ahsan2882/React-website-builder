c2cfba96292e0992c2b49c5bbaac8e19a0e55240
Ahsan Shahid <ahsanshahid2882@gmail.com>
2021-10-30 13:50:29 +0500

    Update Contact4Asset.js

--- src/assets/editingScreenAssets/contactusEditAssets/Contact4Asset.js
+++ src/assets/editingScreenAssets/contactusEditAssets/Contact4Asset.js
@@ -1,13 +1,47 @@
 import React from 'react'
-import Contact4AssetCss from './Contact4Asset.module.css'
 
 export default function Contact4Asset() {
     return (
         <>
-            <h1 className={Contact4AssetCss.firstHead} style={{ color: "red",fontSize: '5rem' }}>Contact Asset 4</h1>
-            <h1 className={Contact4AssetCss.secondHead}>Please contact us</h1>
-            <h1 className={Contact4AssetCss.thirdHead}>We love you!</h1>
-            <h1 className={Contact4AssetCss.fourthHead}>Send pleasing remarks</h1>
+            <section className="text-gray-400 bg-gray-900 body-font relative">
+                <div className="container px-5 py-24 mx-auto flex sm:flex-nowrap flex-wrap">
+                    <div className="lg:w-2/3 md:w-1/2 bg-gray-900 rounded-lg overflow-hidden sm:mr-10 p-10 flex items-end justify-start relative">
+                        <iframe width="100%" height="100%" title="map" className="absolute inset-0" frameBorder="0" marginHeight="0" marginWidth="0" scrolling="no" src="https://maps.google.com/maps?width=100%&height=600&hl=en&q=%C4%B0zmir+(My%20Business%20Name)&ie=UTF8&t=&z=14&iwloc=B&output=embed" style={{ filter: "grayscale(1) contrast(1.2) opacity(0.16)" }}></iframe>
+                        <div className="bg-gray-900 relative flex flex-wrap py-6 rounded shadow-md">
+                            <div className="lg:w-1/2 px-6">
+                                <h2 className="title-font font-semibold text-white tracking-widest text-xs">ADDRESS</h2>
+                                <p className="mt-1">4010 Feeney Way</p>
+                            </div>
+                            <div className="lg:w-1/2 px-6 mt-4 lg:mt-0">
+                                <h2 className="title-font font-semibold text-white tracking-widest text-xs">EMAIL</h2>
+                                <button className="text-blue-400 leading-relaxed">infosecurity@gmail.com</button>
+                                <h2 className="title-font font-semibold text-white tracking-widest text-xs mt-4">PHONE</h2>
+                                <p className="leading-relaxed">123-456-7890</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="lg:w-1/3 md:w-1/2 flex flex-col md:ml-auto w-full md:py-8 mt-8 md:mt-0">
+                        <h2 className="text-white text-lg mb-1 font-medium title-font">CONTACT</h2>
+                        <p className="leading-relaxed mb-5">Security is not a product, it is a process.</p>
+                        <div className="relative mb-4">
+                            <label for="name" className="leading-7 text-sm text-gray-400">Name</label>
+                            <input type="text" id="name" name="name" className="w-full bg-gray-800 rounded border border-gray-700 focus:border-blue-500 focus:ring-2 focus:ring-blue-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
+                        </div>
+                        <div className="relative mb-4">
+                            <label for="email" className="leading-7 text-sm text-gray-400">Email</label>
+                            <input type="email" id="email" name="email" className="w-full bg-gray-800 rounded border border-gray-700 focus:border-blue-500 focus:ring-2 focus:ring-blue-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
+                        </div>
+                        <div className="relative mb-4">
+                            <label for="message" className="leading-7 text-sm text-gray-400">Message</label>
+                            <textarea id="message" name="message" className="w-full bg-gray-800 rounded border border-gray-700 focus:border-blue-500 focus:ring-2 focus:ring-blue-900 h-32 text-base outline-none text-gray-100 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out">
+
+                            </textarea>
+                        </div>
+                        <button className="text-white bg-blue-500 border-0 py-2 px-6 focus:outline-none hover:bg-blue-600 rounded text-lg">SUBMIT</button>
+
+                    </div>
+                </div>
+            </section>
         </>
     )
 }
