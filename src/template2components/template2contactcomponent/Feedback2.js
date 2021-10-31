d46e1afa25916ecbac6726d268c01b9809f7fb0f
Ahsan Shahid <ahsanshahid2882@gmail.com>
2021-10-31 09:57:35 +0500

    Update Feedback2.js

--- src/template2components/template2contactcomponent/Feedback2.js
+++ src/template2components/template2contactcomponent/Feedback2.js
@@ -37,13 +37,16 @@ export default function Feedback2() {
                 key='Feedback1'
                 initialValue={`<span class="${FeedbackCss.SpanColor}"> YOUR </span> FEEDBACK`}
                 init={{
+                  plugins: [
+                    "link"
+                  ],
                   forced_root_block: "",
                   menubar: false,
-                  toolbar: 'bold italic underline | fontsizeselect | backcolor forecolor | alignleft aligncenter ' +
+                  toolbar: 'bold italic underline | link | fontsizeselect | backcolor forecolor | alignleft aligncenter ' +
                     'alignright alignjustify | fontselect',
                   fontsize_formats: "8px 9px 10px 11px 12px 14px 16px 18px 20px 24px 30px 36px 48px 60px 72px 96px",
                   font_formats: "Andale Mono=andale mono,times; Arial=arial,helvetica,sans-serif; Arial Black=arial black,avant garde; Book Antiqua=book antiqua,palatino; Comic Sans MS=comic sans ms,sans-serif; Courier New=courier new,courier; Georgia=georgia,palatino; Helvetica=helvetica; Impact=impact,chicago; Symbol=symbol; Tahoma=tahoma,arial,helvetica,sans-serif; Terminal=terminal,monaco; Times New Roman=times new roman,times; Trebuchet MS=trebuchet ms,geneva; Verdana=verdana,geneva; Webdings=webdings; Wingdings=wingdings,zapf dingbats"
-                }}
+                                        }}
               />
             </div>
             <div className={FeedbackCss.spanDesc}>
@@ -54,13 +57,16 @@ export default function Feedback2() {
                 key='Feedback2'
                 initialValue="<p>Our Services are best in town and premium Lorem ipsum dolor sit amet.</p>"
                 init={{
+                  plugins: [
+                    "link"
+                  ],
                   forced_root_block: "",
                   menubar: false,
-                  toolbar: 'bold italic underline | fontsizeselect | backcolor forecolor | alignleft aligncenter ' +
+                  toolbar: 'bold italic underline | link | fontsizeselect | backcolor forecolor | alignleft aligncenter ' +
                     'alignright alignjustify | fontselect',
                   fontsize_formats: "8px 9px 10px 11px 12px 14px 16px 18px 20px 24px 30px 36px 48px 60px 72px 96px",
                   font_formats: "Andale Mono=andale mono,times; Arial=arial,helvetica,sans-serif; Arial Black=arial black,avant garde; Book Antiqua=book antiqua,palatino; Comic Sans MS=comic sans ms,sans-serif; Courier New=courier new,courier; Georgia=georgia,palatino; Helvetica=helvetica; Impact=impact,chicago; Symbol=symbol; Tahoma=tahoma,arial,helvetica,sans-serif; Terminal=terminal,monaco; Times New Roman=times new roman,times; Trebuchet MS=trebuchet ms,geneva; Verdana=verdana,geneva; Webdings=webdings; Wingdings=wingdings,zapf dingbats"
-                }}
+                                        }}
               />
             </div>
             <div className={FeedbackCss.divMainHeading}>
@@ -83,13 +89,16 @@ export default function Feedback2() {
                 Dolore suscipit sed quis ullam sint velit esse? Omnis quae dolorem rem at, porro amet quam culpa aliquam magnam
                 voluptate saepe?</p>"
               init={{
+                plugins: [
+                  "link"
+                ],
                 forced_root_block: "",
                 menubar: false,
-                toolbar: 'bold italic underline | fontsizeselect | backcolor forecolor | alignleft aligncenter ' +
+                toolbar: 'bold italic underline | link | fontsizeselect | backcolor forecolor | alignleft aligncenter ' +
                   'alignright alignjustify | fontselect',
                 fontsize_formats: "8px 9px 10px 11px 12px 14px 16px 18px 20px 24px 30px 36px 48px 60px 72px 96px",
                 font_formats: "Andale Mono=andale mono,times; Arial=arial,helvetica,sans-serif; Arial Black=arial black,avant garde; Book Antiqua=book antiqua,palatino; Comic Sans MS=comic sans ms,sans-serif; Courier New=courier new,courier; Georgia=georgia,palatino; Helvetica=helvetica; Impact=impact,chicago; Symbol=symbol; Tahoma=tahoma,arial,helvetica,sans-serif; Terminal=terminal,monaco; Times New Roman=times new roman,times; Trebuchet MS=trebuchet ms,geneva; Verdana=verdana,geneva; Webdings=webdings; Wingdings=wingdings,zapf dingbats"
-              }}
+                                        }}
             />
 
             <button className={FeedbackCss.btnb}>
@@ -101,13 +110,16 @@ export default function Feedback2() {
                 key='Feedback3'
                 initialValue="Download Form"
                 init={{
+                  plugins: [
+                    "link"
+                  ],
                   forced_root_block: "",
                   menubar: false,
-                  toolbar: 'bold italic underline | fontsizeselect | backcolor forecolor | alignleft aligncenter ' +
+                  toolbar: 'bold italic underline | link | fontsizeselect | backcolor forecolor | alignleft aligncenter ' +
                     'alignright alignjustify | fontselect',
                   fontsize_formats: "8px 9px 10px 11px 12px 14px 16px 18px 20px 24px 30px 36px 48px 60px 72px 96px",
                   font_formats: "Andale Mono=andale mono,times; Arial=arial,helvetica,sans-serif; Arial Black=arial black,avant garde; Book Antiqua=book antiqua,palatino; Comic Sans MS=comic sans ms,sans-serif; Courier New=courier new,courier; Georgia=georgia,palatino; Helvetica=helvetica; Impact=impact,chicago; Symbol=symbol; Tahoma=tahoma,arial,helvetica,sans-serif; Terminal=terminal,monaco; Times New Roman=times new roman,times; Trebuchet MS=trebuchet ms,geneva; Verdana=verdana,geneva; Webdings=webdings; Wingdings=wingdings,zapf dingbats"
-                }}
+                                        }}
               />
                </button>
 
