f33f30816953348eb988abf40d0aaec92c92a109
Ahsan Shahid <ahsanshahid2882@gmail.com>
2021-10-31 18:04:02 +0500

    Update Blog2.js

--- src/template2components/Blog2.js
+++ src/template2components/Blog2.js
@@ -9,7 +9,7 @@ export const Blog2 = () => {
   return (
     <>
       <section className={`${Blog2moduleCss['head-main']}`}>
-        <h1 class={`${Blog2moduleCss.MainHeading}`}>
+        <h1 className={`${Blog2moduleCss.MainHeading}`}>
           <Editor
             onInit={(evt, editor) => editorRef.current = editor}
             inline={true}
@@ -262,8 +262,7 @@ export const Blog2 = () => {
             </div>
 
             <div className={`${Blog2moduleCss['blog-content']}`}>
-              <h4>
-                <h1>
+                <h4>
                   <Editor
                     onInit={(evt, editor) => editorRef.current = editor}
                     inline={true}
@@ -282,12 +281,7 @@ export const Blog2 = () => {
                       font_formats: "Andale Mono=andale mono,times; Arial=arial,helvetica,sans-serif; Arial Black=arial black,avant garde; Book Antiqua=book antiqua,palatino; Comic Sans MS=comic sans ms,sans-serif; Courier New=courier new,courier; Georgia=georgia,palatino; Helvetica=helvetica; Impact=impact,chicago; Symbol=symbol; Tahoma=tahoma,arial,helvetica,sans-serif; Terminal=terminal,monaco; Times New Roman=times new roman,times; Trebuchet MS=trebuchet ms,geneva; Verdana=verdana,geneva; Webdings=webdings; Wingdings=wingdings,zapf dingbats"
                     }}
                   />
-                </h1>
-
-
-
-
-              </h4>
+                </h4>
               <Editor
                 onInit={(evt, editor) => editorRef.current = editor}
                 inline={true}
