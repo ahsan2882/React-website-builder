35b8ce8e23654c3f8519552225a9b7f127e13dc1
Ahsan Shahid <ahsanshahid2882@gmail.com>
2021-11-01 12:54:39 +0500

    Create nav2Edits.js

---
+++ src/template2components/nav2Edits.js
@@ -0,0 +1,6 @@
+const navbar2 = {
+    aboutLinks: ["Our Vision","Our Experience", "Our Policies", "Our Prices", "Our Accreditations", "Our Team", "Our Partners"],
+    sectorLinks: ["Construction", "Corporate", "Industrial", "Retail", "Logistics", "Property Management"],
+    serviceLinks:["Static Security", "Mobile Patrolling", "CCTV Monitoring", "Lock & Unlock", "Vacant Unit Checks", "Technology Product", "Manned Guarding", "Support & Management", "Event Security", "Guard House"]
+}
+export default navbar2;
