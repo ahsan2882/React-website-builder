0b9f5798991b8c9d278c46a0f4cefa41a7795e72
Ahsan Shahid <ahsanshahid2882@gmail.com>
2021-11-01 08:55:22 +0500

    Create template2subpages.js

---
+++ src/editorScreen/template2subpages.js
@@ -0,0 +1,24 @@
+const sectors = {
+    "CONSTRUCTION": [],
+    "CORPORATE": [],
+    "INDUSTRIAL": [],
+    "RETAIL": [],
+    "LOGISTICS": [],
+    "PROPERTY MANAGEMENT":[]
+}
+
+const services = {
+    "STATIC SECURITY": [],
+    "MOBILE PATROLLING": [],
+    "CCTV MONITORING": [],
+    "LOCK & UNLOCK": [],
+    "VACANT UNIT CHECKS": [],
+    "TECHNOLOGY PRODUCT": [],
+    "MANNED GUARDING": [],
+    "SUPPORT & MANAGEMENT": [],
+    "EVENT SECURITY": [],
+    "GUARD HOUSE":[]
+}
+const subsub = {"SECTORS": sectors, "SERVICES": services}
+
+export default subsub;
