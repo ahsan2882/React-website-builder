1dde2883c400ba6f119335707a9f17450b484b05
Ahsan Shahid <ahsanshahid2882@gmail.com>
2021-11-01 14:26:41 +0500

    Update Hero2.js

--- src/template2components/Hero2.js
+++ src/template2components/Hero2.js
@@ -9,41 +9,22 @@ export const Hero2 = ({ displayDevice, showPopup, overSection }) => {
 
     return (
         <>
-            <section className={`${Hero2moduleCss['slider-temp2']}`} >
-                <div className={`${Hero2moduleCss['slider__warpper']}`}>
-                    <div className={`${Hero2moduleCss['flex__container']} ${Hero2moduleCss['flex--pikachu']} ${Hero2moduleCss['flex--active']}`}>
-                        <img className={`${Hero2moduleCss['bg-line']}`} src="https://i.ibb.co/VwB7mhG/Path-7.png" alt="" />
+            <section style={{ background: currentBackground }}>
+                <section className={`${Hero2moduleCss['slider-temp2']}`} >
+                    <div className={`${Hero2moduleCss['slider__warpper']}`}>
+                        <div className={`${Hero2moduleCss['flex__container']} ${Hero2moduleCss['flex--pikachu']} ${Hero2moduleCss['flex--active']}`}>
+                            <img className={`${Hero2moduleCss['bg-line']}`} src="https://i.ibb.co/VwB7mhG/Path-7.png" alt="" />
 
-                        <div className={`${Hero2moduleCss['{flex__item']} ${Hero2moduleCss['flex__item--left']}`} style={{ background: currentBackground }}>
-                            <img className={displayDevice ? `${Hero2moduleCss.pokemonimg}` : `${Hero2moduleCss.pokemonimgM}`} src="https://i.ibb.co/9Wt8w45/guarg.png" alt="rhuis " />
-                            <div className={`${Hero2moduleCss['flex__content']}`}>
+                            <div className={`${Hero2moduleCss['{flex__item']} ${Hero2moduleCss['flex__item--left']}`} style={{ background: currentBackground }}>
+                                <img className={displayDevice ? `${Hero2moduleCss.pokemonimg}` : `${Hero2moduleCss.pokemonimgM}`} src="https://i.ibb.co/9Wt8w45/guarg.png" alt="rhuis " />
+                                <div className={`${Hero2moduleCss['flex__content']}`}>
 
-                                <Editor
-                                    onInit={(evt, editor) => editorRef.current = editor}
-                                    inline={true}
-                                    key='hero4ed2'
-                                    tinymceScriptSrc={process.env.PUBLIC_URL + '/tinymce/tinymce.min.js'}
-                                    initialValue={displayDevice ? `<p class="${Hero2moduleCss.textSub}">Security</p>` : `<p class="${Hero2moduleCss.textSubM}">Security</p>`}
-                                    init={{
-                                        plugins: [
-                                            "link"
-                                        ],
-                                        forced_root_block: "",
-                                        menubar: false,
-                                        toolbar: 'bold italic underline | link | fontsizeselect | backcolor forecolor | alignleft aligncenter ' +
-                                            'alignright alignjustify | fontselect',
-                                        fontsize_formats: "8px 9px 10px 11px 12px 14px 16px 18px 20px 24px 30px 36px 48px 60px 72px 96px",
-                                        font_formats: "Andale Mono=andale mono,times; Arial=arial,helvetica,sans-serif; Arial Black=arial black,avant garde; Book Antiqua=book antiqua,palatino; Comic Sans MS=comic sans ms,sans-serif; Courier New=courier new,courier; Georgia=georgia,palatino; Helvetica=helvetica; Impact=impact,chicago; Symbol=symbol; Tahoma=tahoma,arial,helvetica,sans-serif; Terminal=terminal,monaco; Times New Roman=times new roman,times; Trebuchet MS=trebuchet ms,geneva; Verdana=verdana,geneva; Webdings=webdings; Wingdings=wingdings,zapf dingbats"
-                                    }}
-                                />
-
-                                <h1 className={displayDevice ? `${Hero2moduleCss['text--big']}` : `${Hero2moduleCss['text--bigM']}`}>
                                     <Editor
                                         onInit={(evt, editor) => editorRef.current = editor}
                                         inline={true}
-                                        key='hero(2)33'
+                                        key='hero4ed2'
                                         tinymceScriptSrc={process.env.PUBLIC_URL + '/tinymce/tinymce.min.js'}
-                                        initialValue="GUARD"
+                                        initialValue={displayDevice ? `<p class="${Hero2moduleCss.textSub}">Security</p>` : `<p class="${Hero2moduleCss.textSubM}">Security</p>`}
                                         init={{
                                             plugins: [
                                                 "link"
@@ -56,20 +37,41 @@ export const Hero2 = ({ displayDevice, showPopup, overSection }) => {
                                             font_formats: "Andale Mono=andale mono,times; Arial=arial,helvetica,sans-serif; Arial Black=arial black,avant garde; Book Antiqua=book antiqua,palatino; Comic Sans MS=comic sans ms,sans-serif; Courier New=courier new,courier; Georgia=georgia,palatino; Helvetica=helvetica; Impact=impact,chicago; Symbol=symbol; Tahoma=tahoma,arial,helvetica,sans-serif; Terminal=terminal,monaco; Times New Roman=times new roman,times; Trebuchet MS=trebuchet ms,geneva; Verdana=verdana,geneva; Webdings=webdings; Wingdings=wingdings,zapf dingbats"
                                         }}
                                     />
-                                </h1>
+
+                                    <h1 className={displayDevice ? `${Hero2moduleCss['text--big']}` : `${Hero2moduleCss['text--bigM']}`}>
+                                        <Editor
+                                            onInit={(evt, editor) => editorRef.current = editor}
+                                            inline={true}
+                                            key='hero(2)33'
+                                            tinymceScriptSrc={process.env.PUBLIC_URL + '/tinymce/tinymce.min.js'}
+                                            initialValue="GUARD"
+                                            init={{
+                                                plugins: [
+                                                    "link"
+                                                ],
+                                                forced_root_block: "",
+                                                menubar: false,
+                                                toolbar: 'bold italic underline | link | fontsizeselect | backcolor forecolor | alignleft aligncenter ' +
+                                                    'alignright alignjustify | fontselect',
+                                                fontsize_formats: "8px 9px 10px 11px 12px 14px 16px 18px 20px 24px 30px 36px 48px 60px 72px 96px",
+                                                font_formats: "Andale Mono=andale mono,times; Arial=arial,helvetica,sans-serif; Arial Black=arial black,avant garde; Book Antiqua=book antiqua,palatino; Comic Sans MS=comic sans ms,sans-serif; Courier New=courier new,courier; Georgia=georgia,palatino; Helvetica=helvetica; Impact=impact,chicago; Symbol=symbol; Tahoma=tahoma,arial,helvetica,sans-serif; Terminal=terminal,monaco; Times New Roman=times new roman,times; Trebuchet MS=trebuchet ms,geneva; Verdana=verdana,geneva; Webdings=webdings; Wingdings=wingdings,zapf dingbats"
+                                            }}
+                                        />
+                                    </h1>
+                                </div>
+                                <p className={`${Hero2moduleCss['text__background']}`}>Security</p>
+
                             </div>
-                            <p className={`${Hero2moduleCss['text__background']}`}>Security</p>
+
 
                         </div>
+
 
 
                     </div>
 
 
-
-                </div>
-
-
+                </section>
             </section>
             <div style={(overSection && showPopup) ? { position: "absolute", top: "9rem", right: "5rem", zIndex: "9999999" } : { display: "none" }} className="toBeRemoved">
                 <GithubPicker
