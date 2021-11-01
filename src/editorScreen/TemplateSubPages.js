7b6389ac6e85c770ba8952974b7e968471ad3d06
Ahsan Shahid <ahsanshahid2882@gmail.com>
2021-11-01 08:55:27 +0500

    Delete TemplateSubPages.js

--- src/editorScreen/TemplateSubPages.js
+++
@@ -1,7 +0,0 @@
-const subPages = {
-    "template1Subpages": ["Home"],
-    "template2Subpages":["Home","About","Contact"],
-    "template3Subpages":["Home"],
-    "template4Subpages":["Home", "Contact", "About"],
-}
-export default subPages;
