5331892db2ac72d274ef6587de62f542edaf8bae
Ahsan Shahid <ahsanshahid2882@gmail.com>
2021-11-04 19:00:47 +0500

    Update About2service.js

--- src/template2components/template2aboutcomponent/About2service.js
+++ src/template2components/template2aboutcomponent/About2service.js
@@ -348,7 +348,7 @@ export default function About2service({ showPopup, overSection}) {
                 </div>
 
             </section>
-            <div style={(overSection && showPopup) ? { position: "absolute", top: "9rem", right: "5rem", zIndex: "9999999" } : { display: "none" }} className="toBeRemoved">
+            <div style={(overSection && showPopup) ? { position: "absolute", top: "5rem", right: "5rem", zIndex: "9999999" } : { display: "none" }} className="toBeRemoved">
                 <GithubPicker
                     triangle="top-right"
                     onChangeComplete={(color) => {
