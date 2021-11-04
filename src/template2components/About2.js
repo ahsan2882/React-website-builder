1f1c9abe51b0334f0490038a03841e5a5cc88bf5
Ahsan Shahid <ahsanshahid2882@gmail.com>
2021-11-04 19:00:17 +0500

    Update About2.js

--- src/template2components/About2.js
+++ src/template2components/About2.js
@@ -93,7 +93,7 @@ export const About2 = ({ displayDevice, showPopup, overSection }) => {
           </section>
         </section>
       </section>
-      <div style={(overSection && showPopup) ? { position: "absolute", top: "9rem", right: "5rem", zIndex: "9999999" } : { display: "none" }} className="toBeRemoved">
+      <div style={(overSection && showPopup) ? { position: "absolute", top: "5rem", right: "5rem", zIndex: "9999999" } : { display: "none" }} className="toBeRemoved">
         <GithubPicker
           triangle="top-right"
           onChangeComplete={(color) => {
