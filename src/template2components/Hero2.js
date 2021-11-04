70454378dc51a544058c666553eaf49033f15425
Ahsan Shahid <ahsanshahid2882@gmail.com>
2021-11-04 18:43:29 +0500

    Update Hero2.js

--- src/template2components/Hero2.js
+++ src/template2components/Hero2.js
@@ -9,7 +9,7 @@ export const Hero2 = ({ displayDevice, showPopup, overSection }) => {
 
     return (
         <>
-            <section style={{ background: currentBackground }}>
+            {/* <section style={{ background: currentBackground }}>
                 <section className={`${Hero2moduleCss['slider-temp2']}`} >
                     <div className={`${Hero2moduleCss['slider__warpper']}`}>
                         <div className={`${Hero2moduleCss['flex__container']} ${Hero2moduleCss['flex--pikachu']} ${Hero2moduleCss['flex--active']}`}>
@@ -72,6 +72,68 @@ export const Hero2 = ({ displayDevice, showPopup, overSection }) => {
 
 
                 </section>
+            </section> */}
+            <section style={{ background: currentBackground }} className={`${Hero2moduleCss.heroSection} relative`}>
+                <img src="https://i.ibb.co/VwB7mhG/Path-7.png" alt="Pah 7" className="w-full h-full" />
+                {/* <img src="https://i.ibb.co/9Wt8w45/guarg.png" alt="guarg" border="0">*/}
+                <section className="absolute bottom-8 right-1/4 z-10">
+                    <Editor
+                        onInit={(evt, editor) => editorRef.current = editor}
+                        inline={true}
+                        key='hero4ed2'
+                        initialValue={`<img src="https://i.ibb.co/9Wt8w45/guarg.png" alt="logo" class="${Hero2moduleCss.guardImg} object-cover"/>`}
+                        init={{
+                            forced_root_block: "",
+                            menubar: false,
+                            images_upload_handler: function (blobInfo, success, failure) {
+                                success("data:" + blobInfo.blob().type + ";base64," + blobInfo.base64());
+                            },
+                            images_upload_url: '/upload-images',
+                            plugins: [
+                                "image"
+                            ],
+                            toolbar: 'image'
+                        }}
+                    />
+                </section>
+                <h1 className="absolute text-2xl text-white" style={{left:"15%", top:"30%"}}>
+                    <Editor
+                        onInit={(evt, editor) => editorRef.current = editor}
+                        inline={true}
+                        key='Hero2Ed1'
+                        initialValue="SECURITY"
+                        init={{
+                            plugins: [
+                                "link"
+                            ],
+                            forced_root_block: "",
+                            menubar: false,
+                            toolbar: 'bold italic underline | link | fontsizeselect | backcolor forecolor | alignleft aligncenter ' +
+                                'alignright alignjustify | fontselect',
+                            fontsize_formats: "8px 9px 10px 11px 12px 14px 16px 18px 20px 24px 30px 36px 48px 60px 72px 96px",
+                            font_formats: "Andale Mono=andale mono,times; Arial=arial,helvetica,sans-serif; Arial Black=arial black,avant garde; Book Antiqua=book antiqua,palatino; Comic Sans MS=comic sans ms,sans-serif; Courier New=courier new,courier; Georgia=georgia,palatino; Helvetica=helvetica; Impact=impact,chicago; Symbol=symbol; Tahoma=tahoma,arial,helvetica,sans-serif; Terminal=terminal,monaco; Times New Roman=times new roman,times; Trebuchet MS=trebuchet ms,geneva; Verdana=verdana,geneva; Webdings=webdings; Wingdings=wingdings,zapf dingbats"
+                        }}
+                    />
+                </h1>
+                <h1 className={`absolute text-white font-bold my-0 ${Hero2moduleCss.mainHead}`} style={{zIndex:"1", left:"14%", top:"35%"}}>
+                    <Editor
+                        onInit={(evt, editor) => editorRef.current = editor}
+                        inline={true}
+                        key='Hero2Ed1'
+                        initialValue="GUARD"
+                        init={{
+                            plugins: [
+                                "link"
+                            ],
+                            forced_root_block: "",
+                            menubar: false,
+                            toolbar: 'bold italic underline | link | fontsizeselect | backcolor forecolor | alignleft aligncenter ' +
+                                'alignright alignjustify | fontselect',
+                            fontsize_formats: "8px 9px 10px 11px 12px 14px 16px 18px 20px 24px 30px 36px 48px 60px 72px 96px",
+                            font_formats: "Andale Mono=andale mono,times; Arial=arial,helvetica,sans-serif; Arial Black=arial black,avant garde; Book Antiqua=book antiqua,palatino; Comic Sans MS=comic sans ms,sans-serif; Courier New=courier new,courier; Georgia=georgia,palatino; Helvetica=helvetica; Impact=impact,chicago; Symbol=symbol; Tahoma=tahoma,arial,helvetica,sans-serif; Terminal=terminal,monaco; Times New Roman=times new roman,times; Trebuchet MS=trebuchet ms,geneva; Verdana=verdana,geneva; Webdings=webdings; Wingdings=wingdings,zapf dingbats"
+                        }}
+                    />
+                </h1>
             </section>
             <div style={(overSection && showPopup) ? { position: "absolute", top: "9rem", right: "5rem", zIndex: "9999999" } : { display: "none" }} className="toBeRemoved">
                 <GithubPicker
