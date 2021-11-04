237a1b738191594a4499fa5f83159d52bcf5b1c7
Ahsan Shahid <ahsanshahid2882@gmail.com>
2021-11-04 19:01:05 +0500

    Update Corporate.js

--- src/template2components/template2SectorComponents/corporate/Corporate.js
+++ src/template2components/template2SectorComponents/corporate/Corporate.js
@@ -414,7 +414,7 @@ export const Corporate = ({ showPopup, overSection }) => {
         </div>
     </div>
 </section>
-<div style={(overSection && showPopup) ? { position: "absolute", top: "9rem", right: "5rem", zIndex: "9999999" } : { display: "none" }} className="toBeRemoved">
+<div style={(overSection && showPopup) ? { position: "absolute", top: "5rem", right: "5rem", zIndex: "9999999" } : { display: "none" }} className="toBeRemoved">
                 <GithubPicker
                     triangle="top-right"
                     onChangeComplete={(color) => {
