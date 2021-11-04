a3e2b0973fb742a3cdd708646a1d26ff725b2801
Ahsan Shahid <ahsanshahid2882@gmail.com>
2021-11-04 19:00:42 +0500

    Update Services2.js

--- src/template2components/Services2.js
+++ src/template2components/Services2.js
@@ -310,7 +310,7 @@ export const Services2 = ({ showPopup, overSection }) => {
                     </div>
                 </section>
             </section>
-            <div style={(overSection && showPopup) ? { position: "absolute", top: "9rem", right: "5rem", zIndex: "9999999" } : { display: "none" }} className="toBeRemoved">
+            <div style={(overSection && showPopup) ? { position: "absolute", top: "5rem", right: "5rem", zIndex: "9999999" } : { display: "none" }} className="toBeRemoved">
                 <GithubPicker
                     triangle="top-right"
                     onChangeComplete={(color) => {
