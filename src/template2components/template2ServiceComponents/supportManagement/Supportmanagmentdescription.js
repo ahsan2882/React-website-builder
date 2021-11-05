99fa06f2fb28b3851e736bc4fb6334849ddde180
Ahsan Shahid <ahsanshahid2882@gmail.com>
2021-11-05 18:52:27 +0500

    Update Supportmanagmentdescription.js

--- src/template2components/template2ServiceComponents/supportManagement/Supportmanagmentdescription.js
+++ src/template2components/template2ServiceComponents/supportManagement/Supportmanagmentdescription.js
@@ -1,6 +1,6 @@
 import React, { useRef } from 'react'
 import SupportdescriptionCss from './SupportManagmentdescription.module.css'
-import Img1 from '../../../assets/template2images/support.jpg'
+// import Img1 from '../../../assets/template2images/support.jpg'
 import { Editor } from '@tinymce/tinymce-react';
 export default function Supportmanagmentdescription() {
  
@@ -79,7 +79,7 @@ export default function Supportmanagmentdescription() {
                             onInit={(evt, editor) => editorRef.current = editor}
                             inline={true}
                             key='EventSecurity4'
-                            initialValue={`<img src=${Img1} alt="news1"w-96 h-72 object-cover"/>`}
+                            initialValue={`<img src="https://i.ibb.co/GkHRXxP/support.jpg" alt="support" class="w-96 h-72 object-cover"/>`}
                             init={{
                                 forced_root_block: "",
                                 menubar: false,
