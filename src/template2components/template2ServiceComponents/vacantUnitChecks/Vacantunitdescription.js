4e24038db49441fc982ef0953a1ecdb4d954d0ae
Ahsan Shahid <ahsanshahid2882@gmail.com>
2021-11-05 18:52:43 +0500

    Update Vacantunitdescription.js

--- src/template2components/template2ServiceComponents/vacantUnitChecks/Vacantunitdescription.js
+++ src/template2components/template2ServiceComponents/vacantUnitChecks/Vacantunitdescription.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import VacantunitdescriptionCss from './Vacantunitdescription.module.css'
-import Img1 from '../../../assets/template2images/services11.jpg'
+// import Img1 from '../../../assets/template2images/services11.jpg'
 export default function Vacantunitdescription() {
     return (
         <section class={VacantunitdescriptionCss['service-content']}>
@@ -11,7 +11,7 @@ export default function Vacantunitdescription() {
             </div>
             <div class={VacantunitdescriptionCss['service-grid']}>
                 <div class={VacantunitdescriptionCss.sg1}><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam, Lorem ipsum dolor sit amet consectetur error?</p> </div>
-                <div class={VacantunitdescriptionCss.sg2}><img src={Img1} alt="news1"/> </div>
+                    <div class={VacantunitdescriptionCss.sg2}><img src="https://i.ibb.co/ngvMh9v/services11.jpg" alt="services11"/> </div>
                 <div class={VacantunitdescriptionCss.sg3}><p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptates provident voluptas sint obcaecati pariatur dolor culpa ad distinctio magni delectus.Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptates provident voluptas sint obcaecati pariatur dolor
                 culpa ad distinctio magni delectus.culpa ad distinctio magni delectus.Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptates provident
                 voluptas sint obcaecati pariatur dolor
