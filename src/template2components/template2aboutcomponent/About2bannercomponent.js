764045aa7af920d23c0a86ba88efa7ef0f31c2b7
Ahsan Shahid <ahsanshahid2882@gmail.com>
2021-11-05 13:56:52 +0500

    Update About2bannercomponent.js

--- src/template2components/template2aboutcomponent/About2bannercomponent.js
+++ src/template2components/template2aboutcomponent/About2bannercomponent.js
@@ -1,14 +1,16 @@
 import React, { useRef } from 'react'
 import About2Css from './About2banner.module.css'
 import { Editor } from '@tinymce/tinymce-react';
-export default function About2bannercomponent({displayDevice}) {
+import { Navbar2 } from '../Navbar2';
+export default function About2bannercomponent({ displayDevice }) {
     const editorRef = useRef(null);
     return (
         <>
-        <section className={About2Css['header-img-serv']}>
-            <div className={About2Css['head-img-text']}>
-                <h1 className={displayDevice ? About2Css['head1'] : About2Css['headM']}>
-                <Editor
+            <Navbar2 displayDevice={displayDevice} />
+            <section className={About2Css['header-img-serv']}>
+                <div className={About2Css['head-img-text']}>
+                    <h1 className={displayDevice ? About2Css['head1'] : About2Css['headM']}>
+                        <Editor
                             onInit={(evt, editor) => editorRef.current = editor}
                             inline={true}
                             key='About1'
@@ -23,16 +25,16 @@ export default function About2bannercomponent({displayDevice}) {
                                     'alignright alignjustify | fontselect',
                                 fontsize_formats: "8px 9px 10px 11px 12px 14px 16px 18px 20px 24px 30px 36px 48px 60px 72px 96px",
                                 font_formats: "Andale Mono=andale mono,times; Arial=arial,helvetica,sans-serif; Arial Black=arial black,avant garde; Book Antiqua=book antiqua,palatino; Comic Sans MS=comic sans ms,sans-serif; Courier New=courier new,courier; Georgia=georgia,palatino; Helvetica=helvetica; Impact=impact,chicago; Symbol=symbol; Tahoma=tahoma,arial,helvetica,sans-serif; Terminal=terminal,monaco; Times New Roman=times new roman,times; Trebuchet MS=trebuchet ms,geneva; Verdana=verdana,geneva; Webdings=webdings; Wingdings=wingdings,zapf dingbats"
-                                         }}
+                            }}
                         />
-                        </h1>
+                    </h1>
                     <div className={About2Css.dash}></div>
                 </div>
                 <Editor
                     onInit={(evt, editor) => editorRef.current = editor}
                     inline={true}
                     key='About2'
-                initialValue={`<img src="https://i.ibb.co/Fmz3cdv/about-us-banner.jpg" alt="about-banner" />`}
+                    initialValue={`<img src="https://i.ibb.co/Fmz3cdv/about-us-banner.jpg" alt="about-banner" />`}
                     init={
                         {
                             forced_root_block: "",
@@ -45,11 +47,11 @@ export default function About2bannercomponent({displayDevice}) {
                                 "image"
                             ],
                             toolbar: 'image'
-                    }}
+                        }}
                 />
             </section>
-            </>
-           
+        </>
+
     )
-} 
+}
 
