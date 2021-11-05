be4e024b50297954a6b77d5eb0d3c461665d9da4
Ahsan Shahid <ahsanshahid2882@gmail.com>
2021-11-05 16:06:25 +0500

    Update SupportManagmentbanner.js

--- src/template2components/template2ServiceComponents/supportManagement/SupportManagmentbanner.js
+++ src/template2components/template2ServiceComponents/supportManagement/SupportManagmentbanner.js
@@ -2,13 +2,16 @@ import React, { useRef } from 'react'
 import SupportManagmentbannerCss from './Supportmanagmentbanner.module.css'
 import Img1 from '../../../assets/template2images/supportandmanagement-banner.jpg'
 import { Editor } from '@tinymce/tinymce-react';
-export default function SupportManagmentbanner() {
+import { Navbar2 } from '../../Navbar2';
+
+export default function SupportManagmentbanner({ displayDevice, linksfunc }) {
     const editorRef = useRef(null);
     return (
         <section className={SupportManagmentbannerCss['header-img-serv']}>
-        <div class={SupportManagmentbannerCss['head-img-text']}>
-            <h1 className={SupportManagmentbannerCss.head1}><b>
-            <Editor
+            <Navbar2 displayDevice={displayDevice} linksfunc={linksfunc} />
+            <div class={SupportManagmentbannerCss['head-img-text']}>
+                <h1 className={SupportManagmentbannerCss.head1}><b>
+                    <Editor
                         onInit={(evt, editor) => editorRef.current = editor}
                         inline={true}
                         key='Guard1'
@@ -25,14 +28,14 @@ export default function SupportManagmentbanner() {
                             font_formats: "Andale Mono=andale mono,times; Arial=arial,helvetica,sans-serif; Arial Black=arial black,avant garde; Book Antiqua=book antiqua,palatino; Comic Sans MS=comic sans ms,sans-serif; Courier New=courier new,courier; Georgia=georgia,palatino; Helvetica=helvetica; Impact=impact,chicago; Symbol=symbol; Tahoma=tahoma,arial,helvetica,sans-serif; Terminal=terminal,monaco; Times New Roman=times new roman,times; Trebuchet MS=trebuchet ms,geneva; Verdana=verdana,geneva; Webdings=webdings; Wingdings=wingdings,zapf dingbats"
                         }}
                     />
-             </b></h1>
-            <div className={SupportManagmentbannerCss.dash}></div>   
-        </div>
-        <Editor
+                </b></h1>
+                <div className={SupportManagmentbannerCss.dash}></div>
+            </div>
+            <Editor
                 onInit={(evt, editor) => editorRef.current = editor}
                 inline={true}
                 key='EventSecurity4'
-                initialValue={`<img src=${Img1} alt="technology-product" w-96 h-72 object-cover"/>`}
+                initialValue={`<img src="${Img1}" alt="technology-product" class="w-full h-full object-cover"/>`}
                 init={{
                     forced_root_block: "",
                     menubar: false,
@@ -46,8 +49,8 @@ export default function SupportManagmentbanner() {
                     toolbar: 'image'
                 }}
             />
-        
-        
-    </section>
+
+
+        </section>
     )
 }
