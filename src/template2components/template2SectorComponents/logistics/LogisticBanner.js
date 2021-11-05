33c5807251eea73d3a750672a02d9327061c3624
Ahsan Shahid <ahsanshahid2882@gmail.com>
2021-11-05 13:56:58 +0500

    Update LogisticBanner.js

--- src/template2components/template2SectorComponents/logistics/LogisticBanner.js
+++ src/template2components/template2SectorComponents/logistics/LogisticBanner.js
@@ -2,15 +2,17 @@ import React, { useRef } from 'react'
 import LogisticBannerCss from './LogisticBanner.module.css'
 import { Editor } from '@tinymce/tinymce-react';
 import img from './logistics-banner.jpg'
-export const LogisticBanner = () => {
+import { Navbar2 } from '../../Navbar2';
+export const LogisticBanner = ({ displayDevice }) => {
     const editorRef = useRef(null);
 
     return (
-     <>
- <section className={LogisticBannerCss['header-img-serv']}>
+        <>
+            <section className={LogisticBannerCss['header-img-serv']}>
+                <Navbar2 displayDevice={displayDevice} />
                 <div className={`${LogisticBannerCss['head-img-text']}`}>
                     <h1>
-                <Editor
+                        <Editor
                             onInit={(evt, editor) => editorRef.current = editor}
                             inline={true}
                             key='About1'
@@ -25,16 +27,16 @@ export const LogisticBanner = () => {
                                     'alignright alignjustify | fontselect',
                                 fontsize_formats: "8px 9px 10px 11px 12px 14px 16px 18px 20px 24px 30px 36px 48px 60px 72px 96px",
                                 font_formats: "Andale Mono=andale mono,times; Arial=arial,helvetica,sans-serif; Arial Black=arial black,avant garde; Book Antiqua=book antiqua,palatino; Comic Sans MS=comic sans ms,sans-serif; Courier New=courier new,courier; Georgia=georgia,palatino; Helvetica=helvetica; Impact=impact,chicago; Symbol=symbol; Tahoma=tahoma,arial,helvetica,sans-serif; Terminal=terminal,monaco; Times New Roman=times new roman,times; Trebuchet MS=trebuchet ms,geneva; Verdana=verdana,geneva; Webdings=webdings; Wingdings=wingdings,zapf dingbats"
-                                         }}
+                            }}
                         />
-                        </h1>
+                    </h1>
                     <div className={LogisticBannerCss.dash}></div>
                 </div>
                 <Editor
                     onInit={(evt, editor) => editorRef.current = editor}
                     inline={true}
                     key='About2'
-                initialValue={`<img src=${img} alt="about-banner" />`}
+                    initialValue={`<img src=${img} alt="about-banner" />`}
                     init={
                         {
                             forced_root_block: "",
@@ -47,11 +49,11 @@ export const LogisticBanner = () => {
                                 "image"
                             ],
                             toolbar: 'image'
-                    }}
+                        }}
                 />
             </section>
 
 
-     </>
+        </>
     )
 }
