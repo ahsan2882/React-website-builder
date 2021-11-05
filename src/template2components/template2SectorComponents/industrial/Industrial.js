e5594404a47fb31ae9f48882ce1eda63ad0dde29
Ahsan Shahid <ahsanshahid2882@gmail.com>
2021-11-05 18:51:13 +0500

    Update Industrial.js

--- src/template2components/template2SectorComponents/industrial/Industrial.js
+++ src/template2components/template2SectorComponents/industrial/Industrial.js
@@ -1,5 +1,5 @@
 import React, { useRef, useState }from 'react'
-import image from './news1.jpg'
+// import image from './news1.jpg'
 import { GithubPicker } from 'react-color';
 
 import { Editor } from '@tinymce/tinymce-react';
