8c09d4f13d196a41ce9231de7230ba2f71456d2f
Ahsan Shahid <ahsanshahid2882@gmail.com>
2021-11-05 18:51:38 +0500

    Update Cctvmonetoringdescription.js

--- src/template2components/template2ServiceComponents/cctvMonitoring/Cctvmonetoringdescription.js
+++ src/template2components/template2ServiceComponents/cctvMonitoring/Cctvmonetoringdescription.js
@@ -1,6 +1,6 @@
 import React, { useRef } from 'react'
 import CctvdescriptionCss from './Cctvmonetoringdescription.module.css'
-import Img1 from '../../../assets/template2images/support.jpg'
+// import Img1 from '../../../assets/template2images/support.jpg'
 import { Editor } from '@tinymce/tinymce-react';
 
 export default function Cctvmonetoringdescription() {
@@ -79,7 +79,7 @@ export default function Cctvmonetoringdescription() {
                         onInit={(evt, editor) => editorRef.current = editor}
                         inline={true}
                         key='EventSecurity4'
-                        initialValue={`<img src=${Img1} alt="news1"w-96 h-72 object-cover"/>`}
+                            initialValue={`<img src="https://i.ibb.co/GkHRXxP/support.jpg" alt="support" class="w-96 h-72 object-cover"/>`}
                         init={{
                             forced_root_block: "",
                             menubar: false,
