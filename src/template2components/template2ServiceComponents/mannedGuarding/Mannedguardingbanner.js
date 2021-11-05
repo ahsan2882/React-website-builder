3c858162935331d94f286d18852df8af1e0c23f8
Ahsan Shahid <ahsanshahid2882@gmail.com>
2021-11-05 18:51:59 +0500

    Update Mannedguardingbanner.js

--- src/template2components/template2ServiceComponents/mannedGuarding/Mannedguardingbanner.js
+++ src/template2components/template2ServiceComponents/mannedGuarding/Mannedguardingbanner.js
@@ -1,6 +1,6 @@
 import React, { useRef } from 'react'
 import MannedBannerCss from './Mannedguardingbanner.module.css'
-import Img1 from '../../../assets/template2images/manned-banner.jpg'
+// import Img1 from '../../../assets/template2images/manned-banner.jpg'
 import { Editor } from '@tinymce/tinymce-react';
 import { Navbar2 } from '../../Navbar2';
 
@@ -35,7 +35,7 @@ export default function Mannedguardingbanner({ displayDevice, linksfunc }) {
                 onInit={(evt, editor) => editorRef.current = editor}
                 inline={true}
                 key='EventSecurity4'
-                initialValue={`<img src="${Img1}" alt="technology-product" class="w-full h-full object-cover"/>`}
+                initialValue={`<img src="https://i.ibb.co/JzJDvk6/manned-banner.jpg" alt="manned-banner" class="w-full h-full object-cover"/>`}
                 init={{
                     forced_root_block: "",
                     menubar: false,
