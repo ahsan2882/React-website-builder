07199cc8c8fc1712b0a0f31692809ff7781019dc
Ahsan Shahid <ahsanshahid2882@gmail.com>
2021-11-05 16:05:59 +0500

    Update Eventsecuritybanner.js

--- src/template2components/template2ServiceComponents/eventSecurity/Eventsecuritybanner.js
+++ src/template2components/template2ServiceComponents/eventSecurity/Eventsecuritybanner.js
@@ -2,13 +2,16 @@ import React, { useRef } from 'react'
 import EventsecurityCss from './Eventsecuritybanner.module.css'
 import Img1 from '../../../assets/template2images/event-secure-banner.jpg'
 import { Editor } from '@tinymce/tinymce-react';
-export default function Eventsecuritybanner() {
+import { Navbar2 } from '../../Navbar2';
+
+export default function Eventsecuritybanner({ displayDevice, linksfunc }) {
     const editorRef = useRef(null);
     return (
         <section className={EventsecurityCss['header-img-serv']}>
-        <div className={EventsecurityCss['head-img-text']}>
-            <h1 className={EventsecurityCss.head1}>\
-            <Editor
+            <Navbar2 displayDevice={displayDevice} linksfunc={linksfunc} />
+            <div className={EventsecurityCss['head-img-text']}>
+                <h1 className={EventsecurityCss.head1}>\
+                    <Editor
                         onInit={(evt, editor) => editorRef.current = editor}
                         inline={true}
                         key='Event Security2'
@@ -25,30 +28,30 @@ export default function Eventsecuritybanner() {
                             font_formats: "Andale Mono=andale mono,times; Arial=arial,helvetica,sans-serif; Arial Black=arial black,avant garde; Book Antiqua=book antiqua,palatino; Comic Sans MS=comic sans ms,sans-serif; Courier New=courier new,courier; Georgia=georgia,palatino; Helvetica=helvetica; Impact=impact,chicago; Symbol=symbol; Tahoma=tahoma,arial,helvetica,sans-serif; Terminal=terminal,monaco; Times New Roman=times new roman,times; Trebuchet MS=trebuchet ms,geneva; Verdana=verdana,geneva; Webdings=webdings; Wingdings=wingdings,zapf dingbats"
                         }}
                     />
-              </h1>
-            <div className={EventsecurityCss.dash}></div>
-            
-        </div>
-        <Editor
-                        onInit={(evt, editor) => editorRef.current = editor}
-                        inline={true}
-                        key='EventSecurity4'
-                        initialValue={`<img src=${Img1} alt="event-security" w-96 h-72 object-cover"/>`}
-                        init={{
-                            forced_root_block: "",
-                            menubar: false,
-                            images_upload_handler: function (blobInfo, success, failure) {
-                                success("data:" + blobInfo.blob().type + ";base64," + blobInfo.base64());
-                            },
-                            images_upload_url: '/upload-images',
-                            plugins: [
-                                "image"
-                            ],
-                            toolbar: 'image'
-                        }}
-                    />
-         
-        
-    </section>
+                </h1>
+                <div className={EventsecurityCss.dash}></div>
+
+            </div>
+            <Editor
+                onInit={(evt, editor) => editorRef.current = editor}
+                inline={true}
+                key='EventSecurity4'
+                initialValue={`<img src="${Img1}" alt="event-security" class="w-full h-full object-cover"/>`}
+                init={{
+                    forced_root_block: "",
+                    menubar: false,
+                    images_upload_handler: function (blobInfo, success, failure) {
+                        success("data:" + blobInfo.blob().type + ";base64," + blobInfo.base64());
+                    },
+                    images_upload_url: '/upload-images',
+                    plugins: [
+                        "image"
+                    ],
+                    toolbar: 'image'
+                }}
+            />
+
+
+        </section>
     )
 }
