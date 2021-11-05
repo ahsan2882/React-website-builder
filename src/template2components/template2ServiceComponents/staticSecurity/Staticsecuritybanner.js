c81ce08a573233d67b742394da95aae67b36954b
Ahsan Shahid <ahsanshahid2882@gmail.com>
2021-11-05 11:28:50 +0500

    Update Staticsecuritybanner.js

--- src/template2components/template2ServiceComponents/staticSecurity/Staticsecuritybanner.js
+++ src/template2components/template2ServiceComponents/staticSecurity/Staticsecuritybanner.js
@@ -1,15 +1,53 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import StaticsecurityCss from './staticsecuritybanner.module.css'
 import Img1 from '../../../assets/template2images/static-security-banner.jpg'
+import { Editor } from '@tinymce/tinymce-react';
 export default function Staticsecuritybanner() {
+    const editorRef = useRef(null);
     return (
         <section className={StaticsecurityCss['header-img-serv']}>
         <div class={StaticsecurityCss['head-img-text']}>
-            <h1 className={StaticsecurityCss.head1}>Static Security</h1>
+            <h1 className={StaticsecurityCss.head1}>
+            <Editor
+                        onInit={(evt, editor) => editorRef.current = editor}
+                        inline={true}
+                        key='Guard1'
+                        initialValue="Static Security"
+                        init={{
+                            plugins: [
+                                "link"
+                            ],
+                            forced_root_block: "",
+                            menubar: false,
+                            toolbar: 'bold italic underline | link | fontsizeselect | backcolor forecolor | alignleft aligncenter ' +
+                                'alignright alignjustify | fontselect',
+                            fontsize_formats: "8px 9px 10px 11px 12px 14px 16px 18px 20px 24px 30px 36px 48px 60px 72px 96px",
+                            font_formats: "Andale Mono=andale mono,times; Arial=arial,helvetica,sans-serif; Arial Black=arial black,avant garde; Book Antiqua=book antiqua,palatino; Comic Sans MS=comic sans ms,sans-serif; Courier New=courier new,courier; Georgia=georgia,palatino; Helvetica=helvetica; Impact=impact,chicago; Symbol=symbol; Tahoma=tahoma,arial,helvetica,sans-serif; Terminal=terminal,monaco; Times New Roman=times new roman,times; Trebuchet MS=trebuchet ms,geneva; Verdana=verdana,geneva; Webdings=webdings; Wingdings=wingdings,zapf dingbats"
+                        }}
+                    />
+                </h1>
             <div className={StaticsecurityCss.dash}></div>
             
         </div>
-         <img src={Img1} alt="static-security"/>
+        <Editor
+                onInit={(evt, editor) => editorRef.current = editor}
+                inline={true}
+                key='EventSecurity4'
+                initialValue={`<img src=${Img1} alt="static-security" w-96 h-72 object-cover"/>`}
+                init={{
+                    forced_root_block: "",
+                    menubar: false,
+                    images_upload_handler: function (blobInfo, success, failure) {
+                        success("data:" + blobInfo.blob().type + ";base64," + blobInfo.base64());
+                    },
+                    images_upload_url: '/upload-images',
+                    plugins: [
+                        "image"
+                    ],
+                    toolbar: 'image'
+                }}
+            />
+       
         
     </section>
     )
