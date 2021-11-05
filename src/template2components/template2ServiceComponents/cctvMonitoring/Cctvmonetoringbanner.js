93c9bac14d8fbf6ab3e9e6a195b1857ae3bf8c69
Ahsan Shahid <ahsanshahid2882@gmail.com>
2021-11-05 16:05:54 +0500

    Update Cctvmonetoringbanner.js

--- src/template2components/template2ServiceComponents/cctvMonitoring/Cctvmonetoringbanner.js
+++ src/template2components/template2ServiceComponents/cctvMonitoring/Cctvmonetoringbanner.js
@@ -2,13 +2,15 @@ import React, { useRef } from 'react'
 import CctvmonetoringCss from './Cctvmonetoringbanner.module.css'
 import Img1 from '../../../assets/template2images/cctv-banner.jpg'
 import { Editor } from '@tinymce/tinymce-react';
-export default function Cctvmonetoringbanner() {
+import { Navbar2 } from '../../Navbar2';
+export default function Cctvmonetoringbanner({ displayDevice, linksfunc }) {
     const editorRef = useRef(null);
     return (
         <section className={CctvmonetoringCss['header-img-serv']}>
-        <div className={CctvmonetoringCss['head-img-text']}>
-            <h1 className={CctvmonetoringCss.head1}>
-            <Editor
+            <Navbar2 displayDevice={displayDevice} linksfunc={linksfunc} />
+            <div className={CctvmonetoringCss['head-img-text']}>
+                <h1 className={CctvmonetoringCss.head1}>
+                    <Editor
                         onInit={(evt, editor) => editorRef.current = editor}
                         inline={true}
                         key='Servillance1'
@@ -24,30 +26,30 @@ export default function Cctvmonetoringbanner() {
                             fontsize_formats: "8px 9px 10px 11px 12px 14px 16px 18px 20px 24px 30px 36px 48px 60px 72px 96px",
                             font_formats: "Andale Mono=andale mono,times; Arial=arial,helvetica,sans-serif; Arial Black=arial black,avant garde; Book Antiqua=book antiqua,palatino; Comic Sans MS=comic sans ms,sans-serif; Courier New=courier new,courier; Georgia=georgia,palatino; Helvetica=helvetica; Impact=impact,chicago; Symbol=symbol; Tahoma=tahoma,arial,helvetica,sans-serif; Terminal=terminal,monaco; Times New Roman=times new roman,times; Trebuchet MS=trebuchet ms,geneva; Verdana=verdana,geneva; Webdings=webdings; Wingdings=wingdings,zapf dingbats"
                         }}
-                    />  
-                </h1>
-            <div className={CctvmonetoringCss.dash}></div>
-            
-        </div>
-        <Editor
-                        onInit={(evt, editor) => editorRef.current = editor}
-                        inline={true}
-                        key='Servillance4'
-                        initialValue={`<img src=${Img1} alt="cctv-monetoring"w-96 h-72 object-cover"/>`}
-                        init={{
-                            forced_root_block: "",
-                            menubar: false,
-                            images_upload_handler: function (blobInfo, success, failure) {
-                                success("data:" + blobInfo.blob().type + ";base64," + blobInfo.base64());
-                            },
-                            images_upload_url: '/upload-images',
-                            plugins: [
-                                "image"
-                            ],
-                            toolbar: 'image'
-                        }}
                     />
-        
-    </section>
+                </h1>
+                <div className={CctvmonetoringCss.dash}></div>
+
+            </div>
+            <Editor
+                onInit={(evt, editor) => editorRef.current = editor}
+                inline={true}
+                key='Servillance4'
+                initialValue={`<img src="${Img1}" alt="cctv-monetoring" class="w-full h-full object-cover"/>`}
+                init={{
+                    forced_root_block: "",
+                    menubar: false,
+                    images_upload_handler: function (blobInfo, success, failure) {
+                        success("data:" + blobInfo.blob().type + ";base64," + blobInfo.base64());
+                    },
+                    images_upload_url: '/upload-images',
+                    plugins: [
+                        "image"
+                    ],
+                    toolbar: 'image'
+                }}
+            />
+
+        </section>
     )
 }
