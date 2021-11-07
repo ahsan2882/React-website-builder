80fa791727f42e345927edcfc0f279a72cabce5b
Ahsan Shahid <ahsanshahid2882@gmail.com>
2021-11-07 16:52:25 +0500

    Update Accreditation4.js

--- src/template4components/Accreditation4.js
+++ src/template4components/Accreditation4.js
@@ -7,6 +7,22 @@ import Accre4Css from './Accreditation4.module.css';
 export default function Accreditation4({ showPopup, overSection, displayDevice }) {
     const [currentBackground, setCurrentBackground] = useState("white");
     const editorRef = useRef(null);
+    const Accreditation4Array = [
+        "https://i.ibb.co/cXXJYMk/client1.png",
+        "https://i.ibb.co/dBjdw5Z/client2.png",
+        "https://i.ibb.co/4P60VRN/client3.png",
+        "https://i.ibb.co/92C0Hwn/client4.png"
+    ]
+    const [accred4, setAccred4] = useState([...Accreditation4Array]);
+    const [accredKey, setAccredKey] = useState("")
+    const addAccred = () => {
+        setAccred4([...accred4, Accreditation4Array[1]])
+    }
+    const removeAccred = (index) => {
+        let newArray = [...accred4];
+        newArray.splice(index, 1);
+        setAccred4([...newArray])
+    }
     return (
         <>
             <section className="py-32 px-0 w-full" style={{ background: currentBackground }}>
@@ -59,185 +75,60 @@ export default function Accreditation4({ showPopup, overSection, displayDevice }
                         </h1>
                     </div>
                     <div className={displayDevice ? `${Accre4Css.flexWrapped}` : `${Accre4Css.flexWrappedM}`}>
-                        <div className={`${Accre4Css.accreCardWrapper} bg-black m-5`}>
-                            <div className="p-5 flex flex-col items-center">
-                                {/* <img src={cl1} alt="client 1" className="w-full" /> */}
-                                <Editor
-                                    onInit={(evt, editor) => editorRef.current = editor}
-                                    inline={true}
-                                    key='AccreIm1'
-                                    initialValue={`<img src="https://i.ibb.co/cXXJYMk/client1.png" alt="client 1" class="w-72 h-36 object-cover"/>`}
-                                    init={{
-                                        images_upload_handler: function (blobInfo, success, failure) {
-                                            success("data:" + blobInfo.blob().type + ";base64," + blobInfo.base64());
-                                        },
-                                        images_upload_url: '/upload-images',
-                                        forced_root_block: "",
-                                        menubar: false,
-                                        plugins: [
-                                            "image"
-                                        ],
-                                        toolbar: 'image'
-                                    }}
-                                />
-                                {/* <TextH3 classStyle={Accre4Css.accreH3} text="Lorem ipsum." /> */}
-                                <h3 style={displayDevice ? null : { fontSize: "0.8rem" }}>
+                        {accred4.map((item, index) => 
+                            <div className={`${Accre4Css.accreCardWrapper} bg-black m-5 relative`}
+                                onMouseEnter={() => setAccredKey(String(index))}
+                                onMouseLeave={() => setAccredKey("")}
+                            >
+                                <div className="p-5 flex flex-col items-center">
                                     <Editor
                                         onInit={(evt, editor) => editorRef.current = editor}
                                         inline={true}
-                                        key='accre4ed3'
-                                        tinymceScriptSrc={process.env.PUBLIC_URL + '/tinymce/tinymce.min.js'}
-                                        initialValue="Typing..."
+                                        key={`Accre4Im${index}`}
+                                        initialValue={`<img src="${item}" alt="client 3" class="w-72 h-36 object-cover"/>`}
                                         init={{
-                                            plugins: [
-                                                "link"
-                                            ],
+                                            images_upload_handler: function (blobInfo, success, failure) {
+                                                success("data:" + blobInfo.blob().type + ";base64," + blobInfo.base64());
+                                            },
+                                            images_upload_url: '/upload-images',
                                             forced_root_block: "",
                                             menubar: false,
-                                            toolbar: 'bold italic underline | link | fontsizeselect | backcolor forecolor | alignleft aligncenter ' +
-                                                'alignright alignjustify | fontselect',
-                                            fontsize_formats: "8px 9px 10px 11px 12px 14px 16px 18px 20px 24px 30px 36px 48px 60px 72px 96px",
-                                            font_formats: "Andale Mono=andale mono,times; Arial=arial,helvetica,sans-serif; Arial Black=arial black,avant garde; Book Antiqua=book antiqua,palatino; Comic Sans MS=comic sans ms,sans-serif; Courier New=courier new,courier; Georgia=georgia,palatino; Helvetica=helvetica; Impact=impact,chicago; Symbol=symbol; Tahoma=tahoma,arial,helvetica,sans-serif; Terminal=terminal,monaco; Times New Roman=times new roman,times; Trebuchet MS=trebuchet ms,geneva; Verdana=verdana,geneva; Webdings=webdings; Wingdings=wingdings,zapf dingbats"
-
-                                        }}
-                                    />
-                                </h3>
-                            </div>
-                        </div>
-
-                        <div className={`${Accre4Css.accreCardWrapper} bg-black m-5`}>
-                            <div className="p-5 flex flex-col items-center">
-                                {/* <img src={cl2} alt="client 2" className="w-full" /> */}
-                                <Editor
-                                    onInit={(evt, editor) => editorRef.current = editor}
-                                    inline={true}
-                                    key='AccreIm2'
-                                    initialValue={`<img src="https://i.ibb.co/dBjdw5Z/client2.png" alt="client 2" class="w-72 h-36 object-cover"/>`}
-                                    init={{
-                                        images_upload_handler: function (blobInfo, success, failure) {
-                                            success("data:" + blobInfo.blob().type + ";base64," + blobInfo.base64());
-                                        },
-                                        images_upload_url: '/upload-images',
-                                        forced_root_block: "",
-                                        menubar: false,
-                                        plugins: [
-                                            "image"
-                                        ],
-                                        toolbar: 'image'
-                                    }}
-                                />
-                                <h3>
-                                    <Editor
-                                        onInit={(evt, editor) => editorRef.current = editor}
-                                        inline={true}
-                                        key='accre4ed4'
-                                        tinymceScriptSrc={process.env.PUBLIC_URL + '/tinymce/tinymce.min.js'}
-                                        initialValue="Typing..."
-                                        init={{
                                             plugins: [
-                                                "link"
+                                                "image"
                                             ],
-                                            forced_root_block: "",
-                                            menubar: false,
-                                            toolbar: 'bold italic underline | link | fontsizeselect | backcolor forecolor | alignleft aligncenter ' +
-                                                'alignright alignjustify | fontselect',
-                                            fontsize_formats: "8px 9px 10px 11px 12px 14px 16px 18px 20px 24px 30px 36px 48px 60px 72px 96px",
-                                            font_formats: "Andale Mono=andale mono,times; Arial=arial,helvetica,sans-serif; Arial Black=arial black,avant garde; Book Antiqua=book antiqua,palatino; Comic Sans MS=comic sans ms,sans-serif; Courier New=courier new,courier; Georgia=georgia,palatino; Helvetica=helvetica; Impact=impact,chicago; Symbol=symbol; Tahoma=tahoma,arial,helvetica,sans-serif; Terminal=terminal,monaco; Times New Roman=times new roman,times; Trebuchet MS=trebuchet ms,geneva; Verdana=verdana,geneva; Webdings=webdings; Wingdings=wingdings,zapf dingbats"
-
+                                            toolbar: 'image'
                                         }}
                                     />
-                                </h3>
+                                    <h3 className="text-center">
+                                        <Editor
+                                            onInit={(evt, editor) => editorRef.current = editor}
+                                            inline={true}
+                                            key={`Accre4H${index}`}
+                                            tinymceScriptSrc={process.env.PUBLIC_URL + '/tinymce/tinymce.min.js'}
+                                            initialValue="Lorem ipsum"
+                                            init={{
+                                                plugins: [
+                                                    "link"
+                                                ],
+                                                forced_root_block: "",
+                                                menubar: false,
+                                                toolbar: 'bold italic underline | link | fontsizeselect | backcolor forecolor | alignleft aligncenter ' +
+                                                    'alignright alignjustify | fontselect',
+                                                fontsize_formats: "8px 9px 10px 11px 12px 14px 16px 18px 20px 24px 30px 36px 48px 60px 72px 96px",
+                                                font_formats: "Andale Mono=andale mono,times; Arial=arial,helvetica,sans-serif; Arial Black=arial black,avant garde; Book Antiqua=book antiqua,palatino; Comic Sans MS=comic sans ms,sans-serif; Courier New=courier new,courier; Georgia=georgia,palatino; Helvetica=helvetica; Impact=impact,chicago; Symbol=symbol; Tahoma=tahoma,arial,helvetica,sans-serif; Terminal=terminal,monaco; Times New Roman=times new roman,times; Trebuchet MS=trebuchet ms,geneva; Verdana=verdana,geneva; Webdings=webdings; Wingdings=wingdings,zapf dingbats"
+                                            }}
+                                        />
+                                    </h3>
+                                </div>
+                                <div className="toBeRemoved" style={accredKey === String(index) ? { position: "absolute", top: "2%", right: "2%" } : { display: "none" }}>
+                                    <button className="w-8 h-8 bg-BL-600" onClick={() => removeAccred(index)}><i className="fas fa-trash-alt"></i></button>
+                                </div>
                             </div>
-                        </div>
-
-                        <div className={`${Accre4Css.accreCardWrapper} bg-black m-5`}>
-                            <div className="p-5 flex flex-col items-center">
-                                {/* <img src={cl3} alt="client 3" className="w-full" /> */}
-                                <Editor
-                                    onInit={(evt, editor) => editorRef.current = editor}
-                                    inline={true}
-                                    key='AccreIm3'
-                                    initialValue={`<img src="https://i.ibb.co/4P60VRN/client3.png" alt="client 3" class="w-72 h-36 object-cover"/>`}
-                                    init={{
-                                        images_upload_handler: function (blobInfo, success, failure) {
-                                            success("data:" + blobInfo.blob().type + ";base64," + blobInfo.base64());
-                                        },
-                                        images_upload_url: '/upload-images',
-                                        forced_root_block: "",
-                                        menubar: false,
-                                        plugins: [
-                                            "image"
-                                        ],
-                                        toolbar: 'image'
-                                    }}
-                                />
-                                <h3>
-                                    <Editor
-                                        onInit={(evt, editor) => editorRef.current = editor}
-                                        inline={true}
-                                        key='accre4ed5'
-                                        tinymceScriptSrc={process.env.PUBLIC_URL + '/tinymce/tinymce.min.js'}
-                                        initialValue="Typing..."
-                                        init={{
-                                            plugins: [
-                                                "link"
-                                            ],
-                                            forced_root_block: "",
-                                            menubar: false,
-                                            toolbar: 'bold italic underline | link | fontsizeselect | backcolor forecolor | alignleft aligncenter ' +
-                                                'alignright alignjustify | fontselect',
-                                            fontsize_formats: "8px 9px 10px 11px 12px 14px 16px 18px 20px 24px 30px 36px 48px 60px 72px 96px",
-                                            font_formats: "Andale Mono=andale mono,times; Arial=arial,helvetica,sans-serif; Arial Black=arial black,avant garde; Book Antiqua=book antiqua,palatino; Comic Sans MS=comic sans ms,sans-serif; Courier New=courier new,courier; Georgia=georgia,palatino; Helvetica=helvetica; Impact=impact,chicago; Symbol=symbol; Tahoma=tahoma,arial,helvetica,sans-serif; Terminal=terminal,monaco; Times New Roman=times new roman,times; Trebuchet MS=trebuchet ms,geneva; Verdana=verdana,geneva; Webdings=webdings; Wingdings=wingdings,zapf dingbats"
-                                        }}
-                                    />
-                                </h3>
-                            </div>
-                        </div>
-
-                        <div className={`${Accre4Css.accreCardWrapper} bg-black m-5`}>
-                            <div className="p-5 flex flex-col items-center">
-                                {/* <img src={cl4} alt="client 4" className="w-full" /> */}
-                                <Editor
-                                    onInit={(evt, editor) => editorRef.current = editor}
-                                    inline={true}
-                                    key='AccreIm4'
-                                    // initialValue={`<img src=${cl4} alt="client 4" class="w-72 h-36 object-cover"/>`}
-                                    initialValue={`<img src="https://i.ibb.co/92C0Hwn/client4.png" alt="client 4" class="w-72 h-36 object-cover"/>`}
-                                    init={{
-                                        images_upload_handler: function (blobInfo, success, failure) {
-                                            success("data:" + blobInfo.blob().type + ";base64," + blobInfo.base64());
-                                        },
-                                        images_upload_url: '/upload-images',
-                                        forced_root_block: "",
-                                        menubar: false,
-                                        plugins: [
-                                            "image"
-                                        ],
-                                        toolbar: 'image'
-                                    }}
-                                />
-                                <h3>
-                                    <Editor
-                                        onInit={(evt, editor) => editorRef.current = editor}
-                                        inline={true}
-                                        key='accre4ed6'
-                                        tinymceScriptSrc={process.env.PUBLIC_URL + '/tinymce/tinymce.min.js'}
-                                        initialValue="Typing..."
-                                        init={{
-                                            plugins: [
-                                                "link"
-                                            ],
-                                            forced_root_block: "",
-                                            menubar: false,
-                                            toolbar: 'bold italic underline | link | fontsizeselect | backcolor forecolor | alignleft aligncenter ' +
-                                                'alignright alignjustify | fontselect',
-                                            fontsize_formats: "8px 9px 10px 11px 12px 14px 16px 18px 20px 24px 30px 36px 48px 60px 72px 96px",
-                                            font_formats: "Andale Mono=andale mono,times; Arial=arial,helvetica,sans-serif; Arial Black=arial black,avant garde; Book Antiqua=book antiqua,palatino; Comic Sans MS=comic sans ms,sans-serif; Courier New=courier new,courier; Georgia=georgia,palatino; Helvetica=helvetica; Impact=impact,chicago; Symbol=symbol; Tahoma=tahoma,arial,helvetica,sans-serif; Terminal=terminal,monaco; Times New Roman=times new roman,times; Trebuchet MS=trebuchet ms,geneva; Verdana=verdana,geneva; Webdings=webdings; Wingdings=wingdings,zapf dingbats"
-                                        }}
-                                    />
-                                </h3>
-                            </div>
-                        </div>
+                        )}
+                        <button onClick={() => addAccred()} className="border-2 border-gray-400 text-gray-500 toBeRemoved w-40 h-40 flex justify-evenly flex-col text-center items-center">
+                            <i className="fas fa-plus text-3xl"></i>
+                            <h2>Add another Accreditation</h2>
+                        </button>
                     </div>
                 </div>
             </section>
