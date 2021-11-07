49adbef99f2e4eb37f1dd149978cc49f524670c1
Ahsan Shahid <ahsanshahid2882@gmail.com>
2021-11-07 16:52:15 +0500

    Update Accrediation3.js

--- src/template3components/Accrediation3.js
+++ src/template3components/Accrediation3.js
@@ -10,9 +10,25 @@ import { Editor } from '@tinymce/tinymce-react';
 export default function Accrediation3({ showPopup, overSection }) {
   const [currentBackground, setCurrentBackground] = useState("#c01120");
   const editorRef = useRef(null);
+  const Accreditation3Array = [
+    accre1,
+    accre2,
+    accre4,
+    accre7
+  ]
+  const [acre3, setAccre3] = useState([...Accreditation3Array])
+  const [accredKey, setAccredKey] = useState("")
+  const addAccred = () => {
+    setAccre3([...acre3, Accreditation3Array[1]])
+  }
+  const removeAccred = (index) => {
+    let newArray = [...acre3];
+    newArray.splice(index, 1);
+    setAccre3([...newArray])
+  }
   return (
     <>
-      <section  className={Accrediation3Css['heading-sec']}>
+      <section className={Accrediation3Css['heading-sec']}>
 
         <div className={Accrediation3Css['heading-main-title']}>
           <div className={Accrediation3Css['sub-title']}>
@@ -56,7 +72,7 @@ export default function Accrediation3({ showPopup, overSection }) {
         <div className={Accrediation3Css['container-accre']}>
           <div className={Accrediation3Css['accre-wrapper']}>
             <div className={Accrediation3Css.arow}>
-              <div className={Accrediation3Css['a-card']}>
+              {/* <div className={Accrediation3Css['a-card']}>
                 <Editor
                   onInit={(evt, editor) => editorRef.current = editor}
                   inline={true}
@@ -76,170 +92,52 @@ export default function Accrediation3({ showPopup, overSection }) {
                   }}
                 />
 
-              </div>
-              <div className={Accrediation3Css['a-card']}>
-                <Editor
-                  onInit={(evt, editor) => editorRef.current = editor}
-                  inline={true}
-                  key='Accre4'
-                  initialValue={`<img src=${accre2} alt="accre2" class="w-24 h-24 object-cover"/>`}
-                  init={{
-                    images_upload_handler: function (blobInfo, success, failure) {
-                      success("data:" + blobInfo.blob().type + ";base64," + blobInfo.base64());
-                    },
-                    images_upload_url: '/upload-images',
-                    forced_root_block: "",
-                    menubar: false,
-                    plugins: [
-                      "image"
-                    ],
-                    toolbar: 'image'
-                  }}
-                />
-
-
-              </div>
-              <div className={Accrediation3Css['a-card']}>
-                <Editor
-                  onInit={(evt, editor) => editorRef.current = editor}
-                  inline={true}
-                  key='Accre5'
-                  initialValue={`<img src=${accre1} alt="accre3" class="w-24 h-24 object-cover"/>`}
-                  init={{
-                    images_upload_handler: function (blobInfo, success, failure) {
-                      success("data:" + blobInfo.blob().type + ";base64," + blobInfo.base64());
-                    },
-                    images_upload_url: '/upload-images',
-                    forced_root_block: "",
-                    menubar: false,
-                    plugins: [
-                      "image"
-                    ],
-                    toolbar: 'image'
-                  }}
-                />
-
-
-              </div>
-              <div className={Accrediation3Css['a-card']}>
-
-                <Editor
-                  onInit={(evt, editor) => editorRef.current = editor}
-                  inline={true}
-                  key='Accre6'
-                  initialValue={`<img src=${accre2} alt="accre4" class="w-24 h-24 object-cover"/>`}
-                  init={{
-                    images_upload_handler: function (blobInfo, success, failure) {
-                      success("data:" + blobInfo.blob().type + ";base64," + blobInfo.base64());
-                    },
-                    images_upload_url: '/upload-images',
-                    forced_root_block: "",
-                    menubar: false,
-                    plugins: [
-                      "image"
-                    ],
-                    toolbar: 'image'
-                  }}
-                />
-
-              </div>
-              <div className={Accrediation3Css['a-card']}>
-                <Editor
-                  onInit={(evt, editor) => editorRef.current = editor}
-                  inline={true}
-                  key='Accre6'
-                  initialValue={`<img src=${accre4} alt="accre5" class="w-24 h-24 object-cover"/>`}
-                  init={{
-                    images_upload_handler: function (blobInfo, success, failure) {
-                      success("data:" + blobInfo.blob().type + ";base64," + blobInfo.base64());
-                    },
-                    images_upload_url: '/upload-images',
-                    forced_root_block: "",
-                    menubar: false,
-                    plugins: [
-                      "image"
-                    ],
-                    toolbar: 'image'
-                  }}
-                />
-
-
-              </div>
-              <div className={Accrediation3Css['a-card']}>
-
-                <Editor
-                  onInit={(evt, editor) => editorRef.current = editor}
-                  inline={true}
-                  key='Accre6'
-                  initialValue={`<img src=${accre7} alt="accre6" class="w-24 h-24 object-cover"/>`}
-                  init={{
-                    images_upload_handler: function (blobInfo, success, failure) {
-                      success("data:" + blobInfo.blob().type + ";base64," + blobInfo.base64());
-                    },
-                    images_upload_url: '/upload-images',
-                    forced_root_block: "",
-                    menubar: false,
-                    plugins: [
-                      "image"
-                    ],
-                    toolbar: 'image'
-                  }}
-                />
-              </div>
-              <div className={Accrediation3Css['a-card']}>
-                
-                <Editor
-                  onInit={(evt, editor) => editorRef.current = editor}
-                  inline={true}
-                  key='Accre6'
-                  initialValue={`<img src=${accre7} alt="accre7"  class="w-24 h-24 object-cover"/>`}
-                  init={{
-                    images_upload_handler: function (blobInfo, success, failure) {
-                      success("data:" + blobInfo.blob().type + ";base64," + blobInfo.base64());
-                    },
-                    images_upload_url: '/upload-images',
-                    forced_root_block: "",
-                    menubar: false,
-                    plugins: [
-                      "image"
-                    ],
-                    toolbar: 'image'
-                  }}
-                />
-              </div>    
-              <div className={Accrediation3Css['a-card']}>
-                <Editor
-                  onInit={(evt, editor) => editorRef.current = editor}
-                  inline={true}
-                  key='Accre6'
-                  initialValue={`<img src=${accre7} alt="accre7"  class="w-24 h-24 object-cover"/>`}
-                  init={{
-                    images_upload_handler: function (blobInfo, success, failure) {
-                      success("data:" + blobInfo.blob().type + ";base64," + blobInfo.base64());
-                    },
-                    images_upload_url: '/upload-images',
-                    forced_root_block: "",
-                    menubar: false,
-                    plugins: [
-                      "image"
-                    ],
-                    toolbar: 'image'
-                  }}
-                />
-              </div>
+              </div> */}
+              {acre3.map((item, index) =>
+                <div className={`relative ${Accrediation3Css['a-card']}`}
+                  onMouseEnter={() => setAccredKey(String(index))}
+                  onMouseLeave={() => setAccredKey("")}
+                >
+                  <Editor
+                    onInit={(evt, editor) => editorRef.current = editor}
+                    inline={true}
+                    key={`Accred3Img${index}`}
+                    initialValue={`<img src="${item}" alt="Accreditation${index}" class="w-24 h-24 object-cover"/>`}
+                    init={{
+                      images_upload_handler: function (blobInfo, success, failure) {
+                        success("data:" + blobInfo.blob().type + ";base64," + blobInfo.base64());
+                      },
+                      images_upload_url: '/upload-images',
+                      forced_root_block: "",
+                      menubar: false,
+                      plugins: [
+                        "image"
+                      ],
+                      toolbar: 'image'
+                    }}
+                  />
+                  <div className="toBeRemoved" style={accredKey === String(index) ? { position: "absolute", top: "2%", right: "2%" } : { display: "none" }}>
+                    <button className="w-8 h-8 bg-BL-600" onClick={() => removeAccred(index)}><i className="fas fa-trash-alt"></i></button>
+                  </div>
+                </div>
+              )}
+              <button onClick={() => addAccred()} className="border-2 border-gray-400 text-gray-500 toBeRemoved w-40 h-32 flex justify-evenly flex-col text-center items-center">
+                <i className="fas fa-plus text-3xl"></i>
+                <h2>Add another Accreditation</h2>
+              </button>
             </div>
           </div>
 
         </div>
       </section>
       <div style={(overSection && showPopup) ? { position: "absolute", top: "9rem", right: "5rem", zIndex: "9999999" } : { display: "none" }} className="toBeRemoved">
-                <GithubPicker
-                    triangle="top-right"
-                    onChangeComplete={(color) => {
-                        setCurrentBackground(color.hex);
-                    }}
-                />
-            </div>
+        <GithubPicker
+          triangle="top-right"
+          onChangeComplete={(color) => {
+            setCurrentBackground(color.hex);
+          }}
+        />
+      </div>
     </>
   )
 }
