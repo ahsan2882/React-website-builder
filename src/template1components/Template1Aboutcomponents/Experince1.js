b29ee49eca243d00b3800b54f14e1a90988187d4
Ahsan Shahid <ahsanshahid2882@gmail.com>
2021-11-08 21:52:35 +0500

    Create Experince1.js

---
+++ src/template1components/Template1Aboutcomponents/Experince1.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import ExpermoduleCss from './Experince1.module.css'
+import Service from './services11.jpg'
+export default function Experince1() {
+    return (
+<>
+
+<section  className={`${ExpermoduleCss['services-sec']}`}>
+        <div style={{display:`flex`,justifyContent:`space-around`}} className={`${ExpermoduleCss['grid-contact']}`}>
+            <div className={`${ExpermoduleCss['services-text']}`}>
+                <div className="back">
+                <h1 className={`${ExpermoduleCss['main-heading']}`}>
+          OUR EXPERIENCE
+                </h1>
+                </div>
+                <p className={`${ExpermoduleCss.para}`}>
+                  Our security agency is committed to helping every client who is at risk. We owe our lives to save the one of a client. We know everything about security. Our staff has valuable experience, doing this job for almost twenty years.
+                  <br/>
+                  Each security agent goes through a set of instructions and challenges to join our professional team. We promise that we won't let you down no matter how tough a situation may be.
+                </p>
+        
+            </div>
+            <div   className={`${ExpermoduleCss['service-img']}`}>
+                <img src={Service} alt="thi is "/>
+            </div>
+        
+        </div>
+        </section>
+</>
+    )
+}
