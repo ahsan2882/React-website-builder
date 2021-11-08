06bd2819a28c4ca3aa3b3013391e436957264604
Ahsan Shahid <ahsanshahid2882@gmail.com>
2021-11-08 21:53:12 +0500

    Create Contact1banner.js

---
+++ src/template1components/template1contact/Contact1banner.js
@@ -0,0 +1,15 @@
+import React from 'react'
+ import Contactbanner1Css from '../template1contactcomponent/Contact1banner.module.css'
+export default function Contact1banner() {
+    return (
+        <>
+        <section className={Contactbanner1Css['head-img2']}>
+    <div className={Contactbanner1Css.contentserv}>
+     
+     <h1 className={Contactbanner1Css.headings}>Contact Us</h1>
+    
+    </div>
+        </section>
+        </>
+    )
+}
