f8d1163d4d8edee124ac3018ba0e742c75319238
Ahsan Shahid <ahsanshahid2882@gmail.com>
2021-11-08 21:52:07 +0500

    Update Service1.js

--- src/template1components/Service1.js
+++ src/template1components/Service1.js
@@ -1,14 +1,25 @@
-import React, { useRef } from 'react'
+import React, { useRef, useState  } from 'react'
 import Service1Css from './Service1.module.css'
 import { Editor } from '@tinymce/tinymce-react';
-// import serv1 from '../assets/template1images/serv-1.png'
-// import serv2 from '../assets/template1images/serv-2.png'
-// import serv3 from '../assets/template1images/serv-3.png'
-export const Service1 = () => {
+import { GithubPicker } from 'react-color';
+
+import serv1 from '../assets/template1images/serv-1.png'
+import serv2 from '../assets/template1images/serv-2.png'
+import serv3 from '../assets/template1images/serv-3.png'
+export const Service1 = ({ showPopup, overSection }) => {
+  const [showPopup1, setShowPopup1] = useState(false);
+  const [showPopup2, setShowPopup2] = useState(false);
+  const [showPopup3, setShowPopup3] = useState(false);
+  const [currentBackground1, setCurrentBackground1] = useState("white");
+  const [currentBackground2, setCurrentBackground2] = useState("white");
+  const [currentBackground3, setCurrentBackground3] = useState("white");
+
+  const [currentBackground, setCurrentBackground] = useState("white");
+
   const editorRef = useRef(null);
   return (
     <>
-      <div id="services" className={`${Service1Css['text-main-text']}`}>
+      <div style={{ background: currentBackground }}  id="services" className={`${Service1Css['text-main-text']}`}>
         <h1 className={`${Service1Css.headings}`}>
           <Editor
             onInit={(evt, editor) => editorRef.current = editor}
@@ -42,10 +53,10 @@ export const Service1 = () => {
           />
         </div>
       </div>
-      <div className={`${Service1Css['wrapper-flex']}`}>
-        <div className={`${Service1Css['container-services']}  ${Service1Css['fade-in']}`}>
+      <div style={{ background: currentBackground }} className={`${Service1Css['wrapper-flex']}`}>
+        <div style={{ background: currentBackground1}} className={`${Service1Css['container-services']}  ${Service1Css['fade-in']}`} onClick={() => setShowPopup1(state => !state)}>
           <div className={`${Service1Css['banner-img']}`}></div>
-          <img src="https://i.ibb.co/zJs31mh/serv-1.png" alt="serv-1" className={`${Service1Css['profile-img']}`}
+          <img src={serv1} alt="profileimage" className={`${Service1Css['profile-img']}`}
           />
           <h1 className={`${Service1Css.name}`}>
             <Editor
@@ -63,23 +74,21 @@ export const Service1 = () => {
               }}
             />
           </h1>
-
-          {/* <p className={`${Service1Css.description}`}> */}
-          <Editor
-            onInit={(evt, editor) => editorRef.current = editor}
-            inline={true}
-            key='Service4'
-            initialValue={`<p class="${Service1Css.description}">Our bodyguards are strong men and professionals who served in
+            <Editor
+              onInit={(evt, editor) => editorRef.current = editor}
+              inline={true}
+              key='Service4'
+              initialValue= {`<p class="${Service1Css.description}">Our bodyguards are strong men and professionals who served in
               military forces.</p>`}
-            init={{
-              forced_root_block: "",
-              menubar: false,
-              toolbar: 'bold italic underline | fontsizeselect | backcolor forecolor | alignleft aligncenter ' +
-                'alignright alignjustify | fontselect',
-              fontsize_formats: "8px 9px 10px 11px 12px 14px 16px 18px 20px 24px 30px 36px 48px 60px 72px 96px",
-              font_formats: "Andale Mono=andale mono,times; Arial=arial,helvetica,sans-serif; Arial Black=arial black,avant garde; Book Antiqua=book antiqua,palatino; Comic Sans MS=comic sans ms,sans-serif; Courier New=courier new,courier; Georgia=georgia,palatino; Helvetica=helvetica; Impact=impact,chicago; Symbol=symbol; Tahoma=tahoma,arial,helvetica,sans-serif; Terminal=terminal,monaco; Times New Roman=times new roman,times; Trebuchet MS=trebuchet ms,geneva; Verdana=verdana,geneva; Webdings=webdings; Wingdings=wingdings,zapf dingbats"
-            }}
-          />
+              init={{
+                forced_root_block: "",
+                menubar: false,
+                toolbar: 'bold italic underline | fontsizeselect | backcolor forecolor | alignleft aligncenter ' +
+                  'alignright alignjustify | fontselect',
+                fontsize_formats: "8px 9px 10px 11px 12px 14px 16px 18px 20px 24px 30px 36px 48px 60px 72px 96px",
+                font_formats: "Andale Mono=andale mono,times; Arial=arial,helvetica,sans-serif; Arial Black=arial black,avant garde; Book Antiqua=book antiqua,palatino; Comic Sans MS=comic sans ms,sans-serif; Courier New=courier new,courier; Georgia=georgia,palatino; Helvetica=helvetica; Impact=impact,chicago; Symbol=symbol; Tahoma=tahoma,arial,helvetica,sans-serif; Terminal=terminal,monaco; Times New Roman=times new roman,times; Trebuchet MS=trebuchet ms,geneva; Verdana=verdana,geneva; Webdings=webdings; Wingdings=wingdings,zapf dingbats"
+              }}
+            />
           {/* </p> */}
 
           <a style={{ marginBottom: `20px` }} className={`${Service1Css.btnb}`} href={2}
@@ -100,11 +109,18 @@ export const Service1 = () => {
             />
             <i className={`${Service1Css['fas fa-long-arrow-alt-right']}`}></i></a>
         </div>
-
-        <div className={`${Service1Css['container-services']} ${Service1Css['fade-in']}`}>
+        <div style={(showPopup1) ?   { position: "absolute", top: "9rem", right: "5rem", zIndex: "9999999" } : { display: "none" }} className="toBeRemoved">
+                <GithubPicker
+                    triangle="top-right"
+                    onChangeComplete={(color) => {
+                        setCurrentBackground1(color.hex);
+                    }}
+                />
+            </div>
+        <div style={{ background: currentBackground2}}  className={`${Service1Css['container-services']} ${Service1Css['fade-in']}`} onClick={() => setShowPopup2(state => !state)}>
           <div className={`${Service1Css['banner-img']}`}></div>
 
-          <img src="https://i.ibb.co/dG4RKvb/serv-2.png" alt="serv-2" className={`${Service1Css['profile-img']}`}
+          <img src={serv2} alt="profileimage" className={`${Service1Css['profile-img']}`}
           />
           <h1 className={`${Service1Css.name}`}>
             <Editor
@@ -123,23 +139,23 @@ export const Service1 = () => {
             />
           </h1>
           {/* <p className={`${Service1Css.description}`}> */}
-          <Editor
-            onInit={(evt, editor) => editorRef.current = editor}
-            inline={true}
-            key='Service6'
-            initialValue={`<p class="${Service1Css.description}">Settle a guard house behind your facility and we'll monitor the
+            <Editor
+              onInit={(evt, editor) => editorRef.current = editor}
+              inline={true}
+              key='Service6'
+              initialValue={`<p class="${Service1Css.description}">Settle a guard house behind your facility and we'll monitor the
               activity around the house.</p>`}
-            init={{
-              forced_root_block: "",
-              menubar: false,
-              toolbar: 'bold italic underline | fontsizeselect | backcolor forecolor | alignleft aligncenter ' +
-                'alignright alignjustify | fontselect',
-              fontsize_formats: "8px 9px 10px 11px 12px 14px 16px 18px 20px 24px 30px 36px 48px 60px 72px 96px",
-              font_formats: "Andale Mono=andale mono,times; Arial=arial,helvetica,sans-serif; Arial Black=arial black,avant garde; Book Antiqua=book antiqua,palatino; Comic Sans MS=comic sans ms,sans-serif; Courier New=courier new,courier; Georgia=georgia,palatino; Helvetica=helvetica; Impact=impact,chicago; Symbol=symbol; Tahoma=tahoma,arial,helvetica,sans-serif; Terminal=terminal,monaco; Times New Roman=times new roman,times; Trebuchet MS=trebuchet ms,geneva; Verdana=verdana,geneva; Webdings=webdings; Wingdings=wingdings,zapf dingbats"
-            }}
-          />
+              init={{
+                forced_root_block: "",
+                menubar: false,
+                toolbar: 'bold italic underline | fontsizeselect | backcolor forecolor | alignleft aligncenter ' +
+                  'alignright alignjustify | fontselect',
+                fontsize_formats: "8px 9px 10px 11px 12px 14px 16px 18px 20px 24px 30px 36px 48px 60px 72px 96px",
+                font_formats: "Andale Mono=andale mono,times; Arial=arial,helvetica,sans-serif; Arial Black=arial black,avant garde; Book Antiqua=book antiqua,palatino; Comic Sans MS=comic sans ms,sans-serif; Courier New=courier new,courier; Georgia=georgia,palatino; Helvetica=helvetica; Impact=impact,chicago; Symbol=symbol; Tahoma=tahoma,arial,helvetica,sans-serif; Terminal=terminal,monaco; Times New Roman=times new roman,times; Trebuchet MS=trebuchet ms,geneva; Verdana=verdana,geneva; Webdings=webdings; Wingdings=wingdings,zapf dingbats"
+              }}
+            />
 
-          {/*  </p> */}
+         {/*  </p> */}
 
           <a style={{ marginBottom: `20px` }} className={`${Service1Css.btnb}`} href={2}
           >
@@ -159,10 +175,18 @@ export const Service1 = () => {
             />
             <i className={`${Service1Css['fas fa-long-arrow-alt-right']}`}></i></a>
         </div>
-        <div className={`${Service1Css['container-services']} ${Service1Css['fade-in']}`}>
+        <div style={(showPopup2)   ? { position: "absolute", top: "9rem", right: "5rem", zIndex: "9999999" } : { display: "none" }} className="toBeRemoved">
+                <GithubPicker
+                    triangle="top-right"
+                    onChangeComplete={(color) => {
+                        setCurrentBackground2(color.hex);
+                    }}
+                />
+            </div>
+        <div  style={{ background: currentBackground3}} className={`${Service1Css['container-services']} ${Service1Css['fade-in']}`} onClick={() => setShowPopup3(state => !state)}>
           <div className={`${Service1Css['banner-img']}`}></div>
 
-          <img src="https://i.ibb.co/PDB2XJM/serv-3.png" alt="serv-3" className={`${Service1Css['profile-img']}`}
+          <img src={serv3} alt="profileImage" className={`${Service1Css['profile-img']}`}
           />
           <h1 className={`${Service1Css.name}`}>
             <Editor
@@ -180,47 +204,62 @@ export const Service1 = () => {
               }}
             />
           </h1>
-
+         
           {/* <p className={`${Service1Css.description}`}> */}
           <Editor
-            onInit={(evt, editor) => editorRef.current = editor}
-            inline={true}
-            key='Service9'
-            initialValue={`<p class="${Service1Css.description}">our strong men and professionals will do 24/7 security surveillance
+          onInit={(evt, editor) => editorRef.current = editor}
+          inline={true}
+          key='Service9'
+          initialValue={`<p class="${Service1Css.description}">our strong men and professionals will do 24/7 security surveillance
           of your house with focus .</p>`}
-            init={{
-              forced_root_block: "",
-              menubar: false,
-              toolbar: 'bold italic underline | fontsizeselect | backcolor forecolor | alignleft aligncenter ' +
-                'alignright alignjustify | fontselect',
-              fontsize_formats: "8px 9px 10px 11px 12px 14px 16px 18px 20px 24px 30px 36px 48px 60px 72px 96px",
-              font_formats: "Andale Mono=andale mono,times; Arial=arial,helvetica,sans-serif; Arial Black=arial black,avant garde; Book Antiqua=book antiqua,palatino; Comic Sans MS=comic sans ms,sans-serif; Courier New=courier new,courier; Georgia=georgia,palatino; Helvetica=helvetica; Impact=impact,chicago; Symbol=symbol; Tahoma=tahoma,arial,helvetica,sans-serif; Terminal=terminal,monaco; Times New Roman=times new roman,times; Trebuchet MS=trebuchet ms,geneva; Verdana=verdana,geneva; Webdings=webdings; Wingdings=wingdings,zapf dingbats"
-            }}
-          />
+          init={{
+            forced_root_block: "",
+               menubar: false,
+            toolbar: 'bold italic underline | fontsizeselect | backcolor forecolor | alignleft aligncenter ' +
+              'alignright alignjustify | fontselect',
+            fontsize_formats: "8px 9px 10px 11px 12px 14px 16px 18px 20px 24px 30px 36px 48px 60px 72px 96px",
+            font_formats: "Andale Mono=andale mono,times; Arial=arial,helvetica,sans-serif; Arial Black=arial black,avant garde; Book Antiqua=book antiqua,palatino; Comic Sans MS=comic sans ms,sans-serif; Courier New=courier new,courier; Georgia=georgia,palatino; Helvetica=helvetica; Impact=impact,chicago; Symbol=symbol; Tahoma=tahoma,arial,helvetica,sans-serif; Terminal=terminal,monaco; Times New Roman=times new roman,times; Trebuchet MS=trebuchet ms,geneva; Verdana=verdana,geneva; Webdings=webdings; Wingdings=wingdings,zapf dingbats"
+          }}
+        />
           {/* </p> */}
 
           <a style={{ marginBottom: `20px` }} className={`${Service1Css.btnb}`} href={2}
           >
             <Editor
-              onInit={(evt, editor) => editorRef.current = editor}
-              inline={true}
-              key='Service10'
-              initialValue="Sign Up"
-              init={{
-                forced_root_block: "",
-                menubar: false,
-                toolbar: 'bold italic underline | fontsizeselect | backcolor forecolor | alignleft aligncenter ' +
-                  'alignright alignjustify | fontselect',
-                fontsize_formats: "8px 9px 10px 11px 12px 14px 16px 18px 20px 24px 30px 36px 48px 60px 72px 96px",
-                font_formats: "Andale Mono=andale mono,times; Arial=arial,helvetica,sans-serif; Arial Black=arial black,avant garde; Book Antiqua=book antiqua,palatino; Comic Sans MS=comic sans ms,sans-serif; Courier New=courier new,courier; Georgia=georgia,palatino; Helvetica=helvetica; Impact=impact,chicago; Symbol=symbol; Tahoma=tahoma,arial,helvetica,sans-serif; Terminal=terminal,monaco; Times New Roman=times new roman,times; Trebuchet MS=trebuchet ms,geneva; Verdana=verdana,geneva; Webdings=webdings; Wingdings=wingdings,zapf dingbats"
-              }}
-            />
-            <i className={`${Service1Css['fas fa-long-arrow-alt-right']}`}></i
-            ></a>
+          onInit={(evt, editor) => editorRef.current = editor}
+          inline={true}
+          key='Service10'
+          initialValue="Sign Up"
+          init={{
+            forced_root_block: "",
+               menubar: false,
+            toolbar: 'bold italic underline | fontsizeselect | backcolor forecolor | alignleft aligncenter ' +
+              'alignright alignjustify | fontselect',
+            fontsize_formats: "8px 9px 10px 11px 12px 14px 16px 18px 20px 24px 30px 36px 48px 60px 72px 96px",
+            font_formats: "Andale Mono=andale mono,times; Arial=arial,helvetica,sans-serif; Arial Black=arial black,avant garde; Book Antiqua=book antiqua,palatino; Comic Sans MS=comic sans ms,sans-serif; Courier New=courier new,courier; Georgia=georgia,palatino; Helvetica=helvetica; Impact=impact,chicago; Symbol=symbol; Tahoma=tahoma,arial,helvetica,sans-serif; Terminal=terminal,monaco; Times New Roman=times new roman,times; Trebuchet MS=trebuchet ms,geneva; Verdana=verdana,geneva; Webdings=webdings; Wingdings=wingdings,zapf dingbats"
+          }}
+        />
+         <i className={`${Service1Css['fas fa-long-arrow-alt-right']}`}></i
+          ></a>
         </div>
+        <div style={(showPopup3) ?  { position: "absolute", top: "9rem", right: "5rem", zIndex: "9999999" } : { display: "none" }} className="toBeRemoved">
+                <GithubPicker
+                    triangle="top-right"
+                    onChangeComplete={(color) => {
+                        setCurrentBackground3(color.hex);
+                    }}
+                />
+            </div>
       </div>
 
-
+      <div style={(overSection && showPopup) ? { position: "absolute", top: "9rem", right: "5rem", zIndex: "9999999" } : { display: "none" }} className="toBeRemoved">
+                <GithubPicker
+                    triangle="top-right"
+                    onChangeComplete={(color) => {
+                        setCurrentBackground(color.hex);
+                    }}
+                />
+            </div>
     </>
   );
 }
