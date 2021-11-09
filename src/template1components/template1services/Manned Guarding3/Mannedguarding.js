8fe6e1cc583c34de78a960285f81009d779d8957
Ahsan Shahid <ahsanshahid2882@gmail.com>
2021-11-09 15:32:56 +0500

    Update Mannedguarding.js

--- src/template1components/template1services/Manned Guarding3/Mannedguarding.js
+++ src/template1components/template1services/Manned Guarding3/Mannedguarding.js
@@ -3,11 +3,12 @@ import StaticSecurity3Css from '../Manned Guarding3/Mannedguarding.module.css'
 import Img1 from '../../../assets/template1images/wavef1.png'
 import Img2 from '../../../assets/template1images/services66.jpg'
 import { Editor } from '@tinymce/tinymce-react';
-
-export default function Mannedguarding() {
+import Navbar1 from '../../Navbar1';
+export default function Mannedguarding({displayDevice, linksfunc}) {
     const editorRef = useRef(null);
     return (
         <>
+        <Navbar1 displayDevice={displayDevice} linksfunc={linksfunc} />
         <section className={StaticSecurity3Css['services-sec']}>
 
             <div className={`${StaticSecurity3Css['containers']} ${StaticSecurity3Css['grid-contact']}`}>
