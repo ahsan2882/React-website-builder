a9076cdfa97d5bcc952989329258f8441fc25a05
Ahsan Shahid <ahsanshahid2882@gmail.com>
2021-11-09 15:33:04 +0500

    Update HomeAbout.js

--- src/template1components/Template1Aboutcomponents/HomeAbout.js
+++ src/template1components/Template1Aboutcomponents/HomeAbout.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import Navbar1 from '../Navbar1'
 import HomeAboutmoduleCss from './HomeAbout.module.css'
 // import Service from './head2.jpg'
-export default function HomeAbout() {
+export default function HomeAbout({displayDevice, linksfunc}) {
     return (
    <>
+   <Navbar1 displayDevice={displayDevice} linksfunc={linksfunc}/>
     <section  className={`${HomeAboutmoduleCss['services-sec']}`}>
     <div className={`${HomeAboutmoduleCss.contentserv}`}>
               <h1 className={`${HomeAboutmoduleCss.headings}`}>About US</h1>
