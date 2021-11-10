5736ec90b5f3126e6c0c7f97f3d1ed329e5b42b4
Ahsan Shahid <ahsanshahid2882@gmail.com>
2021-11-10 11:30:18 +0500

    Update HeroSection3.js

--- src/template3components/HeroSection3.js
+++ src/template3components/HeroSection3.js
@@ -1,21 +1,21 @@
 import { Editor } from '@tinymce/tinymce-react';
 import React, { useRef, useState } from 'react'
 import Hero3Css from './HeroSection3.module.css'
-import { GithubPicker } from 'react-color';
+// import { GithubPicker } from 'react-color';
 
 export default function HeroSection3({ displayDevice, showPopup, overSection }) {
     const [backgroundType, setBackgroundType] = useState("video");
-    const [currentBackgroundColor, setCurrentBackgroundColor] = useState("black");
+    // const [currentBackgroundColor, setCurrentBackgroundColor] = useState("black");
     return (
         <>
             <section className="overflow-hidden">
                 {/* {backgroundType === "color" ? <HeroSection4ComponentColorImage color={currentBackgroundColor} /> : (backgroundType === "image" ? <HeroSection4ComponentColorImage image={currentBackgroundImage} /> : <HeroSection4ComponentVideo />)} */}
-                {backgroundType === "color" ? <HeroSection3ComponentColor color={currentBackgroundColor} displayDevice={displayDevice} /> : null}
-                {backgroundType === "image" ? <HeroSection3ComponentImage displayDevice={displayDevice} /> : null}
+                {/* {backgroundType === "color" ? <HeroSection3ComponentColor color={currentBackgroundColor} displayDevice={displayDevice} /> : null} */}
+                {/* {backgroundType === "image" ? <HeroSection3ComponentImage displayDevice={displayDevice} /> : null} */}
                 {backgroundType === "video" ? <HeroSection3ComponentVideo displayDevice={displayDevice} /> : null}
                 <div className="toBeRemoved" style={(overSection && showPopup) ? { position: "absolute", top: "5rem", right: "5rem", zIndex: "9999999" } : { display: "none" }}>
                     <div className="flex flex-col justify-center items-center border-2 border-black w-64 rounded-lg bg-white">
-                        <button className="py-4" onClick={() => setBackgroundType("color")}>Color</button>
+                        {/* <button className="py-4" onClick={() => setBackgroundType("color")}>Color</button>
                         {backgroundType === "color" ? <GithubPicker
                             triangle="top-right"
                             onChangeComplete={(color) => {
@@ -23,7 +23,7 @@ export default function HeroSection3({ displayDevice, showPopup, overSection }) 
                                 setCurrentBackgroundColor(color.hex);
                             }}
                         /> : null}
-                        <button className="py-2" onClick={() => setBackgroundType("image")}>Image</button>
+                        <button className="py-2" onClick={() => setBackgroundType("image")}>Image</button> */}
                         <button className="py-4" onClick={() => setBackgroundType("video")}>Video</button>
 
                     </div>
@@ -34,23 +34,23 @@ export default function HeroSection3({ displayDevice, showPopup, overSection }) 
 }
 
 
-const HeroSection3ComponentColor = ({ color, displayDevice }) => {
-    const editorRef = useRef(null);
-    return (
-        <>
+// const HeroSection3ComponentColor = ({ color, displayDevice }) => {
+//     const editorRef = useRef(null);
+//     return (
+//         <>
 
-        </>
-    )
-}
+//         </>
+//     )
+// }
 
-const HeroSection3ComponentImage = ({ displayDevice }) => {
-    const editorRef = useRef(null);
-    return (
-        <>
+// const HeroSection3ComponentImage = ({ displayDevice }) => {
+//     const editorRef = useRef(null);
+//     return (
+//         <>
 
-        </>
-    )
-}
+//         </>
+//     )
+// }
 
 const HeroSection3ComponentVideo = ({ displayDevice }) => {
     const editorRef = useRef(null);
