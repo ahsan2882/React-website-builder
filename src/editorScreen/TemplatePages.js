9b3e932115857b1cc78da648005f927bf85b89db
Ahsan Shahid <ahsanshahid2882@gmail.com>
2021-11-11 10:13:28 +0500

    Update TemplatePages.js

--- src/editorScreen/TemplatePages.js
+++ src/editorScreen/TemplatePages.js
@@ -12,7 +12,7 @@ const pages = {
         },
         {
             "groupName": "Services",
-            "subpages" :["Static Security", "Mobile Patrolling", "CCTV Monitoring", "Lock & Unlock", "Vacant Unit Checks", "Technology Product", "Manned Guarding", "Support & Management", "Event Security", "Guard House"]
+            "subpages" :["Static Security", "Mobile Patrolling", "CCTV Monitoring", "Lock & Unlock", "Vacant Unit Checks", "Technology Product", "Manned Guarding", "Support & Management", "Event Security"]
         },
         {
             "groupName": "Contact",
