b73e79763e02c05748bd0a03af374ebddb94f5d8
Ahsan Shahid <ahsanshahid2882@gmail.com>
2021-11-11 20:23:13 +0500

    Update Navbar3.js

--- src/template3components/Navbar3.js
+++ src/template3components/Navbar3.js
@@ -1,10 +1,53 @@
-import React, { useRef } from 'react'
+import React, { useRef, useState } from 'react'
 // import { GithubPicker } from 'react-color';
 // import logo from '../assets/template3images/logo-security-red-black.png'
 import Navbar3moduleCss from './Navbar3.module.css'
 import { Editor } from '@tinymce/tinymce-react';
 export default function Navbar3({ displayDevice, linksfunc }) {
     const editorRef = useRef(null);
+    const navSectorArray = [
+        "CONSTRUCTION",
+        "CORPORATE",
+        "INDUSTRIAL",
+        "RETAIL",
+        "LOGISTICS",
+        "PROPERTY MANAGEMENT"
+    ]
+    const navServiceArray = [
+        "STATIC SECURITY",
+        "MOBILE PATROLLING",
+        "CCTV MONITORING",
+        "LOCK & UNLOCK",
+        "VACANT UNIT CHECKS",
+        "TECHNOLOGY PRODUCT",
+        "MANNED GUARDING",
+        "SUPPORT & MANAGEMENT",
+        "EVENT SECURITY",
+        "GUARD HOUSE",
+        "24/7 SURVEILLANCE"
+    ]
+    const [navSector, setNavSector] = useState(navSectorArray)
+    const [navServices, setNavServices] = useState(navServiceArray)
+    const removeService = (index) => {
+        let newArray = [...navServices];
+        newArray.splice(index, 1);
+        setNavServices([...newArray])
+    }
+    const removeSector = (index) => {
+        let newArray = [...navSector];
+        newArray.splice(index, 1);
+        setNavSector([...newArray])
+    }
+    const addSector = () => {
+        let newArray = [...navSector];
+        newArray.push(navSectorArray[0]);
+        setNavSector([...newArray])
+    }
+    const addService = () => {
+        let newArray = [...navServices];
+        newArray.push(navServiceArray[0]);
+        setNavServices([...newArray])
+    }
     return (
         <>
             <header className="w-full h-20 font-bold">
@@ -45,27 +88,14 @@ export default function Navbar3({ displayDevice, linksfunc }) {
                                 </li>
                                 <li className={`${Navbar3moduleCss.menu} px-4 py-4`}>SECTORS
                                     <ul className={`${Navbar3moduleCss.subMenu} hidden text-black bg-white absolute top-16 font-normal`}>
-                                        <li>CONSTRUCTION</li>
-                                        <li>CORPORATE</li>
-                                        <li>INDUSTRIAL</li>
-                                        <li>RETAIL</li>
-                                        <li>LOGISTICS</li>
-                                        <li>PROPERTY MANAGEMENT</li>
+                                        {navSector.map((items, indexs) => <li>{items}<button className="toBeRemoved" onClick={() => removeSector(indexs)}><i className="ml-4 fas fa-trash-alt"></i></button></li>)}
+                                        <li className={`flex mt-8 mx-4 mb-0 justify-between toBeRemoved`}><button onClick={() => addSector()}><i className="fas fa-plus"></i> ADD NEW SECTOR</button></li>
                                     </ul>
                                 </li>
                                 <li className={`${Navbar3moduleCss.menu} px-4 py-4`}>SERVICES
                                     <ul className={`${Navbar3moduleCss.subMenu} hidden text-black bg-white absolute top-16 font-normal`}>
-                                        <li>STATIC SECURITY</li>
-                                        <li>MOBILE PATROLLING</li>
-                                        <li>CCTV MONITORING</li>
-                                        <li>LOCK & UNLOCK</li>
-                                        <li>VACANT UNIT CHECKS</li>
-                                        <li>TECHNOLOGY PRODUCT</li>
-                                        <li>MANNED GUARDING</li>
-                                        <li>SUPPORT & MANAGEMENT</li>
-                                        <li>EVENT SECURITY</li>
-                                        <li>ARD HOUSE</li>
-                                        <li>24/7 SURVEILLANCE</li>
+                                        {navServices.map((item, index) => <li>{item}<button className="toBeRemoved" onClick={() => removeService(index)}><i className="ml-4 fas fa-trash-alt"></i></button></li>)}
+                                        <li className={`flex mt-8 mx-4 mb-0 justify-between toBeRemoved`}><button onClick={() => addService()}><i className="fas fa-plus"></i> ADD NEW SERVICE</button></li>
                                     </ul>
                                 </li>
                                 <li className="px-4 py-4">{linksfunc ? <a href="contact.html" className="no-underline">CONTACT</a> : <>CONTACT</>}</li>
