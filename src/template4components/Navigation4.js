16462ca8a2bc50584bed8e281d48cd851b409bb4
Ahsan Shahid <ahsanshahid2882@gmail.com>
2021-11-11 14:54:57 +0500

    Update Navigation4.js

--- src/template4components/Navigation4.js
+++ src/template4components/Navigation4.js
@@ -2,19 +2,37 @@ import { Editor } from '@tinymce/tinymce-react';
 import React, { useRef, useState } from 'react';
 import Nav4Css from './Navigation4.module.css'
 
-export default function Navigation4({ displayDevice,linksfunc }) {
+export default function Navigation4({ displayDevice, linksfunc }) {
     const [activeHamburger, setActiveHamburger] = useState(false);
     // const [navbarOpt, setNavOpt] = useState([]);
     const toggleHamburger = () => {
         setActiveHamburger(state => !state);
     }
+    const [navSector, setNavSector] = useState([
+        "CONSTRUCTION",
+        "CORPORATE",
+        "INDYSTRIAL",
+        "RETAIL",
+        "LOGISTICS",
+        "PROPERTY MANAGEMENT"
+    ])
+    const [navServices, setNavServices] = useState([
+        "STATIC SECURITY",
+        "MOBILE PATROLLING",
+        "CCTV MONITORING",
+        "LOCK & UNLOCK",
+        "VACANT UNIT CHECKS",
+        "TECHNOLOGY PRODUCT",
+        "MANNED GUARDING",
+        "SUPPORT & MANAGEMENT",
+        "EVENT SECURITY"
+    ])
     const editorRef = useRef(null);
     return (
-        
+
         <>
-            <div className="w-full" style={{ backgroundColor: 'rgba(0, 193, 207, 0.75)'}}>
+            <div className="w-full" style={{ backgroundColor: 'rgba(0, 193, 207, 0.75)' }}>
                 <div className={`flex justify-between items-center py-6 px-6 ${Nav4Css.container1}`}>
-                    {/* <img src="https://i.ibb.co/Xyt25vh/thefnflogo.png" alt="nav-logo" className="w-52" /> */}
                     <Editor
                         onInit={(evt, editor) => editorRef.current = editor}
                         inline={true}
@@ -34,72 +52,23 @@ export default function Navigation4({ displayDevice,linksfunc }) {
                         }}
                     />
                     <nav>
-                        <ul className={displayDevice ? `${Nav4Css.navBar}` : `${Nav4Css.navBarM}`} style={(!displayDevice && activeHamburger) ? {left: "0"} : null}>
-                            <li className={`${Nav4Css.navBarLink} hover:text-black`}>{ linksfunc ? <a href="index.html">HOME</a> : <>HOME</>}</li>
-                            <li className={`${Nav4Css.navBarLink} hover:text-black`}> { linksfunc ? <a href="about.html">ABOUT US</a> : <> ABOUT US</>}</li>
+                        <ul className={displayDevice ? `${Nav4Css.navBar}` : `${Nav4Css.navBarM}`} style={(!displayDevice && activeHamburger) ? { left: "0" } : null}>
+                            <li className={`${Nav4Css.navBarLink} hover:text-black`}>{linksfunc ? <a href="index.html">HOME</a> : <>HOME</>}</li>
+                            <li className={`${Nav4Css.navBarLink} hover:text-black`}> {linksfunc ? <a href="about.html">ABOUT US</a> : <> ABOUT US</>}</li>
                             <li className={`${Nav4Css.navBarLink} hover:text-black ${Nav4Css.dropdownServiceListItem}`}>SERVICES
                                 <ul className={`${Nav4Css.dropdownServicesContent} list-none p-0 absolute`}>
-                                    <li className={`${Nav4Css.servicesLinks} flex mt-8 mx-4 mb-0`}>
-                                        <h4 className={Nav4Css.navigationLinkContent}>STATIC SECURITY</h4>
-                                    </li>
-                                    <li className={`${Nav4Css.servicesLinks} flex mt-8 mx-4 mb-0`}>
-                                        <h4 className={Nav4Css.navigationLinkContent}>MOBILE PATROLLING</h4>
-                                    </li>
-                                    <li className={`${Nav4Css.servicesLinks} flex mt-8 mx-4 mb-0`}>
-                                        <h4 className={Nav4Css.navigationLinkContent}>CCTV MONITORING</h4>
-                                    </li>
-                                    <li className={`${Nav4Css.servicesLinks} flex mt-8 mx-4 mb-0`}>
-                                        <h4 className={Nav4Css.navigationLinkContent}>LOCK & UNLOCK</h4>
-                                    </li>
-                                    <li className={`${Nav4Css.servicesLinks} flex mt-8 mx-4 mb-0`}>
-                                        <h4 className={Nav4Css.navigationLinkContent}>
-                                            VACANT UNIT CHECKS
-                                        </h4>
-                                    </li>
-                                    <li className={`${Nav4Css.servicesLinks} flex mt-8 mx-4 mb-0`}>
-                                        <h4 className={Nav4Css.navigationLinkContent}>
-                                            TECHNOLOGY PRODUCT
-                                        </h4>
-                                    </li>
-                                    <li className={`${Nav4Css.servicesLinks} flex mt-8 mx-4 mb-0`}>
-                                        <h4 className={Nav4Css.navigationLinkContent}>MANNED GUARDING</h4>
-                                    </li>
-                                    <li className={`${Nav4Css.servicesLinks} flex mt-8 mx-4 mb-0`}>
-                                        <h4 className={Nav4Css.navigationLinkContent}>
-                                            SUPPORT & MANAGEMENT
-                                        </h4>
-                                    </li>
-                                    <li className={`${Nav4Css.servicesLinks} flex my-8 mx-4`}>
-                                        <h4 className={Nav4Css.navigationLinkContent}>EVENT SECURITY</h4>
-                                    </li>
+                                    {navServices.map((item) => <li className={`${Nav4Css.servicesLinks} flex mt-8 mx-4 mb-0`}>
+                                        <h4 className={Nav4Css.navigationLinkContent}>{item}</h4>
+                                    </li>)}
                                 </ul>
                             </li>
                             <li className={`${Nav4Css.navBarLink} hover:text-black ${Nav4Css.dropdownSectorsListItem}`}>SECTORS
                                 <ul className={`${Nav4Css.dropdownSectorsContent} list-none p-0 absolute`}>
-                                    <li className={`${Nav4Css.sectorsLink} flex mt-8 mx-4 mb-0`}>
-                                        <h4 className={Nav4Css.navigationLinkContent}>CONSTRUCTION</h4>
-                                    </li>
-                                    <li className={`${Nav4Css.sectorsLink} flex mt-8 mx-4 mb-0`}>
-                                        <h4 className={Nav4Css.navigationLinkContent}>CORPORATE</h4>
-                                    </li>
-                                    <li className={`${Nav4Css.sectorsLink} flex mt-8 mx-4 mb-0`}>
-                                        <h4 className={Nav4Css.navigationLinkContent}>INDUSTRIAL</h4>
-                                    </li>
-                                    <li className={`${Nav4Css.sectorsLink} flex mt-8 mx-4 mb-0`}>
-                                        <h4 className={Nav4Css.navigationLinkContent}>RETAIL</h4>
-                                    </li>
-                                    <li className={`${Nav4Css.sectorsLink} flex mt-8 mx-4 mb-0`}>
-                                        <h4 className={Nav4Css.navigationLinkContent}>LOGISTICS</h4>
-                                    </li>
-                                    <li className={`${Nav4Css.sectorsLink} flex my-8 mx-4`}>
-                                        <h4 className={Nav4Css.navigationLinkContent}>
-                                            PROPERTY MANAGEMENT
-                                        </h4>
-                                    </li>
+                                    {navSector.map((items) => <li className={`${Nav4Css.sectorsLink} flex mt-8 mx-4 mb-0`}><h4 className={Nav4Css.navigationLinkContent}>{items}</h4></li>)}
                                 </ul>
                             </li>
                             <li className={`${Nav4Css.navBarLink} hover:text-black`}>
-                                <button className="text-white"> { linksfunc ? <a href="contact.html"> CONTACT Us</a> : <>CONTACT Us</>}</button>
+                                <button className="text-white"> {linksfunc ? <a href="contact.html"> CONTACT Us</a> : <>CONTACT Us</>}</button>
                             </li>
                         </ul>
                         <section className={displayDevice ? `${Nav4Css.hamburger}` : (activeHamburger ? `${Nav4Css.hamburgerMobile} ${Nav4Css.active}` : `${Nav4Css.hamburgerMobile}`)} onClick={() => toggleHamburger()}>
