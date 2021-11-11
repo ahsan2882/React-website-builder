4bf229337cfa2160fa3582881d01af8e3ee059ec
Ahsan Shahid <ahsanshahid2882@gmail.com>
2021-11-11 10:47:41 +0500

    Update About3banner.js

--- src/template3components/template3about/About3banner.js
+++ src/template3components/template3about/About3banner.js
@@ -1,7 +1,7 @@
 import React, { useRef, useState } from 'react'
 import { GithubPicker } from 'react-color';
 import About3Css from '../template3about/About3.module.css'
-// import Img1 from '../template3contactcomponents/gPeonv.png'
+// import Img1 from '../../assets/template3images/gPeonv.png'
 import { Editor } from '@tinymce/tinymce-react';
 export default function About3banner() {
     const [currentBackground, setCurrentBackground] = useState("white");
@@ -17,20 +17,21 @@ export default function About3banner() {
                         key='Service1'
                         initialValue="About"
                         init={{
+                            plugins: [
+                                "link"
+                            ],
                             forced_root_block: "",
                             menubar: false,
-                            toolbar: 'bold italic underline | fontsizeselect | backcolor forecolor | alignleft aligncenter ' +
-                                'alignright alignjustify | fontselect | lineheight',
+                            toolbar: 'bold italic underline | link | fontsizeselect | backcolor forecolor | alignleft aligncenter ' +
+                                'alignright alignjustify | fontselect',
                             fontsize_formats: "8px 9px 10px 11px 12px 14px 16px 18px 20px 24px 30px 36px 48px 60px 72px 96px",
-                            font_formats: "Roboto=roboto, sans-serif, serif; Mohave=mohave, sans-serif, serif;Open Sans=open sans, sans-serif, serif;Lato=lato, sans-serif, serif; Red Hat Mono=red hat mono, sans-serif, serif; Montserrat=montserrat, sans-serif, serif; Roboto Condensed=roboto condensed, sans-serif, serif; Source Sans Pro= source sans pro, sans-serif, serif; Dongle=dongle, sans-serif, serif; Poppins=poppins, sans-serif, serif; Oswald=oswald, sans-serif, serif; Roboto Mono=roboto mono, sans-serif, serif; Raleway=raleway, sans-serif, serif; Ubuntu=ubuntu, sans-serif, serif; Merriweather=merriweather, sans-serif, serif;Andale Mono=andale mono,times; Arial=arial,helvetica,sans-serif; Arial Black=arial black,avant garde; Book Antiqua=book antiqua,palatino; Comic Sans MS=comic sans ms,sans-serif; Courier New=courier new,courier; Georgia=georgia,palatino; Helvetica=helvetica; Impact=impact,chicago; Tahoma=tahoma,arial,helvetica,sans-serif; Terminal=terminal,monaco; Times New Roman=times new roman,times; Trebuchet MS=trebuchet ms,geneva; Verdana=verdana,geneva",
-
+                            font_formats: "Andale Mono=andale mono,times; Arial=arial,helvetica,sans-serif; Arial Black=arial black,avant garde; Book Antiqua=book antiqua,palatino; Comic Sans MS=comic sans ms,sans-serif; Courier New=courier new,courier; Georgia=georgia,palatino; Helvetica=helvetica; Impact=impact,chicago; Symbol=symbol; Tahoma=tahoma,arial,helvetica,sans-serif; Terminal=terminal,monaco; Times New Roman=times new roman,times; Trebuchet MS=trebuchet ms,geneva; Verdana=verdana,geneva; Webdings=webdings; Wingdings=wingdings,zapf dingbats"
                         }}
                     />
                 </h1>
                 <div className={About3Css.dash}></div>
             </div>
-
-
+            {/* <img src="https://i.ibb.co/QH3ksQx/gPeonv.png" alt="gPeonv" border="0"> */}
             <img src="https://i.ibb.co/QH3ksQx/gPeonv.png" alt="gPeonv" />
         </section>
     )
