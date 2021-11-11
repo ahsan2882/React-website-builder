734ba937dde0355cd5a069cc99e4727ba3ccc7e3
Ahsan Shahid <ahsanshahid2882@gmail.com>
2021-11-11 10:29:58 +0500

    Update AllTemplates.js

--- src/myComponents/AllTemplates.js
+++ src/myComponents/AllTemplates.js
@@ -107,6 +107,11 @@ import Contact3form from '../template3components/template3Contact/Contact3form'
 import Contact3feedback from '../template3components/template3Contact/Contact3feedback'
 import Contact1banner from "../template1components/template1contact/Contact1banner";
 import Contactform1 from '../template1components/template1contact/Contactform1'
+import Pricing3 from "../template3components/Pricing3";
+import About3banner from '../template3components/template3about/About3banner'
+import Aboutvission from '../template3components/template3about/Aboutvission'
+import AboutService3 from '../template3components/template3about/AboutService3'
+import AboutPolicy3 from '../template3components/template3about/AboutPolicy3'
 
 export const templateComponents = {
     template1Components: [
@@ -285,7 +290,7 @@ export const templateComponents = {
         {
             groupName: "about",
             pageName: "About",
-            pageSections: [Navbar3, HeroSection3, Services3, Guard3, Latestnews3, Ourprograms3, About3, Accrediation3, Footer3]
+            pageSections: [Navbar3, About3banner, Aboutvission, AboutService3, Pricing3, Guard3, Accrediation3, AboutPolicy3, Footer3]
         },
         {
             groupName: "contact",
