5cdbe37c68da084468b5bb6525bde4d54e5a5893
Ahsan Shahid <ahsanshahid2882@gmail.com>
2021-11-11 10:47:29 +0500

    Update AboutService3.js

--- src/template3components/template3about/AboutService3.js
+++ src/template3components/template3about/AboutService3.js
@@ -1,5 +1,5 @@
 import React, { useRef, useState } from 'react'
-import { GithubPicker } from 'react-color';
+import { GithubPicker } from 'react-color'; 
 import Aboutservices3Css from '../template3about/Aboutservices3.module.css'
 import { Editor } from '@tinymce/tinymce-react';
 
@@ -20,13 +20,15 @@ export default function AboutService3() {
                                 key='Service1'
                                 initialValue="<h3>Best In Town</h3>"
                                 init={{
+                                    plugins: [
+                                        "link"
+                                    ],
                                     forced_root_block: "",
                                     menubar: false,
-                                    toolbar: 'bold italic underline | fontsizeselect | backcolor forecolor | alignleft aligncenter ' +
-                                        'alignright alignjustify | fontselect | lineheight',
+                                    toolbar: 'bold italic underline | link | fontsizeselect | backcolor forecolor | alignleft aligncenter ' +
+                                        'alignright alignjustify | fontselect',
                                     fontsize_formats: "8px 9px 10px 11px 12px 14px 16px 18px 20px 24px 30px 36px 48px 60px 72px 96px",
-                                    font_formats: "Roboto=roboto, sans-serif, serif; Mohave=mohave, sans-serif, serif;Open Sans=open sans, sans-serif, serif;Lato=lato, sans-serif, serif; Red Hat Mono=red hat mono, sans-serif, serif; Montserrat=montserrat, sans-serif, serif; Roboto Condensed=roboto condensed, sans-serif, serif; Source Sans Pro= source sans pro, sans-serif, serif; Dongle=dongle, sans-serif, serif; Poppins=poppins, sans-serif, serif; Oswald=oswald, sans-serif, serif; Roboto Mono=roboto mono, sans-serif, serif; Raleway=raleway, sans-serif, serif; Ubuntu=ubuntu, sans-serif, serif; Merriweather=merriweather, sans-serif, serif;Andale Mono=andale mono,times; Arial=arial,helvetica,sans-serif; Arial Black=arial black,avant garde; Book Antiqua=book antiqua,palatino; Comic Sans MS=comic sans ms,sans-serif; Courier New=courier new,courier; Georgia=georgia,palatino; Helvetica=helvetica; Impact=impact,chicago; Tahoma=tahoma,arial,helvetica,sans-serif; Terminal=terminal,monaco; Times New Roman=times new roman,times; Trebuchet MS=trebuchet ms,geneva; Verdana=verdana,geneva",
-
+                                    font_formats: "Andale Mono=andale mono,times; Arial=arial,helvetica,sans-serif; Arial Black=arial black,avant garde; Book Antiqua=book antiqua,palatino; Comic Sans MS=comic sans ms,sans-serif; Courier New=courier new,courier; Georgia=georgia,palatino; Helvetica=helvetica; Impact=impact,chicago; Symbol=symbol; Tahoma=tahoma,arial,helvetica,sans-serif; Terminal=terminal,monaco; Times New Roman=times new roman,times; Trebuchet MS=trebuchet ms,geneva; Verdana=verdana,geneva; Webdings=webdings; Wingdings=wingdings,zapf dingbats"
                                 }}
                             />
 
@@ -38,13 +40,15 @@ export default function AboutService3() {
                             key='Service2'
                             initialValue="<h1>SERVICES</h1>"
                             init={{
+                                plugins: [
+                                    "link"
+                                ],
                                 forced_root_block: "",
                                 menubar: false,
-                                toolbar: 'bold italic underline | fontsizeselect | backcolor forecolor | alignleft aligncenter ' +
-                                    'alignright alignjustify | fontselect | lineheight',
+                                toolbar: 'bold italic underline | link | fontsizeselect | backcolor forecolor | alignleft aligncenter ' +
+                                    'alignright alignjustify | fontselect',
                                 fontsize_formats: "8px 9px 10px 11px 12px 14px 16px 18px 20px 24px 30px 36px 48px 60px 72px 96px",
-                                font_formats: "Roboto=roboto, sans-serif, serif; Mohave=mohave, sans-serif, serif;Open Sans=open sans, sans-serif, serif;Lato=lato, sans-serif, serif; Red Hat Mono=red hat mono, sans-serif, serif; Montserrat=montserrat, sans-serif, serif; Roboto Condensed=roboto condensed, sans-serif, serif; Source Sans Pro= source sans pro, sans-serif, serif; Dongle=dongle, sans-serif, serif; Poppins=poppins, sans-serif, serif; Oswald=oswald, sans-serif, serif; Roboto Mono=roboto mono, sans-serif, serif; Raleway=raleway, sans-serif, serif; Ubuntu=ubuntu, sans-serif, serif; Merriweather=merriweather, sans-serif, serif;Andale Mono=andale mono,times; Arial=arial,helvetica,sans-serif; Arial Black=arial black,avant garde; Book Antiqua=book antiqua,palatino; Comic Sans MS=comic sans ms,sans-serif; Courier New=courier new,courier; Georgia=georgia,palatino; Helvetica=helvetica; Impact=impact,chicago; Tahoma=tahoma,arial,helvetica,sans-serif; Terminal=terminal,monaco; Times New Roman=times new roman,times; Trebuchet MS=trebuchet ms,geneva; Verdana=verdana,geneva",
-
+                                font_formats: "Andale Mono=andale mono,times; Arial=arial,helvetica,sans-serif; Arial Black=arial black,avant garde; Book Antiqua=book antiqua,palatino; Comic Sans MS=comic sans ms,sans-serif; Courier New=courier new,courier; Georgia=georgia,palatino; Helvetica=helvetica; Impact=impact,chicago; Symbol=symbol; Tahoma=tahoma,arial,helvetica,sans-serif; Terminal=terminal,monaco; Times New Roman=times new roman,times; Trebuchet MS=trebuchet ms,geneva; Verdana=verdana,geneva; Webdings=webdings; Wingdings=wingdings,zapf dingbats"
                             }}
                         />
 
@@ -56,7 +60,7 @@ export default function AboutService3() {
                     <div className={Aboutservices3Css['box']}>
 
                         <lord-icon src="https://cdn.lordicon.com/tyounuzx.json" trigger="loop" colors={{ "primary": "#c01120", "secondary": "var(--clr-secondary)" }}
-                            style={{ "width": "250px", "height": "250px" }}>
+                            style={{ "width": "150px", "height": "150px" }}>
                         </lord-icon>
                         <div className={Aboutservices3Css['box_content-hover']}>
                             <Editor
@@ -66,13 +70,15 @@ export default function AboutService3() {
                                 key='Service2'
                                 initialValue="<p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. At, deleniti?lorem200</p>"
                                 init={{
+                                    plugins: [
+                                        "link"
+                                    ],
                                     forced_root_block: "",
                                     menubar: false,
-                                    toolbar: 'bold italic underline | fontsizeselect | backcolor forecolor | alignleft aligncenter ' +
-                                        'alignright alignjustify | fontselect | lineheight',
+                                    toolbar: 'bold italic underline | link | fontsizeselect | backcolor forecolor | alignleft aligncenter ' +
+                                        'alignright alignjustify | fontselect',
                                     fontsize_formats: "8px 9px 10px 11px 12px 14px 16px 18px 20px 24px 30px 36px 48px 60px 72px 96px",
-                                    font_formats: "Roboto=roboto, sans-serif, serif; Mohave=mohave, sans-serif, serif;Open Sans=open sans, sans-serif, serif;Lato=lato, sans-serif, serif; Red Hat Mono=red hat mono, sans-serif, serif; Montserrat=montserrat, sans-serif, serif; Roboto Condensed=roboto condensed, sans-serif, serif; Source Sans Pro= source sans pro, sans-serif, serif; Dongle=dongle, sans-serif, serif; Poppins=poppins, sans-serif, serif; Oswald=oswald, sans-serif, serif; Roboto Mono=roboto mono, sans-serif, serif; Raleway=raleway, sans-serif, serif; Ubuntu=ubuntu, sans-serif, serif; Merriweather=merriweather, sans-serif, serif;Andale Mono=andale mono,times; Arial=arial,helvetica,sans-serif; Arial Black=arial black,avant garde; Book Antiqua=book antiqua,palatino; Comic Sans MS=comic sans ms,sans-serif; Courier New=courier new,courier; Georgia=georgia,palatino; Helvetica=helvetica; Impact=impact,chicago; Tahoma=tahoma,arial,helvetica,sans-serif; Terminal=terminal,monaco; Times New Roman=times new roman,times; Trebuchet MS=trebuchet ms,geneva; Verdana=verdana,geneva",
-
+                                    font_formats: "Andale Mono=andale mono,times; Arial=arial,helvetica,sans-serif; Arial Black=arial black,avant garde; Book Antiqua=book antiqua,palatino; Comic Sans MS=comic sans ms,sans-serif; Courier New=courier new,courier; Georgia=georgia,palatino; Helvetica=helvetica; Impact=impact,chicago; Symbol=symbol; Tahoma=tahoma,arial,helvetica,sans-serif; Terminal=terminal,monaco; Times New Roman=times new roman,times; Trebuchet MS=trebuchet ms,geneva; Verdana=verdana,geneva; Webdings=webdings; Wingdings=wingdings,zapf dingbats"
                                 }}
                             />
 
@@ -82,7 +88,7 @@ export default function AboutService3() {
                     <div className={Aboutservices3Css['box']}>
 
                         <lord-icon src="https://cdn.lordicon.com/usjxhgom.json" trigger="loop" colors={{ "primary": "#c01120", "secondary": "var(--clr-secondary)" }}
-                            style={{ "width": "250px", "height": "250px" }}>
+                            style={{ "width": "150px", "height": "150px" }}>
                         </lord-icon>
                         <div className={Aboutservices3Css['box_content-hover']}>
 
@@ -91,7 +97,7 @@ export default function AboutService3() {
                     <div className={Aboutservices3Css['box']}>
 
                         <lord-icon src="https://cdn.lordicon.com/gqzfzudq.json" trigger="loop" colors={{ "primary": "#c01120", "secondary": "var(--clr-secondary)" }}
-                            style={{ "width": "250px", "height": "250px" }}>
+                            style={{ "width": "150px", "height": "150px" }}>
                         </lord-icon>
                         <div className={Aboutservices3Css['box_content-hover']}>
 
@@ -102,7 +108,7 @@ export default function AboutService3() {
                     <div className={Aboutservices3Css['box']}>
 
                         <lord-icon src="https://cdn.lordicon.com/tvafngxn.json" trigger="loop"
-                            colors={{ "primary": "#c01120", "secondary": "var(--clr-secondary)" }} style={{ "width": "250px", "height": "250px" }}>
+                            colors={{ "primary": "#c01120", "secondary": "var(--clr-secondary)" }} style={{ "width": "150px", "height": "150px" }}>
                         </lord-icon>
                         <div className={Aboutservices3Css['box_content-hover']}>
                             <Editor
@@ -112,23 +118,25 @@ export default function AboutService3() {
                                 key='Service3'
                                 initialValue="<p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. At, deleniti?lorem200</p>"
                                 init={{
+                                    plugins: [
+                                        "link"
+                                    ],
                                     forced_root_block: "",
                                     menubar: false,
-                                    toolbar: 'bold italic underline | fontsizeselect | backcolor forecolor | alignleft aligncenter ' +
-                                        'alignright alignjustify | fontselect | lineheight',
+                                    toolbar: 'bold italic underline | link | fontsizeselect | backcolor forecolor | alignleft aligncenter ' +
+                                        'alignright alignjustify | fontselect',
                                     fontsize_formats: "8px 9px 10px 11px 12px 14px 16px 18px 20px 24px 30px 36px 48px 60px 72px 96px",
-                                    font_formats: "Roboto=roboto, sans-serif, serif; Mohave=mohave, sans-serif, serif;Open Sans=open sans, sans-serif, serif;Lato=lato, sans-serif, serif; Red Hat Mono=red hat mono, sans-serif, serif; Montserrat=montserrat, sans-serif, serif; Roboto Condensed=roboto condensed, sans-serif, serif; Source Sans Pro= source sans pro, sans-serif, serif; Dongle=dongle, sans-serif, serif; Poppins=poppins, sans-serif, serif; Oswald=oswald, sans-serif, serif; Roboto Mono=roboto mono, sans-serif, serif; Raleway=raleway, sans-serif, serif; Ubuntu=ubuntu, sans-serif, serif; Merriweather=merriweather, sans-serif, serif;Andale Mono=andale mono,times; Arial=arial,helvetica,sans-serif; Arial Black=arial black,avant garde; Book Antiqua=book antiqua,palatino; Comic Sans MS=comic sans ms,sans-serif; Courier New=courier new,courier; Georgia=georgia,palatino; Helvetica=helvetica; Impact=impact,chicago; Tahoma=tahoma,arial,helvetica,sans-serif; Terminal=terminal,monaco; Times New Roman=times new roman,times; Trebuchet MS=trebuchet ms,geneva; Verdana=verdana,geneva",
-
+                                    font_formats: "Andale Mono=andale mono,times; Arial=arial,helvetica,sans-serif; Arial Black=arial black,avant garde; Book Antiqua=book antiqua,palatino; Comic Sans MS=comic sans ms,sans-serif; Courier New=courier new,courier; Georgia=georgia,palatino; Helvetica=helvetica; Impact=impact,chicago; Symbol=symbol; Tahoma=tahoma,arial,helvetica,sans-serif; Terminal=terminal,monaco; Times New Roman=times new roman,times; Trebuchet MS=trebuchet ms,geneva; Verdana=verdana,geneva; Webdings=webdings; Wingdings=wingdings,zapf dingbats"
                                 }}
                             />
-
+                            
 
                         </div>
                     </div>
                     <div className={Aboutservices3Css['box']}>
 
                         <lord-icon src="https://cdn.lordicon.com/uneninia.json" trigger="loop"
-                            colors={{ "primary": "#c01120", "secondary": "var(--clr-secondary)" }} style={{ "width": "250px", "height": "250px" }}>
+                            colors={{ "primary": "#c01120", "secondary": "var(--clr-secondary)" }} style={{ "width": "150px", "height": "150px" }}>
                         </lord-icon>
                         <div className={Aboutservices3Css['box_content-hover']}>
 
@@ -137,7 +145,7 @@ export default function AboutService3() {
                     <div className={Aboutservices3Css['box']}>
 
                         <lord-icon src="https://cdn.lordicon.com/nxaaasqe.json" trigger="loop"
-                            colors={{ "primary": "#c01120", "secondary": "var(--clr-secondary)" }} style={{ "width": "250px", "height": "250px" }}>
+                            colors={{ "primary": "#c01120", "secondary": "var(--clr-secondary)" }} style={{ "width": "150px", "height": "150px" }}>
                         </lord-icon>
                         <div className={Aboutservices3Css['box_content-hover']} >
 
