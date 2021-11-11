a323831ee942d2b493a2d21cf54e74fc8041dac0
Ahsan Shahid <ahsanshahid2882@gmail.com>
2021-11-11 20:23:01 +0500

    Update Technologyproduct3.js

--- src/template1components/template1services/technologyproduct/Technologyproduct3.js
+++ src/template1components/template1services/technologyproduct/Technologyproduct3.js
@@ -1,5 +1,5 @@
 import React, { useRef } from 'react'
-import StaticSecurity3Css from '../technologyproduct/Technologyproduct.module.css'
+import StaticSecurity3Css from './Technologyproduct.module.css'
 // import Img1 from '../../../assets/template1images/wavef1.png'
 // import Img2 from '../../../assets/template1images/services56.jpg'
 import { Editor } from '@tinymce/tinymce-react';
@@ -108,26 +108,24 @@ export default function Technologyproduct3({ showPopup, overSection, displayDevi
             <div className={StaticSecurity3Css.container}>
                 <div className={StaticSecurity3Css['services-text2']}>
                     <div className={StaticSecurity3Css['txt-serv']}>
-                        <h1>
-                            <Editor
-                                onInit={(evt, editor) => editorRef.current = editor}
-                                inline={true}
-                                key='Static3'
-                                initialValue="Technology Product"
-                                init={{
-                                    plugins: [
-                                        "link"
-                                    ],
-                                    forced_root_block: "",
-                                    menubar: false,
-                                    toolbar: 'bold italic underline | link | fontsizeselect | backcolor forecolor | alignleft aligncenter ' +
-                                        'alignright alignjustify | fontselect',
-                                    fontsize_formats: "8px 9px 10px 11px 12px 14px 16px 18px 20px 24px 30px 36px 48px 60px 72px 96px",
-                                    font_formats: "Andale Mono=andale mono,times; Arial=arial,helvetica,sans-serif; Arial Black=arial black,avant garde; Book Antiqua=book antiqua,palatino; Comic Sans MS=comic sans ms,sans-serif; Courier New=courier new,courier; Georgia=georgia,palatino; Helvetica=helvetica; Impact=impact,chicago; Symbol=symbol; Tahoma=tahoma,arial,helvetica,sans-serif; Terminal=terminal,monaco; Times New Roman=times new roman,times; Trebuchet MS=trebuchet ms,geneva; Verdana=verdana,geneva; Webdings=webdings; Wingdings=wingdings,zapf dingbats"
-                                }}
-                            />
-
-                        </h1>
+                      
+                        <Editor
+                                    onInit={(evt, editor) => editorRef.current = editor}
+                                    inline={true}
+                                    key='Static3'
+                                    initialValue={`<span class="${StaticSecurity3Css.line1}"> Technology </span><span class="${StaticSecurity3Css.line2}"> Product</span>`}
+                                    init={{
+                                        plugins: [
+                                            "link"
+                                        ],
+                                        forced_root_block: "",
+                                        menubar: false,
+                                        toolbar: 'bold italic underline | link | fontsizeselect | backcolor forecolor | alignleft aligncenter ' +
+                                            'alignright alignjustify | fontselect',
+                                        fontsize_formats: "8px 9px 10px 11px 12px 14px 16px 18px 20px 24px 30px 36px 48px 60px 72px 96px",
+                                        font_formats: "Andale Mono=andale mono,times; Arial=arial,helvetica,sans-serif; Arial Black=arial black,avant garde; Book Antiqua=book antiqua,palatino; Comic Sans MS=comic sans ms,sans-serif; Courier New=courier new,courier; Georgia=georgia,palatino; Helvetica=helvetica; Impact=impact,chicago; Symbol=symbol; Tahoma=tahoma,arial,helvetica,sans-serif; Terminal=terminal,monaco; Times New Roman=times new roman,times; Trebuchet MS=trebuchet ms,geneva; Verdana=verdana,geneva; Webdings=webdings; Wingdings=wingdings,zapf dingbats"
+                                    }}
+                                />
                       
                     </div>
                     <Editor
