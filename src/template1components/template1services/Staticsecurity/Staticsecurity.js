53f3007bc354640ae238350da02863a47ab5558e
Ahsan Shahid <ahsanshahid2882@gmail.com>
2021-11-11 17:50:14 +0500

    Update Staticsecurity.js

--- src/template1components/template1services/Staticsecurity/Staticsecurity.js
+++ src/template1components/template1services/Staticsecurity/Staticsecurity.js
@@ -1,5 +1,5 @@
 import React, { useRef } from 'react'
-import StaticSecurity3Css from '../Staticsecurity/Staticsecurity.module.css'
+import StaticSecurity3Css from './Staticsecurity.module.css'
 // import Img1 from '../../../assets/template1images/wavef1.png'
 // import Img2 from '../../../assets/template1images/services11.jpg'
 import { Editor } from '@tinymce/tinymce-react';
