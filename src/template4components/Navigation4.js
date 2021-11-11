0bdd1a18bea7294b1d1c1e25b30dee733fcd6ca5
Ahsan Shahid <ahsanshahid2882@gmail.com>
2021-11-11 17:50:43 +0500

    Update Navigation4.js

--- src/template4components/Navigation4.js
+++ src/template4components/Navigation4.js
@@ -79,14 +79,14 @@ export default function Navigation4({ displayDevice, linksfunc }) {
                             <li className={`${Nav4Css.navBarLink} hover:text-black`}> {linksfunc ? <a href="about.html">ABOUT US</a> : <> ABOUT US</>}</li>
                             <li className={`${Nav4Css.navBarLink} hover:text-black ${Nav4Css.dropdownServiceListItem}`}>SERVICES
                                 <ul className={`${Nav4Css.dropdownServicesContent} list-none p-0 absolute`}>
-                                    {navServices.map((item, index) => <li className={`${Nav4Css.servicesLinks} flex mt-8 mx-4 mb-0 justify-between`}><h4 className={Nav4Css.navigationLinkContent}>{item}</h4><button onClick={() => removeService(index)}><i className="ml-4 fas fa-trash-alt"></i></button></li>)}
-                                    <li className={`flex mt-8 mx-4 mb-0 justify-between`}><button onClick={() => addService()}><i className="fas fa-plus"></i> ADD NEW SERVICE</button></li>
+                                    {navServices.map((item, index) => <li className={`${Nav4Css.servicesLinks} flex mt-8 mx-4 mb-0 justify-between`}><h4 className={Nav4Css.navigationLinkContent}>{item}</h4><button className="toBeRemoved" onClick={() => removeService(index)}><i className="ml-4 fas fa-trash-alt"></i></button></li>)}
+                                    <li className={`flex mt-8 mx-4 mb-0 justify-between toBeRemoved`}><button onClick={() => addService()}><i className="fas fa-plus"></i> ADD NEW SERVICE</button></li>
                                 </ul>
                             </li>
                             <li className={`${Nav4Css.navBarLink} hover:text-black ${Nav4Css.dropdownSectorsListItem}`}>SECTORS
                                 <ul className={`${Nav4Css.dropdownSectorsContent} list-none p-0 absolute`}>
-                                    {navSector.map((items, indexs) => <li className={`${Nav4Css.sectorsLink} flex mt-8 mx-4 mb-0 justify-between`}><h4 className={Nav4Css.navigationLinkContent}>{items}</h4><button onClick={() => removeSector(indexs)}><i className="ml-4 fas fa-trash-alt"></i></button></li>)}
-                                    <li className={`flex mt-8 mx-4 mb-0 justify-between`}><button onClick={() => addSector()}><i className="fas fa-plus"></i> ADD NEW SECTOR</button></li>
+                                    {navSector.map((items, indexs) => <li className={`${Nav4Css.sectorsLink} flex mt-8 mx-4 mb-0 justify-between`}><h4 className={Nav4Css.navigationLinkContent}>{items}</h4><button className="toBeRemoved" onClick={() => removeSector(indexs)}><i className="ml-4 fas fa-trash-alt"></i></button></li>)}
+                                    <li className={`flex mt-8 mx-4 mb-0 justify-between toBeRemoved`}><button onClick={() => addSector()}><i className="fas fa-plus"></i> ADD NEW SECTOR</button></li>
                                 </ul>
                             </li>
                             <li className={`${Nav4Css.navBarLink} hover:text-black`}>
