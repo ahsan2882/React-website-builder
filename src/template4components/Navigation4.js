b54f7b07ee89aaec8c80f996bf7337374a82fe13
Ahsan Shahid <ahsanshahid2882@gmail.com>
2021-11-11 15:15:15 +0500

    Update Navigation4.js

--- src/template4components/Navigation4.js
+++ src/template4components/Navigation4.js
@@ -8,15 +8,15 @@ export default function Navigation4({ displayDevice, linksfunc }) {
     const toggleHamburger = () => {
         setActiveHamburger(state => !state);
     }
-    const [navSector, setNavSector] = useState([
+    const navSectorArray = [
         "CONSTRUCTION",
         "CORPORATE",
-        "INDYSTRIAL",
+        "INDUSTRIAL",
         "RETAIL",
         "LOGISTICS",
         "PROPERTY MANAGEMENT"
-    ])
-    const [navServices, setNavServices] = useState([
+    ]
+    const navServiceArray = [
         "STATIC SECURITY",
         "MOBILE PATROLLING",
         "CCTV MONITORING",
@@ -26,7 +26,29 @@ export default function Navigation4({ displayDevice, linksfunc }) {
         "MANNED GUARDING",
         "SUPPORT & MANAGEMENT",
         "EVENT SECURITY"
-    ])
+    ]
+    const [navSector, setNavSector] = useState(navSectorArray)
+    const [navServices, setNavServices] = useState(navServiceArray)
+    const removeService = (index) => {
+        let newArray = [...navServices];
+        newArray.splice(index, 1);
+        setNavServices([...newArray])
+    }
+    const removeSector = (index) => {
+        let newArray = [...navSector];
+        newArray.splice(index, 1);
+        setNavSector([...newArray])
+    }
+    const addSector = () => {
+        let newArray = [...navSector];
+        newArray.push(navSectorArray[0]);
+        setNavSector([...newArray])
+    }
+    const addService = () => {
+        let newArray = [...navServices];
+        newArray.push(navServiceArray[0]);
+        setNavServices([...newArray])
+    }
     const editorRef = useRef(null);
     return (
 
@@ -57,14 +79,14 @@ export default function Navigation4({ displayDevice, linksfunc }) {
                             <li className={`${Nav4Css.navBarLink} hover:text-black`}> {linksfunc ? <a href="about.html">ABOUT US</a> : <> ABOUT US</>}</li>
                             <li className={`${Nav4Css.navBarLink} hover:text-black ${Nav4Css.dropdownServiceListItem}`}>SERVICES
                                 <ul className={`${Nav4Css.dropdownServicesContent} list-none p-0 absolute`}>
-                                    {navServices.map((item) => <li className={`${Nav4Css.servicesLinks} flex mt-8 mx-4 mb-0`}>
-                                        <h4 className={Nav4Css.navigationLinkContent}>{item}</h4>
-                                    </li>)}
+                                    {navServices.map((item, index) => <li className={`${Nav4Css.servicesLinks} flex mt-8 mx-4 mb-0 justify-between`}><h4 className={Nav4Css.navigationLinkContent}>{item}</h4><button onClick={() => removeService(index)}><i className="ml-4 fas fa-trash-alt"></i></button></li>)}
+                                    <li className={`flex mt-8 mx-4 mb-0 justify-between`}><button onClick={() => addService()}><i className="fas fa-plus"></i> ADD NEW SERVICE</button></li>
                                 </ul>
                             </li>
                             <li className={`${Nav4Css.navBarLink} hover:text-black ${Nav4Css.dropdownSectorsListItem}`}>SECTORS
                                 <ul className={`${Nav4Css.dropdownSectorsContent} list-none p-0 absolute`}>
-                                    {navSector.map((items) => <li className={`${Nav4Css.sectorsLink} flex mt-8 mx-4 mb-0`}><h4 className={Nav4Css.navigationLinkContent}>{items}</h4></li>)}
+                                    {navSector.map((items, indexs) => <li className={`${Nav4Css.sectorsLink} flex mt-8 mx-4 mb-0 justify-between`}><h4 className={Nav4Css.navigationLinkContent}>{items}</h4><button onClick={() => removeSector(indexs)}><i className="ml-4 fas fa-trash-alt"></i></button></li>)}
+                                    <li className={`flex mt-8 mx-4 mb-0 justify-between`}><button onClick={() => addSector()}><i className="fas fa-plus"></i> ADD NEW SECTOR</button></li>
                                 </ul>
                             </li>
                             <li className={`${Nav4Css.navBarLink} hover:text-black`}>
