301bc40a215807bab7c8f06d63a58c91a65a0728
Ahsan Shahid <ahsanshahid2882@gmail.com>
2021-11-12 12:28:43 +0500

    Update Footer3.js

--- src/template3components/Footer3.js
+++ src/template3components/Footer3.js
@@ -13,6 +13,7 @@ export default function Footer3({ showPopup, overSection }) {
                 <div style={{ background: currentBackground }} className={Footer3Css['footer']}>
                     <div className={Footer3Css['row-foot']}>
                         <div className={Footer3Css['column-foot']}>
+                            <div className={Footer3Css['span']}>...........</div>
                             <h2 className={Footer3Css.hh1}>
                                 <Editor
                                     onInit={(evt, editor) => editorRef.current = editor}
@@ -130,24 +131,7 @@ export default function Footer3({ showPopup, overSection }) {
                             </div>
                         </div>
                         <div className={Footer3Css['column-foot']}>
-                            <div className={Footer3Css['span']}>
-                                <Editor
-                                    onInit={(evt, editor) => editorRef.current = editor}
-                                    inline={true}
-                                    key='Footer7'
-                                    initialValue=" .........."
-                                    init={{
-                                        forced_root_block: "",
-                                        menubar: false,
-                                        toolbar: 'bold italic underline | fontsizeselect | backcolor forecolor | alignleft aligncenter ' +
-                                            'alignright alignjustify | fontselect | lineheight',
-                                        fontsize_formats: "8px 9px 10px 11px 12px 14px 16px 18px 20px 24px 30px 36px 48px 60px 72px 96px",
-                                        font_formats: "Roboto=roboto, sans-serif, serif; Mohave=mohave, sans-serif, serif;Open Sans=open sans, sans-serif, serif;Lato=lato, sans-serif, serif; Red Hat Mono=red hat mono, sans-serif, serif; Montserrat=montserrat, sans-serif, serif; Roboto Condensed=roboto condensed, sans-serif, serif; Source Sans Pro= source sans pro, sans-serif, serif; Dongle=dongle, sans-serif, serif; Poppins=poppins, sans-serif, serif; Oswald=oswald, sans-serif, serif; Roboto Mono=roboto mono, sans-serif, serif; Raleway=raleway, sans-serif, serif; Ubuntu=ubuntu, sans-serif, serif; Merriweather=merriweather, sans-serif, serif;Andale Mono=andale mono,times; Arial=arial,helvetica,sans-serif; Arial Black=arial black,avant garde; Book Antiqua=book antiqua,palatino; Comic Sans MS=comic sans ms,sans-serif; Courier New=courier new,courier; Georgia=georgia,palatino; Helvetica=helvetica; Impact=impact,chicago; Tahoma=tahoma,arial,helvetica,sans-serif; Terminal=terminal,monaco; Times New Roman=times new roman,times; Trebuchet MS=trebuchet ms,geneva; Verdana=verdana,geneva",
-                                        // 
-                                        //     
-                                    }}
-                                />
-                            </div>
+                            <div className={Footer3Css['span']}>...........</div>
                             <h2 className={Footer3Css.hh1}>
                                 <Editor
                                     onInit={(evt, editor) => editorRef.current = editor}
@@ -273,24 +257,7 @@ export default function Footer3({ showPopup, overSection }) {
                         </div>
                         <div>
                             <div className={Footer3Css['column-foot']}>
-                                <div className={Footer3Css['span']}>
-                                    <Editor
-                                        onInit={(evt, editor) => editorRef.current = editor}
-                                        inline={true}
-                                        key='Footer12'
-                                        initialValue=".........."
-                                        init={{
-                                            forced_root_block: "",
-                                            menubar: false,
-                                            toolbar: 'bold italic underline | fontsizeselect | backcolor forecolor | alignleft aligncenter ' +
-                                                'alignright alignjustify | fontselect | lineheight',
-                                            fontsize_formats: "8px 9px 10px 11px 12px 14px 16px 18px 20px 24px 30px 36px 48px 60px 72px 96px",
-                                            font_formats: "Roboto=roboto, sans-serif, serif; Mohave=mohave, sans-serif, serif;Open Sans=open sans, sans-serif, serif;Lato=lato, sans-serif, serif; Red Hat Mono=red hat mono, sans-serif, serif; Montserrat=montserrat, sans-serif, serif; Roboto Condensed=roboto condensed, sans-serif, serif; Source Sans Pro= source sans pro, sans-serif, serif; Dongle=dongle, sans-serif, serif; Poppins=poppins, sans-serif, serif; Oswald=oswald, sans-serif, serif; Roboto Mono=roboto mono, sans-serif, serif; Raleway=raleway, sans-serif, serif; Ubuntu=ubuntu, sans-serif, serif; Merriweather=merriweather, sans-serif, serif;Andale Mono=andale mono,times; Arial=arial,helvetica,sans-serif; Arial Black=arial black,avant garde; Book Antiqua=book antiqua,palatino; Comic Sans MS=comic sans ms,sans-serif; Courier New=courier new,courier; Georgia=georgia,palatino; Helvetica=helvetica; Impact=impact,chicago; Tahoma=tahoma,arial,helvetica,sans-serif; Terminal=terminal,monaco; Times New Roman=times new roman,times; Trebuchet MS=trebuchet ms,geneva; Verdana=verdana,geneva",
-                                            // 
-                                            //     
-                                        }}
-                                    />
-                                </div>
+                                <div className={Footer3Css['span']}>...........</div>
                                 <h2 className={Footer3Css['hh1']}>
 
                                     <Editor
