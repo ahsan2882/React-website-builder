4c4351df14efd5dc2313b1365c39e750a068f87c
Ahsan Shahid <ahsanshahid2882@gmail.com>
2021-11-12 18:50:45 +0500

    Update About1.js

--- src/template1components/About1.js
+++ src/template1components/About1.js
@@ -59,9 +59,24 @@ export const About1 = ({ showPopup, overSection, displayDevice }) => {
         <div style={{ background: currentBackground }} className={`${About1Css.container9}`}>
           <div style={{ background: currentBackground }} className={displayDevice ? ` mt-36 ${About1Css.row}` : ` mt-36 ${About1Css.rowM}`}>
             <div style={{ background: currentBackground }} className="container9-content">
+              <Editor
+                onInit={(evt, editor) => editorRef.current = editor}
+                inline={true}
+                key='About333'
+                initialValue={`<span class="${About1Css.headlinecontent2}""><span><b>Commited To</b> Your Business</span></span>`}
+                init={{
+                  plugins: [
+                    "link"
+                  ],
+                  forced_root_block: "",
+                  menubar: false,
+                  toolbar: 'bold italic underline | link | fontsizeselect | backcolor forecolor | alignleft aligncenter ' +
+                    'alignright alignjustify | fontselect | lineheight',
+                  fontsize_formats: "8px 9px 10px 11px 12px 14px 16px 18px 20px 24px 30px 36px 48px 60px 72px 96px",
+                  font_formats: "Roboto=roboto, sans-serif, serif; Mohave=mohave, sans-serif, serif;Open Sans=open sans, sans-serif, serif;Lato=lato, sans-serif, serif; Red Hat Mono=red hat mono, sans-serif, serif; Montserrat=montserrat, sans-serif, serif; Roboto Condensed=roboto condensed, sans-serif, serif; Source Sans Pro= source sans pro, sans-serif, serif; Dongle=dongle, sans-serif, serif; Poppins=poppins, sans-serif, serif; Oswald=oswald, sans-serif, serif; Roboto Mono=roboto mono, sans-serif, serif; Raleway=raleway, sans-serif, serif; Ubuntu=ubuntu, sans-serif, serif; Merriweather=merriweather, sans-serif, serif",
 
-              <span className={`${About1Css['headline_content2']}`}><span><b>Commited To</b> Your Business</span></span>
-              {/* <p className={`${About1Css['testimonial-text']}`}> */}
+                }}
+              />
               <Editor
                 onInit={(evt, editor) => editorRef.current = editor}
                 inline={true}
@@ -80,8 +95,6 @@ export const About1 = ({ showPopup, overSection, displayDevice }) => {
 
                 }}
               />
-
-              {/* </p> */}
               <div className={`${About1Css.author2}`}>
                 <button className={`${About1Css.btna}`}>
 
