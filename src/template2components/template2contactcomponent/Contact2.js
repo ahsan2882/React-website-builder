1dc1923ddf41201a0bd001cfef5542a2dca2302e
Ahsan Shahid <ahsanshahid2882@gmail.com>
2021-11-13 08:52:20 +0500

    Update Contact2.js

--- src/template2components/template2contactcomponent/Contact2.js
+++ src/template2components/template2contactcomponent/Contact2.js
@@ -2,12 +2,13 @@ import React, { useRef } from 'react'
 import Contact2Css from './Contact2.module.css'
 import { Editor } from '@tinymce/tinymce-react';
 import { Navbar2 } from '../Navbar2';
-export default function Contact2({ displayDevice, linksfunc }) {
+export default function Contact2({ displayDevice, linksfunc, nav2Service, setNav2Services, nav2Sector, setNav2Sector }) {
     const editorRef = useRef(null);
     return (
         <>
             <section className={Contact2Css['header-img-serv']}>
-                <Navbar2 displayDevice={displayDevice} linksfunc={linksfunc} />
+                <Navbar2 displayDevice={displayDevice} linksfunc={linksfunc} nav2Service={nav2Service} setNav2Services={setNav2Services}
+                    nav2Sector={nav2Sector} setNav2Sector={setNav2Sector} />
                 <div className={Contact2Css['head-img-text']}>
                     <h1>
                         <Editor
