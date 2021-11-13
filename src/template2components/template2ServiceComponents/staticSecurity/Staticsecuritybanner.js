d214dfc21bbed1f4fd14166a017d39fe40250e56
Ahsan Shahid <ahsanshahid2882@gmail.com>
2021-11-13 08:53:19 +0500

    Update Staticsecuritybanner.js

--- src/template2components/template2ServiceComponents/staticSecurity/Staticsecuritybanner.js
+++ src/template2components/template2ServiceComponents/staticSecurity/Staticsecuritybanner.js
@@ -4,11 +4,12 @@ import StaticsecurityCss from './staticsecuritybanner.module.css'
 import { Editor } from '@tinymce/tinymce-react';
 import { Navbar2 } from '../../Navbar2';
 
-export default function Staticsecuritybanner({ displayDevice, linksfunc }) {
+export default function Staticsecuritybanner({ displayDevice, linksfunc, nav2Service, setNav2Services, nav2Sector, setNav2Sector }) {
     const editorRef = useRef(null);
     return (
         <section className={StaticsecurityCss['header-img-serv']}>
-            <Navbar2 displayDevice={displayDevice} linksfunc={linksfunc} />
+            <Navbar2 displayDevice={displayDevice} linksfunc={linksfunc} nav2Service={nav2Service} setNav2Services={setNav2Services}
+                    nav2Sector={nav2Sector} setNav2Sector={setNav2Sector}/>
             <div class={StaticsecurityCss['head-img-text']}>
                 <h1 className={StaticsecurityCss.head1}>
                     <Editor
